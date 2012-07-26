93e5bfc7bedbff0507d47deb49a4b3960f494fae
Byron Lutz <byronlutz@gmail.com>
2012-07-26 16:11:40 -0700

    Responsive for large screen.

--- js/script.js
+++ js/script.js
@@ -1,10 +1,13 @@
-/* Author:
+/* Author: Byron Lutz 
 
 */
 
 
 
+/****** RESPONSIVE STYLES *******/
 
-
-
-
+// Large screen
+if($(window).width() >= 1200) {
+	$('#topcontent-rotator').removeClass('span8').addClass('span7');
+	$('#topcontent-stories').removeClass('span4').addClass('span5');
+}
