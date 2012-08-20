e2c283b459cb4712889f6b46a471be64bed35a15
Byron Lutz <byronlutz@gmail.com>
2012-08-20 02:31:17 -0700

    Moved ad code into a seperate template part. Enabled all ads.

--- 404.php
+++ 404.php
@@ -20,11 +20,7 @@
       
       	<aside class="span4" id="sidebar" role="complementary">
 		<div id="sidebar-inside">
-			<div class="visible-desktop" id="side-ad">
-<!-- 									<iframe id='a1b65622' name='a1b65622' src='http://www.oncampusweb.com/delivery/afr.php?zoneid=162&amp;cb=893274237927328048530' framespacing='0' frameborder='no' scrolling='no' width='300' height='250'><a href='http://www.oncampusweb.com/delivery/ck.php?n=a04df0d6&amp;cb=893274237927328048530' target='_blank'><img src='http://www.oncampusweb.com/delivery/avw.php?zoneid=162&amp;cb=893274237927328048530&amp;n=a04df0d6' border='0' alt='' /></a></iframe> -->
-				<img class="ad visible-desktop" src="http://placehold.it/300x250&text=ADVERTISEMENT" />
-			</div>
-
+		<?php get_template_part('ad','side'); ?>
 			
 			
 			<div id="sidebar-popular">
