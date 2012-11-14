40b3745dcbc7af47c5f4c083ef0baef7fafec251
Byron Lutz <byronlutz@gmail.com>
2012-11-14 14:28:20 -0800

    Nameplate now links to the homepage. Fixes #16

--- templates/header-default.php
+++ templates/header-default.php
@@ -18,7 +18,7 @@
 					<span style="color: white;">Today's Paper</span>
 				</div>
 				<div class="span6" id="nameplate-image">
-					<a href="<?php home_url('/'); ?>"><img src="/img/nameplate.png" /></a>
+					<a href="<?php echo home_url('/'); ?>"><img src="/img/nameplate.png" /></a>
 					<button type="button" class="btn btn-navbar visible-phone" data-toggle="collapse" data-target="#mobile-nav">
 						<span class="icon-bar"></span>
 						<span class="icon-bar"></span>
