5c37d4a4c7182f59335d5892ebb2e256b3219fc8
Byron Lutz <byronlutz@gmail.com>
2013-01-02 09:42:55 -0800

    Customized our first house ad

--- ad-housebanner.php
+++ ad-housebanner.php
@@ -1,5 +1,10 @@
 			<div class="visible-desktop" id="ad-house">
 				<noscript>
- 				<img class="ad visible-desktop" src="http://placehold.it/468x60&text=House%20ad" />
+				<?php // 468px by 60px ?>
+				<a href="http://graphics.dailybruin.com/survey/index.php/survey/index/sid/761299/lang/en" class="housead-survey" target="_blank">
+					<h4>Welcome to the new DailyBruin.com</h4>
+					<span>Take a brief survey to let us know what you think &raquo;</span>
+				</a><!-- div.housead-survey -->
+
  				</noscript>
 			</div>
