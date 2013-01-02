1873663ec36580f2cfebc97904dd84dc931eba7c
Byron Lutz <byronlutz@gmail.com>
2013-01-02 08:55:46 -0800

    404 page improvements

--- 404.php
+++ 404.php
@@ -10,6 +10,7 @@
 			<div class="f-search">
 				<?php get_search_form(); ?>
 			</div><!-- end div.f-search -->
+			<?php if(function_exists('wbz404_suggestions')) {wbz404_suggestions();} ?>
 		</div><!-- end div.fourohfour -->
 	</div><!-- end div.span8 -->
 
