8f6992138c5b39d3a7293b521cc2f08bad0b4551
Byron Lutz <byronlutz@gmail.com>
2013-02-17 09:31:11 -0800

    Now it actually works for mobile

--- page-dance-marathon.php
+++ page-dance-marathon.php
@@ -1,8 +1,10 @@
 <?php get_header(); ?>
-<script>
-jQuery(document.ready(function() {
-    $('meta[name="viewport"]').attr('content','width=980, initial-scale=1.0');
-}));
+<script type="text/javascript">
+jQuery(document).ready(function() {
+    $('meta[name="viewport"]').attr('content','width=980, initial-scale=0.1');
+    $('link[href="/css/bootstrap-responsive.css"]').attr('disabled','true');
+    $('link[href="/css/app-responsive.css"]').attr('disabled','true');
+});
 </script>
 <style type="text/css">
     .dm-2013-page {
