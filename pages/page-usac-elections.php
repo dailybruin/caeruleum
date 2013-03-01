bab8d8c30e5ac90406c26d7fd33b3d2eeab3ce08
jsjwooowooo <jsjwooowooo@gmail.com>
2013-03-01 12:29:23 -0800

    Added a template page for project

---
+++ pages/page-usac-elections.php
@@ -0,0 +1,31 @@
+<?php/*
+Template Name: USAC Elections 2013
+*/ ?>
+<?php get_header(); ?>
+
+<script type="text/javascript">
+
+jQuery(document).ready(function() {
+    unresponsivize();
+}); 
+
+</script>
+
+<style type="text/css">
+
+</style>
+
+<div id="usac page">
+
+    <!-- Assuming we don't need regular DB sidebar -->
+
+    <!-- TODO: What ads do we have to display? This one? <?php get_template_part('ad','smallbanner'); ?>--> 
+
+	<?php get_template_part('ad','side'); ?>
+
+    
+	
+</div><!-- end div#usac page -->      
+
+
+<?php get_footer(); ?>
