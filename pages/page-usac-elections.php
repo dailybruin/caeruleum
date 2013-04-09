a730a8ef23431096b08a292b001d4eae4df08a18
mmanjunath <megha.manjunath@gmail.com>
2013-04-09 10:16:21 -0700

    Added html code

--- pages/page-usac-elections.php
+++ pages/page-usac-elections.php
@@ -16,6 +16,32 @@ jQuery(document).ready(function() {
 </style>
 
 <div id="usac page">
+<!DOCTYPE html>
+<html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+        <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <title>USAC Elections | Daily Bruin</title>
+        <link rel="stylesheet" type="text/css" href="usac.css" />
+        </head>
+        <body>
+        <div id="container">
+        <div id="banner">BANNER</div>
+        <div id="page_tab">
+        <div id="candidate">CANDIDATE</div>
+        <div id="info">INFO</div>
+        <div id="opinion">OPINION</div>
+        </div>
+        <div id="campaign_violation">CAMPAIGN VIOLATION</div>
+        <div id="calendar">CALENDAR</div>
+        <div id="article">ARTICLE</div>
+        <div id="social_media">SOCIAL MEDIA</div>
+        </div>
+        </body>
 
     <!-- Assuming we don't need regular DB sidebar -->
 
