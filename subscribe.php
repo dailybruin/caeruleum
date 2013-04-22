018121cd9994418b5b83c68f8a2971370ac9af47
Aman <aagarwal@media.ucla.edu>
2013-04-21 20:29:39 -0700

    added Kenny stuff accordian wooowooowoo

--- subscribe.php
+++ subscribe.php
@@ -17,9 +17,15 @@ p.inner {
   font-size: 18.5px;
   font-family: "Arial", serif;
 }
+
+#accordion {
+  font-size: 20px;
+  font-family: "Arial", serif;
+}
+
 #unimportant {
-	font-size: 13px;
-	font-family: "Arial", serif;
+  font-size: 17px;
+  font-family: "Arial", serif;
 }
 p.list_check {
     width: 50%;
@@ -94,7 +100,12 @@ p.big {
     <!--
 widget_wysija 
     -->
-        <p class="inner">To subscribe to our dandy newsletter simply add your email below. A confirmation email will be sent to you!</p>
+        <p class="inner">
+          To subscribe to our dandy newsletter simply add your email below. A confirmation email will be sent to you!
+        </p>
+        <p class="inner">
+          To customize your interests and preferences, please select from the options below.
+        </p>
     <div class="row">
     <div class="span3 offset1">
       <p class="wysija_list_check">
@@ -120,7 +131,7 @@ widget_wysija
     </div>
     </div>
     <div class="row">
-    <div class="span6 offset1">
+    <div class="span8">
         <p class="wysija-p-email"><input type="text" id="form-html51749a146e680-wysija-to" value="<?php echo $_GET['email']; ?>" placeholder="Email" title="Email" class="input_field defaultlabels wysija-email validate[required,custom[email]]" name="wysija[user][email]" /></p>
         <span class="wysija-p-email abs-req"><input type="text" id="form-html5160b35adfee7-abs-email" value="" class="defaultlabels validated[abs][email]" name="wysija[user][abs][email]" /></span>
 
@@ -128,104 +139,110 @@ widget_wysija
         <input type="hidden" name="action" value="save" />
         <input type="hidden" name="message_success" value="Check your inbox now to confirm your subscription." />
         <input type="hidden" name="controller" value="subscribers" /><input type="hidden" value="1" name="wysija-page" />
+        <div class="wysija_lists">
+<div class="accordion" id="accordion">
+  <div class="accordion-group">
+    <div class="accordion-heading">
+      <a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion2" href="#collapseOne">
+        Click here to see some other newsletters that you can subscribe to!
+      </a>
     </div>
-    </div>
-    <div class="row">
-    <div class="span7">
-            <p class="inner">
-            To customize your interests and preferences, please select from the options below.
-            </p>
-    </div>
-    </div>
-    <div class="wysija_lists">
-		<div class="row">
-		<div class="span4">
-      <p class="wysija_list_check">
-        <label for="form-html51749e245b688_list_id_33"><input id="form-html51749e245b688_list_id_33" class="validate[minCheckbox[1]] checkbox checklists" type="checkbox" name="wysija[user_list][list_id][]" value="33" checked="checked"/> Women's Water Polo </label>
-      </p>
-      <p class="wysija_list_check">
-        <label for="form-html51749e245b688_list_id_32"><input id="form-html51749e245b688_list_id_32" class="validate[minCheckbox[1]] checkbox checklists" type="checkbox" name="wysija[user_list][list_id][]" value="32" checked="checked"/> Women's Volleyball </label>
-      </p>
-      <p class="wysija_list_check">
-        <label for="form-html51749e245b688_list_id_31"><input id="form-html51749e245b688_list_id_31" class="validate[minCheckbox[1]] checkbox checklists" type="checkbox" name="wysija[user_list][list_id][]" value="31" checked="checked"/> Women's Track and Field </label>
-      </p>
-      <p class="wysija_list_check">
-        <label for="form-html51749e245b688_list_id_30"><input id="form-html51749e245b688_list_id_30" class="validate[minCheckbox[1]] checkbox checklists" type="checkbox" name="wysija[user_list][list_id][]" value="30" checked="checked"/> Women's Tennis </label>
-      </p>
-      <p class="wysija_list_check">
-        <label for="form-html51749e245b688_list_id_29"><input id="form-html51749e245b688_list_id_29" class="validate[minCheckbox[1]] checkbox checklists" type="checkbox" name="wysija[user_list][list_id][]" value="29" checked="checked"/> Women's Soccer </label>
-      </p>
-      <p class="wysija_list_check">
-        <label for="form-html51749e245b688_list_id_28"><input id="form-html51749e245b688_list_id_28" class="validate[minCheckbox[1]] checkbox checklists" type="checkbox" name="wysija[user_list][list_id][]" value="28" checked="checked"/> Women's Golf </label>
-      </p>
-      <p class="wysija_list_check">
-        <label for="form-html51749e245b688_list_id_27"><input id="form-html51749e245b688_list_id_27" class="validate[minCheckbox[1]] checkbox checklists" type="checkbox" name="wysija[user_list][list_id][]" value="27" checked="checked"/> Women's Basketball </label>
-      </p>
-      <p class="wysija_list_check">
-        <label for="form-html51749e245b688_list_id_26"><input id="form-html51749e245b688_list_id_26" class="validate[minCheckbox[1]] checkbox checklists" type="checkbox" name="wysija[user_list][list_id][]" value="26" checked="checked"/> Swim and Dive </label>
-      </p>
-      <p class="wysija_list_check">
-        <label for="form-html51749e245b688_list_id_25"><input id="form-html51749e245b688_list_id_25" class="validate[minCheckbox[1]] checkbox checklists" type="checkbox" name="wysija[user_list][list_id][]" value="25" checked="checked"/> Softball </label>
-      </p>
-      <p class="wysija_list_check">
-        <label for="form-html51749e245b688_list_id_24"><input id="form-html51749e245b688_list_id_24" class="validate[minCheckbox[1]] checkbox checklists" type="checkbox" name="wysija[user_list][list_id][]" value="24" checked="checked"/> Rowing </label>
-      </p>
-      <p class="wysija_list_check">
-        <label for="form-html51749e245b688_list_id_23"><input id="form-html51749e245b688_list_id_23" class="validate[minCheckbox[1]] checkbox checklists" type="checkbox" name="wysija[user_list][list_id][]" value="23" checked="checked"/> Men's Water Polo </label>
-      </p>
-      <p class="wysija_list_check">
-        <label for="form-html51749e245b688_list_id_22"><input id="form-html51749e245b688_list_id_22" class="validate[minCheckbox[1]] checkbox checklists" type="checkbox" name="wysija[user_list][list_id][]" value="22" checked="checked"/> Men's Volleyball </label>
-      </p>
-      </div>
+    <div id="collapseOne" class="accordion-body collapse">
+      <div class="accordion-inner">
+      <div class="row">
       <div class="span4">
-      <p class="wysija_list_check">
-        <label for="form-html51749e245b688_list_id_21"><input id="form-html51749e245b688_list_id_21" class="validate[minCheckbox[1]] checkbox checklists" type="checkbox" name="wysija[user_list][list_id][]" value="21" checked="checked"/> Men's Track and Field </label>
+        <p class="wysija_list_check">
+        <label id="unimportant" for="form-html51749e245b688_list_id_33"><input id="form-html51749e245b688_list_id_33" class="validate[minCheckbox[1]] checkbox checklists" type="checkbox" name="wysija[user_list][list_id][]" value="33" checked="checked"/> Women's Water Polo </label>
       </p>
       <p class="wysija_list_check">
-        <label for="form-html51749e245b688_list_id_20"><input id="form-html51749e245b688_list_id_20" class="validate[minCheckbox[1]] checkbox checklists" type="checkbox" name="wysija[user_list][list_id][]" value="20" checked="checked"/> Men's Tennis </label>
+        <label id="unimportant" for="form-html51749e245b688_list_id_32"><input id="form-html51749e245b688_list_id_32" class="validate[minCheckbox[1]] checkbox checklists" type="checkbox" name="wysija[user_list][list_id][]" value="32" checked="checked"/> Women's Volleyball </label>
       </p>
       <p class="wysija_list_check">
-        <label for="form-html51749e245b688_list_id_19"><input id="form-html51749e245b688_list_id_19" class="validate[minCheckbox[1]] checkbox checklists" type="checkbox" name="wysija[user_list][list_id][]" value="19" checked="checked"/> Men's Soccer </label>
+        <label id="unimportant" for="form-html51749e245b688_list_id_31"><input id="form-html51749e245b688_list_id_31" class="validate[minCheckbox[1]] checkbox checklists" type="checkbox" name="wysija[user_list][list_id][]" value="31" checked="checked"/> Women's Track and Field </label>
       </p>
       <p class="wysija_list_check">
-        <label for="form-html51749e245b688_list_id_18"><input id="form-html51749e245b688_list_id_18" class="validate[minCheckbox[1]] checkbox checklists" type="checkbox" name="wysija[user_list][list_id][]" value="18" checked="checked"/> Men's Golf </label>
+        <label id="unimportant" for="form-html51749e245b688_list_id_30"><input id="form-html51749e245b688_list_id_30" class="validate[minCheckbox[1]] checkbox checklists" type="checkbox" name="wysija[user_list][list_id][]" value="30" checked="checked"/> Women's Tennis </label>
       </p>
       <p class="wysija_list_check">
-        <label for="form-html51749e245b688_list_id_17"><input id="form-html51749e245b688_list_id_17" class="validate[minCheckbox[1]] checkbox checklists" type="checkbox" name="wysija[user_list][list_id][]" value="17" checked="checked"/> Men's Basketball </label>
+        <label id="unimportant" for="form-html51749e245b688_list_id_29"><input id="form-html51749e245b688_list_id_29" class="validate[minCheckbox[1]] checkbox checklists" type="checkbox" name="wysija[user_list][list_id][]" value="29" checked="checked"/> Women's Soccer </label>
       </p>
       <p class="wysija_list_check">
-        <label for="form-html51749e245b688_list_id_16"><input id="form-html51749e245b688_list_id_16" class="validate[minCheckbox[1]] checkbox checklists" type="checkbox" name="wysija[user_list][list_id][]" value="16" checked="checked"/> Gymnastics </label>
+        <label id="unimportant" for="form-html51749e245b688_list_id_28"><input id="form-html51749e245b688_list_id_28" class="validate[minCheckbox[1]] checkbox checklists" type="checkbox" name="wysija[user_list][list_id][]" value="28" checked="checked"/> Women's Golf </label>
       </p>
       <p class="wysija_list_check">
-        <label for="form-html51749e245b688_list_id_15"><input id="form-html51749e245b688_list_id_15" class="validate[minCheckbox[1]] checkbox checklists" type="checkbox" name="wysija[user_list][list_id][]" value="15" checked="checked"/> Cross Country </label>
+        <label id="unimportant" for="form-html51749e245b688_list_id_27"><input id="form-html51749e245b688_list_id_27" class="validate[minCheckbox[1]] checkbox checklists" type="checkbox" name="wysija[user_list][list_id][]" value="27" checked="checked"/> Women's Basketball </label>
       </p>
       <p class="wysija_list_check">
-        <label for="form-html51749e245b688_list_id_14"><input id="form-html51749e245b688_list_id_14" class="validate[minCheckbox[1]] checkbox checklists" type="checkbox" name="wysija[user_list][list_id][]" value="14" checked="checked"/> Club and Intramural Sports </label>
+        <label id="unimportant" for="form-html51749e245b688_list_id_26"><input id="form-html51749e245b688_list_id_26" class="validate[minCheckbox[1]] checkbox checklists" type="checkbox" name="wysija[user_list][list_id][]" value="26" checked="checked"/> Swim and Dive </label>
       </p>
       <p class="wysija_list_check">
-        <label for="form-html51749e245b688_list_id_13"><input id="form-html51749e245b688_list_id_13" class="validate[minCheckbox[1]] checkbox checklists" type="checkbox" name="wysija[user_list][list_id][]" value="13" checked="checked"/> Baseball </label>
+        <label id="unimportant" for="form-html51749e245b688_list_id_25"><input id="form-html51749e245b688_list_id_25" class="validate[minCheckbox[1]] checkbox checklists" type="checkbox" name="wysija[user_list][list_id][]" value="25" checked="checked"/> Softball </label>
       </p>
       <p class="wysija_list_check">
-        <label for="form-html51749e245b688_list_id_12"><input id="form-html51749e245b688_list_id_12" class="validate[minCheckbox[1]] checkbox checklists" type="checkbox" name="wysija[user_list][list_id][]" value="12" checked="checked"/> Opinion </label>
+        <label id="unimportant" for="form-html51749e245b688_list_id_24"><input id="form-html51749e245b688_list_id_24" class="validate[minCheckbox[1]] checkbox checklists" type="checkbox" name="wysija[user_list][list_id][]" value="24" checked="checked"/> Rowing </label>
       </p>
       <p class="wysija_list_check">
-        <label for="form-html51749e245b688_list_id_11"><input id="form-html51749e245b688_list_id_11" class="validate[minCheckbox[1]] checkbox checklists" type="checkbox" name="wysija[user_list][list_id][]" value="11" checked="checked"/> A&E </label>
+        <label id="unimportant" for="form-html51749e245b688_list_id_23"><input id="form-html51749e245b688_list_id_23" class="validate[minCheckbox[1]] checkbox checklists" type="checkbox" name="wysija[user_list][list_id][]" value="23" checked="checked"/> Men's Water Polo </label>
       </p>
       <p class="wysija_list_check">
-        <label for="form-html51749e245b688_list_id_10"><input id="form-html51749e245b688_list_id_10" class="validate[minCheckbox[1]] checkbox checklists" type="checkbox" name="wysija[user_list][list_id][]" value="10" checked="checked"/> News </label>
+        <label id="unimportant" for="form-html51749e245b688_list_id_22"><input id="form-html51749e245b688_list_id_22" class="validate[minCheckbox[1]] checkbox checklists" type="checkbox" name="wysija[user_list][list_id][]" value="22" checked="checked"/> Men's Volleyball </label>
       </p>
-	  </div>
-		</div>
+    </div>
+    <div class="span4">
+      <p class="wysija_list_check">
+        <label id="unimportant" for="form-html51749e245b688_list_id_21"><input id="form-html51749e245b688_list_id_21" class="validate[minCheckbox[1]] checkbox checklists" type="checkbox" name="wysija[user_list][list_id][]" value="21" checked="checked"/> Men's Track and Field </label>
+      </p>
+      <p class="wysija_list_check">
+        <label id="unimportant" for="form-html51749e245b688_list_id_20"><input id="form-html51749e245b688_list_id_20" class="validate[minCheckbox[1]] checkbox checklists" type="checkbox" name="wysija[user_list][list_id][]" value="20" checked="checked"/> Men's Tennis </label>
+      </p>
+      <p class="wysija_list_check">
+        <label id="unimportant" for="form-html51749e245b688_list_id_19"><input id="form-html51749e245b688_list_id_19" class="validate[minCheckbox[1]] checkbox checklists" type="checkbox" name="wysija[user_list][list_id][]" value="19" checked="checked"/> Men's Soccer </label>
+      </p>
+      <p class="wysija_list_check">
+        <label id="unimportant" for="form-html51749e245b688_list_id_18"><input id="form-html51749e245b688_list_id_18" class="validate[minCheckbox[1]] checkbox checklists" type="checkbox" name="wysija[user_list][list_id][]" value="18" checked="checked"/> Men's Golf </label>
+      </p>
+      <p class="wysija_list_check">
+        <label id="unimportant" for="form-html51749e245b688_list_id_17"><input id="form-html51749e245b688_list_id_17" class="validate[minCheckbox[1]] checkbox checklists" type="checkbox" name="wysija[user_list][list_id][]" value="17" checked="checked"/> Men's Basketball </label>
+      </p>
+      <p class="wysija_list_check">
+        <label id="unimportant" for="form-html51749e245b688_list_id_16"><input id="form-html51749e245b688_list_id_16" class="validate[minCheckbox[1]] checkbox checklists" type="checkbox" name="wysija[user_list][list_id][]" value="16" checked="checked"/> Gymnastics </label>
+      </p>
+      <p class="wysija_list_check">
+        <label id="unimportant" for="form-html51749e245b688_list_id_15"><input id="form-html51749e245b688_list_id_15" class="validate[minCheckbox[1]] checkbox checklists" type="checkbox" name="wysija[user_list][list_id][]" value="15" checked="checked"/> Cross Country </label>
+      </p>
+      <p class="wysija_list_check">
+        <label id="unimportant" for="form-html51749e245b688_list_id_14"><input id="form-html51749e245b688_list_id_14" class="validate[minCheckbox[1]] checkbox checklists" type="checkbox" name="wysija[user_list][list_id][]" value="14" checked="checked"/> Club and Intramural Sports </label>
+      </p>
+      <p class="wysija_list_check">
+        <label id="unimportant" for="form-html51749e245b688_list_id_13"><input id="form-html51749e245b688_list_id_13" class="validate[minCheckbox[1]] checkbox checklists" type="checkbox" name="wysija[user_list][list_id][]" value="13" checked="checked"/> Baseball </label>
+      </p>
+      <p class="wysija_list_check">
+        <label id="unimportant" for="form-html51749e245b688_list_id_12"><input id="form-html51749e245b688_list_id_12" class="validate[minCheckbox[1]] checkbox checklists" type="checkbox" name="wysija[user_list][list_id][]" value="12" checked="checked"/> Opinion </label>
+      </p>
+      <p class="wysija_list_check">
+        <label id="unimportant" for="form-html51749e245b688_list_id_11"><input id="form-html51749e245b688_list_id_11" class="validate[minCheckbox[1]] checkbox checklists" type="checkbox" name="wysija[user_list][list_id][]" value="11" checked="checked"/> A&E </label>
+      </p>
+      <p class="wysija_list_check">
+        <label id="unimportant" for="form-html51749e245b688_list_id_10"><input id="form-html51749e245b688_list_id_10" class="validate[minCheckbox[1]] checkbox checklists" type="checkbox" name="wysija[user_list][list_id][]" value="10" checked="checked"/> News </label>
+      </p>
+    </div>
+   </div>
+    </div>
+    </div>
+  </div>
+  </div>
+    </div>
+    </div>
     </div>
     </form>
 </div>
 
 
         <?php get_template_part('loop', 'page'); ?>
-	</div><!-- end div#post -->
-	
-	<?php get_sidebar(); ?>
-	
+  </div><!-- end div#post -->
+  
+  <?php get_sidebar(); ?>
+  
 </div><!-- end div#single-post -->      
 
 
