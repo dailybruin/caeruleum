bf5f561e708b358135b8542c5054671e6c70be7d
mmanjunath <megha.manjunath@gmail.com>
2013-04-28 23:06:30 -0700

    Update page-usac-elections-2013.php
    update twitter code

--- pages/page-usac-elections-2013.php
+++ pages/page-usac-elections-2013.php
@@ -144,8 +144,9 @@ Template Name: USAC Elections 2013
       <div class='ad'>
         <?php get_template_part('ad','side'); ?>
       </div>
-       <a class="twitter-timeline" href="https://twitter.com/search?q=%23USACelections" data-widget-id="328262590398603264">Tweets about "#USACelections"</a>
+      <a class="twitter-timeline" data-dnt="true" href="https://twitter.com/search?q=%23USACelections" data-widget-id="328749991814369281">Tweets about "#USACelections"</a>
 <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
+
 </div>
 
       </div><!--/row-->
