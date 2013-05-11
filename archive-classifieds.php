6b0e405cc62d25c8a189e579915b1e88a1628279
Byron Lutz <byronlutz@gmail.com>
2013-05-11 16:50:29 -0700

    Added rates and calendar to classifieds section

--- archive-classifieds.php
+++ archive-classifieds.php
@@ -13,10 +13,15 @@
 					<h4>Place a Classified Ad</h4>
 				</div><!-- end div.side-head -->
 				<div class="side-info">
+					<a href="http://adpro.media.ucla.edu/online" class="btn btn-inverse classifieds-button" target="_blank">Place an ad &raquo;</a>
+					<div class="btn-group classifieds-button-group">
+						<a href="http://dailybruin.com/images/ads/classifieds_rates.pdf" class="btn classifieds-button-small"><i class="icon-tasks"></i> Rates</a>
+						<a href="http://dailybruin.com/images/ads/classifieds_calendar.pdf" class="btn classifieds-button-small"><i class="icon-calendar"></i> Publication calendar</a>
+					</div><!-- end div.btn-group -->
 					<p>Daily Bruin classifieds are the best way to find qualified applicants for your job openings or rental vacancies. Your classified ad appears every day in the Daily Bruin and online and weekly in the Corsair, the campus newspaper at nearby Santa Monica City College.</p>
 					<p>Deadline for placing an ad is 12 noon, one business day before publication. Payment must be received before deadline. In order to receive the flat rate, the ad must be 20 words or fewer. Anything with a space before and after is counted as one word. We reserve the right to edit ads.</p>
 					<p>You can also order over the phone by calling <a href="tel:3108252221">310-825-2221</a>.</p>
-					<p><a href="http://adpro.media.ucla.edu/online">Place a classified ad</a></p>
+					<p><a href="http://adpro.media.ucla.edu/online" target="_blank">Click here to place a classified ad &raquo;</a></p>
 
 
 				</div><!-- end div.side-info -->
