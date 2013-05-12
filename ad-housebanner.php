03e04e38446af48eb37f984bfbc0b9a5cdf5848b
Byron Lutz <byronlutz@gmail.com>
2013-05-12 13:23:30 -0700

    Added FREE to the ad

--- ad-housebanner.php
+++ ad-housebanner.php
@@ -5,7 +5,7 @@
 					<div id="housead-grunge1"> </div><div id="housead-grunge2"> </div><div id="housead-grunge3"> </div>
 
 					<h4>Follow <span class="housead-extraword">UCLA</span> Bruin Football on the go</h4>
-					<span>Download the Bruin Football iPhone app &raquo;</span>
+					<span>Download the <strong>free</strong> Bruin Football iPhone app &raquo;</span>
 				</a><!-- div.housead-survey -->
 
  				</noscript>
