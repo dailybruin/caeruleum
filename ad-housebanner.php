84aaf3c85171a998e768205041bdb87a4ddf4edb
Byron Lutz <byronlutz@gmail.com>
2013-05-11 18:34:51 -0700

    Added url to the sports add banner

--- ad-housebanner.php
+++ ad-housebanner.php
@@ -1,7 +1,7 @@
 			<div class="visible-desktop" id="ad-house">
 				<noscript>
 				<?php // 468px by 60px ?>
-				<a href="#" class="housead" target="_blank">
+				<a href="/football-app" class="housead" target="_blank">
 					<div id="housead-grunge1"> </div><div id="housead-grunge2"> </div><div id="housead-grunge3"> </div>
 
 					<h4>Follow <span class="housead-extraword">UCLA</span> Bruin Football on the go</h4>
