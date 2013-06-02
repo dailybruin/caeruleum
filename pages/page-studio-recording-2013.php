5ff73886790c8c7437b016c317a4349b00d07f6f
Byron Lutz <byronlutz@gmail.com>
2013-06-02 15:21:46 -0700

    Added grooveshark widget

--- pages/page-studio-recording-2013.php
+++ pages/page-studio-recording-2013.php
@@ -11,6 +11,12 @@ Template Name: A Secret Kind of Art
     .studio-recording-page .video-explainer {
         margin-top:6px;
     }
+    .studio-recording-page #gsPlaylist8717217575 {
+        display:block;
+        margin-left:auto;
+        margin-right:auto;
+        max-width:100% !important;
+    }
 </style>
 <div class="row">
 	<div class="span8 studio-recording-page" id="post">
@@ -34,6 +40,10 @@ Template Name: A Secret Kind of Art
                 <iframe width="100%" height="240px" src="http://www.youtube.com/embed/kfVsfOSbJY0" frameborder="0" allowfullscreen></iframe>
                 <p class="video-explainer">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce congue, dolor et tempus pulvinar, nisi dolor dapibus velit, at pulvinar orci turpis eu nisl. Curabitur tincidunt leo vel risus elementum mollis. Nulla commodo, augue vel venenatis sollicitudin, tellus velit porta metus, quis bibendum lacus sem eu nisl.</p>
             </div><!-- end div.span4 -->
+            <div class="span8">
+                <h3>Listen to their songs</h3>
+                <p>Do you want an explainer here?</p>
+                <object width="100%" height="320" classid="clsid:D27CDB6E-AE6D-11cf-96B8-444553540000" id="gsPlaylist8717217531" name="gsPlaylist8717217531"><param name="movie" value="http://grooveshark.com/widget.swf" /><param name="wmode" value="window" /><param name="allowScriptAccess" value="always" /><param name="flashvars" value="hostname=grooveshark.com&playlistID=87172175&p=0&bbg=232323&bth=232323&pfg=232323&lfg=232323&bt=d1d5bc&pbg=d1d5bc&pfgh=d1d5bc&si=d1d5bc&lbg=d1d5bc&lfgh=d1d5bc&sb=d1d5bc&bfg=62ba45&pbgh=62ba45&lbgh=62ba45&sbh=62ba45" /><object type="application/x-shockwave-flash" data="http://grooveshark.com/widget.swf" width="100%" height="320"><param name="wmode" value="window" /><param name="allowScriptAccess" value="always" /><param name="flashvars" value="hostname=grooveshark.com&playlistID=87172175&p=0&bbg=232323&bth=232323&pfg=232323&lfg=232323&bt=d1d5bc&pbg=d1d5bc&pfgh=d1d5bc&si=d1d5bc&lbg=d1d5bc&lfgh=d1d5bc&sb=d1d5bc&bfg=62ba45&pbgh=62ba45&lbgh=62ba45&sbh=62ba45" /><span><a href="http://grooveshark.com/search/playlist?q=Introduction%20to%20Recording%20Engineering%20%E2%80%94%20Class%20projects%20sampler%202009-2012%20Daily%20Bruin%20Online" title="Introduction to Recording Engineering — Class projects sampler 2009-2012 by Daily Bruin Online on Grooveshark">Introduction to Recording Engineering — Class projects sampler 2009-2012 by Daily Bruin Online on Grooveshark</a></span></object></object>            </div><!-- end div.span8 -->
         </div><!-- end div.row -->
 
 	</div><!-- end div#post -->
