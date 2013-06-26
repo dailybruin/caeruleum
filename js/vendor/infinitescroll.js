0b0746d4087607918bc09a09a58149a61cd12c19
Yongjie Lim <yongjie@ubuntu.(none)>
2013-06-25 22:13:59 -0700

    load works, preloading problem fixed

--- js/vendor/infinitescroll.js
+++ js/vendor/infinitescroll.js
@@ -1,34 +1,41 @@
 $(window).load(function() {	
 	offset_inc = 1;
-	loaded = 0;
 	$(window).scroll(function() { 
 		var scrolltop = $(this).scrollTop();
 		var botBound = $("#banner-bottom").offset().top;
-		if (/*sbotBound - scrolltop > 600 &&*/ !loaded) {
-			var data = {
-				action: 'infinite_scroll',
-				offset: 4*(offset_inc),
-				numberposts: 8,
-			};
-			$.post(ajax_object.ajax_url, data, function(response) {
-				var image_urls = JSON.parse(response);
-				//alert(image_urls[0]);
-				var container = $("#howewo-container");
-			    column = $(".howewo-column");
-			    image_preload = new Array();
-			    image_text = new Array();
-				$.each( image_urls, function(key, value) {
-					var image = $('<img/>', {
-						"class": 'attachment-db-category-full wp-post-image',
-						"src": value[0],
-					}).load(
-						alert('hello!'));
-				    image_preload.push(image);
-				    image_text.push(value[1]);
+
+		var data = {
+			action: 'infinite_scroll',
+			offset: 4*(offset_inc),
+			numberposts: 8,
+		};
+		$.post(ajax_object.ajax_url, data, function(response) {
+			var image_urls = JSON.parse(response);
+			var container = $("#howewo-container");
+		    column = $(".howewo-column");
+			$.each( image_urls, function(key, value) {
+				var image = $('<img/>', {
+					"class": 'attachment-db-category-full wp-post-image',
+					"src": value[0],
+				}).load(function() {
+					
+
+					var smallest = column[0].offsetHeight;
+					var small_i = 0;
+					for (var p = 1; p < column.length; p++){
+						if (smallest > column[p].offsetHeight){
+							smallest = column[p].offsetHeight;
+							small_i = p;
+						}
+					}
+					var wrap = $('<div/>', {
+					    "class": 'howewo-img-wrap',
+					}).append(image,$('<div/>', { "text": value[1], }));
+					wrap.appendTo(column[small_i]);
 				});
 			});
-			loaded = 1;
-		}/*
+		});
+		/*
 		else if (botBound - scrolltop < 1500 && loaded) {
 			var images = image_preload;
 			for (var i = 0; i < images.length; i++){
