19f31a2b5e19d9c44445034a96c5a6d2a76fc4e6
Byron Lutz <byronlutz@gmail.com>
2013-07-27 16:23:56 -0700

    Removed display_name filter because it was conflicting with camayak. @omniaura @thecalvinchan be careful when creating new users in WP; not sure how it normally responds

--- inc/custom.php
+++ inc/custom.php
@@ -299,39 +299,6 @@ function new_excerpt_more($more) {
 }
 add_filter('excerpt_more', 'new_excerpt_more');
 
-// Use full names (substituted on login)
-	/*
-	Plugin Name: Substitute Displayname
-	Version: 0.1
-	Description: Substitutes a default Display name for new registrants.
-	Author: Mac McDonald
-	*/
-/* Version check */
-global $wp_version;
-$exit_msg='Substitute Author requires WordPress 2.5 or newer.  <a href="http://codex.wordpress.org/Upgrading_WordPress">Please update!</a>';
-if (version_compare($wp_version,"2.5","<")) {
-   exit ($exit_msg);
-}
-function sd_new_login_filter ($login) {
-/* Don't do anything to login, just see if already in database.*/
-   global $wpdb, $sd_is_new_login;
-
-   $id = $wpdb->get_var("SELECT ID FROM $wpdb->users WHERE user_login = '$login'");
-   $sd_is_new_login = (isset($id)) ? false : true;
-   return $login;
-}
-
-function sd_substitute_displayname_filter ($display_name) {
-   global $sd_is_new_login;
-
-   if ($sd_is_new_login) $display_name = $_POST['first_name']." ".$_POST['last_name'];
-   return $display_name;
-}
-add_filter('pre_user_login', 'sd_new_login_filter');
-add_filter('pre_user_display_name', 'sd_substitute_displayname_filter');
-
-
-
 // Add an RSS button to the end of the top menu
 function db_nav_menu_filter($items, $args) {
 	$feed_url = get_bloginfo('rss2_url');
