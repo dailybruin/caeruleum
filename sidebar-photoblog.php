63b2b0b5285e0b0420283c50a3379d32169421c3
omniaura <omniaura5@gmail.com>
2013-10-01 01:13:21 -0700

    fixed about spectrum link

--- sidebar-photoblog.php
+++ sidebar-photoblog.php
@@ -6,7 +6,7 @@
 			<hr />
 	        <?php wp_nav_menu( array( 'theme_location' => 'photoblog_sidebar' ) );?>
 	        <hr /> 
-	        <a id="sidebar-about-link" href="/spectrum-about"> <?php _e( 'About Spectrum', 'gryphondor' ); ?></a>
+	        <a id="sidebar-about-link" href="/about-spectrum"> <?php _e( 'About Spectrum', 'gryphondor' ); ?></a>
      	 </div><!--sidebar-menu-->
   </div>
 </aside>
