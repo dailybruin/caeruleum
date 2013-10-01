7b15cc348c449243c54785d068ae79576c86034e
omniaura <omniaura5@gmail.com>
2013-10-01 02:55:14 -0700

    added archive link to sidebar

--- sidebar-photoblog.php
+++ sidebar-photoblog.php
@@ -5,8 +5,9 @@
 		<div class="sidebar-menu hidden-phone">
 			<hr />
 	        <?php wp_nav_menu( array( 'theme_location' => 'photoblog_sidebar' ) );?>
-	        <hr /> 
-	        <a id="sidebar-about-link" href="/about-spectrum"> <?php _e( 'About Spectrum', 'gryphondor' ); ?></a>
+	        <hr />
+            <a class="sidebar-link" href="/category/photo-galleries">Archive</a> 
+	        <a class="sidebar-link" href="/about-spectrum"> <?php _e( 'About Spectrum', 'gryphondor' ); ?></a>
      	 </div><!--sidebar-menu-->
   </div>
 </aside>
