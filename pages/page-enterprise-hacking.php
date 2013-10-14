2a8495c635c6d3ead5eabe9b25cffef372303112
liddiard <omniaura5@gmail.com>
2013-10-13 22:59:17 -0700

    enterprise hacking flat page initial commit

---
+++ pages/page-enterprise-hacking.php
@@ -0,0 +1,138 @@
+<?php/*
+Template Name: Enterprise Hacking Feature
+*/ ?>
+<?php get_header(); ?>
+<script type="text/javascript">
+    $(document).ready(function(){
+        unresponsivize();
+    });
+</script>
+
+<style type="text/css">
+#eh-cover {
+    position: relative;
+    width: 100%;
+    height: 350px;
+    background-color: red;
+    color: white;
+    margin-bottom: 10px;
+}
+
+#eh-cover h1 {
+    position: absolute;
+    bottom: 5px;
+    right: 5px;
+    font-size: 5em;
+}
+
+.eh-header {
+    text-transform: none;
+    font-family: "Helvetica Neue", Helvetica, Arial, sans-serif;
+    color: #0043DE;
+}
+
+.eh-infobox {
+    width: 350px;
+}
+
+.eh-infobox-left {
+    float: left;
+    margin: 0 2em 2em 0;
+}
+
+.eh-infobox-right {
+    float: right;
+    margin: 0 0 2em 2em;
+}
+
+.eh-caption {
+    font-size: 0.8em;
+    font-family: "Helvetica Neue", Helvetica, Arial, sans-serif;
+    color: gray;
+}
+
+.eh-center {
+    text-align: center;
+}
+
+.eh-stat {
+    text-align: center;
+    font-size: 3em;
+    font-weight: light;
+    line-height: 1.1;
+}
+
+.eh-credit {
+    font-size: 0.8em;
+    font-family: "Helvetica Neue", Helvetica, Arial, sans-serif;
+    color: gray;
+    font-style: italic;
+}
+
+p {
+    font-family: "Helvetica Neue", Helvetica, Arial, sans-serif;
+}
+</style>
+
+<div id="eh-cover">
+    <h1>Here is a title.</h1>
+</div>
+<p>I am your worst nightmare! You bet I'm agitated! I may be surrounded by insanity, but I am not insane. Yesterday I did not know how to eat gagh. I'll be sure to note that in my log. Fate protects fools, little children and ships named Enterprise.</p> 
+<div class="eh-infobox eh-infobox-right">
+    <h3 class="eh-header">Response process to security breach at UCLA</h3>
+    <ol>
+        <li>Organization heads – the appropriate campus officials – assess the scope of the incident and decide whether an official breach has occurred.
+        <li>IT officials within the appropriate campus department or organization immediately neutralize threat or shut down infected site.
+        <li>Campus officials notify all persons whose personal information may have been affected or illegally obtained by an unauthorized source, either through hard copy or email.
+        <li>Officials do in-depth post-mortum analysis of breach, to analyze what can be done to prevent it in future.
+        <li>Any costs incurred by the university during the process are paid by the organization within which the breach originated.
+    </ol>
+    <p class="eh-caption">*If the breach is systemwide, follow measures outlined in UC Privacy and Data Security Incident Response Plan</p>
+    <p class="eh-credit">Source: UCLA Administrative Policy 420</p>
+</div>
+<p>Your shields were failing, sir. What? We're not at all alike! In all trust, there is the possibility for betrayal. Well, I'll say this for him - he's sure of himself. I'd like to think that I haven't changed those things, sir. Ensign Babyface! A lot of things can change in twelve years, Admiral. Maybe we better talk out here; the observation lounge has turned into a swamp. Well, that's certainly good to know. Wouldn't that bring about chaos? I'll alert the crew. Shields up! Rrrrred alert! Damage report!</p>
+<p>We have a saboteur aboard. Why don't we just give everybody a promotion and call it a night - 'Commander'? Some days you get the bear, and some days the bear gets you. Maybe if we felt any human loss as keenly as we feel one of those close to us, human history would be far less bloody. About four years. I got tired of hearing how young I looked. Flair is what marks the difference between artistry and mere competence. The unexpected is our normal routine.</p>
+<p>It's sort of like going from prime rib to&hellip; I don't know&hellip; weird brother of prime rib. You are a worse psychiatrist than you are a son-in-law and you will never get work as an actor because you have no talent. They frame my junk. Operation Hot Mother. No, no, it's pronounced a-nal-ra-pist. This show was cancelled. I mean, COME ON.</p>
+<p>I just haven't had sex in a month. You know, you've been here two months. It's hard to gauge time. That's how Tony Wonder lost a nut. If you don't start pulling your weight around here its going to be shape up, or&hellip;ship up. It just seems like there's still light coming in from under the door. No, it's the opposite. It's like my heart is getting hard. Oh, yeah. The guy in the $4,000 suit is holding the elevator for a guy who doesn't make that in three months. COME ON! Friday night. This show was cancelled. I mean, COME ON.</p>
+<div class="eh-infobox eh-infobox-left">
+    <h3 class="eh-header">Intellectual property at UCLA</h3>
+    <div class="eh-center">
+        <span class="eh-stat">852</span>
+        <p>Total number of active U.S. patents</p>
+        <span class="eh-stat">2,083</span>
+        <p>Total active inventions in portfolio</p>
+        <span class="eh-stat">406</span>
+        <p>Invention disclosures submitted by UCLA researchers during Fiscal Year 2013</p>
+    </div>
+    <p class="eh-credit">Source: UCLA Office of Intellectual Property and Industry Sponsored Research</p>
+</div>
+<p>There are dozens of us! DOZENS! She's always got to wedge herself in the middle of us so that she can control everything. Yeah. Mom's awesome. Mr. Zuckerkorn, you've been warned about touching.&nbsp;<em>Barry:</em>&nbsp;You said spanking. Oh, I don't have any drugs for sale, unless&hellip; did you want me to follow you to your car? What is she doing at a beauty pageant? Is she running the lights or something? Don't leave your Uncle T-bag hanging.</p>
+<p>Sh&eacute;male. I hear the jury's still out on science. Te quiero. English, please. I love you! Great, now I'm late. So maybe you could start jete-ing, and stop je-terrorizing me! Daddy horny, Michael. The Man Inside Me seems well reviewed.</p>
+<p>What's next, Michael? Are you going to make dancing illegal? Is this the tiny town from Footloose? We need a name. Maybe 'Operation Hot Mother'. No, Pop-pop does not get a treat. I just bought you a f**king pizza.</p>
+<p>It looks like you've been looking for dragons&hellip; in the future. We need a name. Maybe "Operation Hot Mother." No, let's try to top that. (They never did.) Got a big ass room at the travelodge. What a fun, sexy time for you. And although the intervention didn't work, it turned into one of the Bluth family's better parties. Steve Holt? The moron jock?</p>
+<div class="eh-infobox eh-infobox-right">
+    <h3 class="eh-header">UCLA IT Security Office's Tips for Password Security</h3>
+    <ul>
+        <li> Never give your password to anybody or write it down in a visible place. Password sharing is also a violation of the Bruin OnLine Acceptable Use Policy, and can make you lose your account.
+        <li>Change your password at least once every three to six months, or immediately after using a publicly-accessible computer.
+        <li>Don't choose a password that can be found in your username or a dictionary in any language, whether spelled forward or backward. Also, don't use personal information (especially when this information is available on social networking profiles). For example - names of family, places, pets, birthdays, address information, hobbies, etc.
+        <li>Choose a long and complex password. Passwords should use a mix of capital and non-capital letters, numbers, and special characters. You should substitute numbers or symbols for letters. For example, "@" for "a."
+        <li>Don't choose a password with letters or numbers that appear on successive keys on the keyboard (like qwerty or 123).
+    </ul>
+    <p class="eh-credit">Source: UCLA Office of Information Technology Security.</p>
+</div>
+<p>What's gotten into you? Have you been eating cheese? Hahahahah! However, she mistook the drowsy eye alcohol warning for a winking eye alcohol suggestion. It's a jetpack, Michael. What could possibly go wrong? Bob Loblaw Law Blog. She keeps saying that God is going to show me a sign. The&hellip; something of my ways. Wisdom? It's probably wisdom.</p>
+<p>Michael, I'm your older brother. I'll never be proud of you. So you take your mom to work every day? Bummer. Moms are such a pain in the ass, huh? It's, like, die already!</p>
+<p>He's a regular Freddie Wilson, that one. Ann certainly has a great deal of Mass. I spent so much time making sweet love on my wife that it's hard to hear anything over the clatter of her breasts. Sorry, some of my students are arguing the significance of the shankbone on the seder plate. But we do not - NOT wag our genitals at one another to make a point.</p>
+<p>No, no, it's pronounced a-nal-ra-pist. Her lawyers are claiming the seal is worth $250,000. And that's not even including Buster's Swatch.</p>
+<p>I'm sure, wherever your father is, she loves you very much. A night of heterosexual intercourse.</p>
+<p>Am I in two thirds of a hospital room? This is the best free scrapbooking class I've ever taken! So did you see the new Poof? His name's Gary and we don't need anymore lawsuits. How do you know Steve Holt? Are you in AA? Ah coodle doodle doo, ah coodle doodle doo. Stack the chafing dishes outside by the mailbox. I'm on the job. Why are you squeezing me with your body?</p>
+<p>Well, yeah you've gotta lock that down. Suddenly playing with yourself is a scholarly pursuit? This is not me encouraging you to go here and write a review. No. Oh, yes, there absolutely will be a margarita made in my mouth.</p>
+<p>Oh, I can just taste those meaty leading man parts in my mouth. Hey, look at that &ndash; you're mean sober, too. And I wouldn't just lie there, if that's what you're thinking. That's not what I WAS thinking. NO TOUCHING!</p>
+<div class="eh-infobox eh-infobox-right">
+    <h3 class="eh-header">Did You Know?</h3>
+    UCLA network users can now have passwords as long as 128 characters. UCLA made the change to increase its maximum password length, so you can even choose a sentence, such as one from your favorite book.
+</div>
+<p>They're not gonna strip, right? I told them not to, but I can't guarantee their instincts won't kick in. Everybody dance NOW. What have we always said is the most important thing? Talk you off what, Pop Pop? It just seems like there's still light coming in from under the door. A million [bleep]ing diamonds! Let's see some bananas and nuts!</p>
+<p>Did Ted make an appointment? No. Well then Ted can GET THE HELL OUT OF THIS OFFICE! YOU GET THE HELL OUT! Did you enjoy your lunch, mom? You drank it fast enough. The Man Inside Me seems well reviewed. Even though so many people in this office are begging for it.</p>
+<?php get_footer(); ?>
