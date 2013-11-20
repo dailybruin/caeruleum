b1ff89feadad9a9b64a24da4f148670b2b87a371
Daily Bruin Intern <dailybruin@dailybruin.(none)>
2013-11-19 17:06:12 -0800

    added nested.css, and added boxes

--- pages/page-usc-football-rivalry-2013.php
+++ pages/page-usc-football-rivalry-2013.php
@@ -2,7 +2,17 @@
 Template Name: USC Football Rivalry 2013
 */ ?>
 <?php get_header(); ?>
-<h1>
-Hello, world.
-</h1>
+<script src="../js/jquery.nested.js"></script>
+<link href="../css/nested.css" rel="stylesheet" type="text/css" />
+<div id="big-box">
+    <div class="box size21">1</div>
+    <div class="box size22">2</div>
+    <div class="box size34">3</div>
+</div>
+<script>
+$("#big-box").nested({
+    minWidth: 300,
+    gutter: 10
+});
+</script>
 <?php get_footer(); ?>
