a2fbbfc123541e58f1427b9a5ec03b873580c5ca
Aein <aein.hope@gmail.com>
2014-02-17 03:15:42 -0800

    inline photos + nav bar placeholder

--- pages/page-sports-gymnastics-day1.php
+++ pages/page-sports-gymnastics-day1.php
@@ -10,8 +10,20 @@ Template Name: Sports Gymnastics day1
 <!--css -->
 <article id="sp-gym">
 	<img src="http://dailybruin.com/images/2013/04/web.ns_.4.22.gymnastics.wrap_.picA_.jpg">
+	<span class="photocred">Erin Ng / Daily Bruin Senior Staff</span><br><br><br>
+	<div class="navbar">
+  <div class="navbar-inner">
+    <ul class="nav"><li class="divider-vertical"></li>
+      <li class="active"><a href="#">Gymnastics Day 1</a></li><li class="divider-vertical"></li>
+      <li class="disabled"><a href="#">Gymnastics Day 2</a></li><li class="divider-vertical"></li>
+      <li class="disabled"><a href="#">Gymnastics Day 3</a></li><li class="divider-vertical"></li>
+      <li class="disabled"><a href="#">Gymnastics Day 4</a></li><li class="divider-vertical"></li>
+    </ul>
+  </div>
+</div>
+
 <h1>Gymnastics Day 1</h1>
-	<p>
+	<strong>BY SOMEBODY SPECIAL</strong><br><br><p>
 Redshirt junior Samantha Peszek swung around the bar once, twice, three times and landed firmly on the mat, solidifying a perfect routine. The judges rewarded her perfection with a perfect score of 10.<br><br>
 
 One flawless performance on uneven bars may have been enough to help the UCLA women’s gymnastics team overtake the Arizona State Sun Devils 196.925-194.950 in Pauley Pavilion on Saturday, but the meet was riddled with errors and shaky performances.<br><br>
@@ -46,7 +58,7 @@ Peszek’s perfect ten was followed by another nearly flawless beam routine, whe
 Though Peszek delivered solid performances to help secure a win, the team’s errors during the meet have revealed mental weaknesses. The team still has to readjust perspective if it wants to finish the season with a title.<br><br>
 
 “They talk about being champions, and they need to believe that they are champions in order to compete like it because you can see the people that believe it – Olivia (Courtney), Syd(ney Sawa), Sam Peszek – they believe they’re champions. Danusia (Francis) believes she’s a champion,” Kondos Field said. “The other ones talk it, but they’re not walking it.”<br><br> 
-Craddock redeemed herself with a solid starting performance on beam, adding more than a point to her bars routine score with a 9.825.<br><br>   <div class="thumbnail with-caption"><img src="http://dailybruin.com/images/2013/09/b4fa2619-41ff-4429-8704-143b536f7b92-225x300.jpg" width="225"><span class="photocred-inline"><br>Courtesy of Naz Papen</span></div>
+Craddock redeemed herself with a solid starting performance on beam, adding more than a point to her bars routine score with a 9.825.<br><br>   <div class="thumbnail with-caption"><img src="http://dailybruin.com/images/2014/02/a025fd6c-8bde-488c-a8ea-a388340f570a-218x300.jpg" width="225"><span class="photocred-inline">Courtesy of Naz Papen</span></div>
 Craddock’s performance followed Peszek’s flawless bars routine.<br><br>
 
 Peszek has been resting an Achilles injury and thus has not been competing as often as she typically does during the season.<br><br>
@@ -57,14 +69,32 @@ She said that she has never received a ten before and is encouraged to know that
 
 Kondos Field praised Peszek’s work ethic, and mentioned her willingness to ruthlessly attack her weaknesses. Kondos Field added that Peszek has earned the right to be as confident as she is.<br><br>
 
-Peszek’s perfect ten was followed by another nearly flawless beam routine, where she scored a 9.95.<br><br>
+The team still has to readjust perspective if it wants to finish the season with a title.Peszek’s perfect ten was followed by another nearly flawless beam routine, where she scored a 9.95.<br><br>
 
 Though Peszek delivered solid performances to help secure a win, the team’s errors during the meet have revealed mental weaknesses. The team still has to readjust perspective if it wants to finish the season with a title.<br><br>
 
 “They talk about being champions, and they need to believe that they are champions in order to compete like it because you can see the people that believe it – Olivia (Courtney), Syd(ney Sawa), Sam Peszek – they believe they’re champions. Danusia (Francis) believes she’s a champion,” Kondos Field said. “The other ones talk it, but they’re not walking it.”<br><br> 
 
+Redshirt junior Samantha Peszek swung around the bar once, twice, three times and landed firmly on the mat, solidifying a perfect routine. The judges rewarded her perfection with a perfect score of 10.<br><br>
 
+One flawless performance on uneven bars may have been enough to help the UCLA women’s gymnastics team overtake the Arizona State Sun Devils 196.925-194.950 in Pauley Pavilion on Saturday, but the meet was riddled with errors and shaky performances.<br><br>
 
+“We had ups and downs … which is okay for the first or second meet, but not for the … fifth meet of the year,” said coach Valorie Kondos Field. “The mistakes that we made were not okay, simply not okay. They need to start living what they talk.”<br><br>
+
+While the vault performances in the first rotation were fairly routine, the No. 9 Bruins exhibited errors in every event afterward.<br><br>
+
+Redshirt sophomore Ellette Craddock led the second rotation with two falls on bars. She said she felt calm before the event, but ended up losing her grip on a turn, resulting in a score of 8.575.<br><br>
+
+Craddock added that her miscues on bars allowed her to gain clarity as to how she should approach the event in the future.<br><br>
+“I have a lot of experience, so to know that I can do it even when I haven’t been training gives me a lot of confidence and hopefully gives the team a lot of confidence that I can do my job no matter what the circumstances are,” Peszek said.<br><br>
+
+Kondos Field praised Peszek’s work ethic, and mentioned her willingness to ruthlessly attack her weaknesses. Kondos Field added that Peszek has earned the right to be as confident as she is.<br><br>
+
+Peszek’s perfect ten was followed by another nearly flawless beam routine, where she scored a 9.95.<br><br>
+
+Though Peszek delivered solid performances to help secure a win, the team’s errors during the meet have revealed mental weaknesses. The team still has to readjust perspective if it wants to finish the season with a title.<br><br>
+
+“They talk about being champions, and they need to believe that they are champions in order to compete like it because you can see the people that believe it – Olivia (Courtney), Syd(ney Sawa), Sam Peszek – they believe they’re champions. Danusia (Francis) believes she’s a champion,” Kondos Field said. “The other ones talk it, but they’re not walking it.”<br><br> 
 </p>
 </article> 
 
