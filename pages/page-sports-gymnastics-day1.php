0e881c3938a9bba50bf560c672378c9c7ad45b05
Christina Yang <christinay412@gmail.com>
2014-02-17 20:35:19 -0800

    added the article

--- pages/page-sports-gymnastics-day1.php
+++ pages/page-sports-gymnastics-day1.php
@@ -22,17 +22,26 @@ Template Name: Sports Gymnastics day1
 
 <h1>Samantha Peszek</h1>
 	<strong>BY SOMEBODY SPECIAL</strong><br><br><p>
-Redshirt junior Samantha Peszek swung around the bar once, twice, three times and landed firmly on the mat, solidifying a perfect routine. The judges rewarded her perfection with a perfect score of 10.<br><br>
+<p>As her feet pressed into the mat below her, Samantha Peszek took just a small stutter stepbackward. </p>
 
-One flawless performance on uneven bars may have been enough to help the UCLA women’s gymnastics team overtake the Arizona State Sun Devils 196.925-194.950 in Pauley Pavilion on Saturday, but the meet was riddled with errors and shaky performances.<br><br>
+<p>For a short moment, she raised her arms, signaling the end of her routine on uneven bars and somberly forced her eyes toward the Olympic judges. </p>
 
-“We had ups and downs … which is okay for the first or second meet, but not for the … fifth meet of the year,” said coach Valorie Kondos Field. “The mistakes that we made were not okay, simply not okay. They need to start living what they talk.”<br><br>
+<p>Then her eyes flicked down, her shoulders slumped and she quickly stepped away from the pad under her feet.</p>
 
-While the vault performances in the first rotation were fairly routine, the No. 9 Bruins exhibited errors in every event afterward.<br><br>
+<p>It was only after one or two limp-free steps away from the apparatus that Peszek remembered what her coaches had told her after she injured her foot in warm-ups minutes ago: You're not allowed to cry. You're not allowed to talk about it. You’re not allowed to be upset.</p>
 
-Redshirt sophomore Ellette Craddock led the second rotation with two falls on bars. She said she felt calm before the event, but ended up losing her grip on a turn, resulting in a score of 8.575.<br><br>
+<div class="thumbnail with-caption-left"><img src="http://dailybruin.com/images/2014/02/web.peszek.picB_-300x190.jpg" width="300"><span class="photocred-inline">Katie Meyers / Daily Bruin</span></div>
 
-Craddock added that her miscues on bars allowed her to gain clarity as to how she should approach the event in the future.
+<p>So the then-16-year-old hid her look of pain, put a smile on her face and walked away from her first – and what turned out to be only – event in the 2008 Beijing Olympics.</p>
+
+<p>Even though the now-redshirt-junior was originally slated to represent the United States in all four gymnastics events, she showed off her ability to compete through less-than-ideal circumstances and hit the one routine Team USA needed her to hit – a skill she frequently displays as a gymnast at UCLA.</p>
+
+<p>“I asked if I could do the other events and the (coach) was like ‘No, we really just need you to hit (an uneven) bar routine,’” she said. "‘That’s your one job.’”</p>
+
+<p>While the women’s gymnastics team, headlined by household names Nastia Liukin and Shawn Johnson, went on to win a silver medal, Peszek later discovered she tore every ligament in her foot and was unable to compete after the preliminary round.</p>
+
+<p>And if the mental turmoil of being held from competition in Beijing wasn’t enough, Peszek was told to hide her despair for the sake of the other girls on the team.</p>
+
 
 <div class="pq-wrap pq-style-2"><div class="pq-thumb"></div>
     <blockquote style-"pq-style-2">
@@ -40,67 +49,45 @@ Craddock added that her miscues on bars allowed her to gain clarity as to how sh
     </blockquote>
 </div>
 
- “I went really big, and I didn’t catch my first release,” Craddock said. “And then, just like getting the flow back, I mean I should’ve been able to do that … since I do that all the time in workout.”<br><br>
+<p>“I just had to switch gears really quick and do everything in my power to help (the team) compete well,” she said. “If I would've been distraught and a mess about it, that wouldn’t have really been helping.”</p>
 
-Craddock redeemed herself with a solid starting performance on beam, adding more than a point to her bars routine score with a 9.825.<br><br>
+<p>This wasn’t the first time Peszek masked her emotions for the sake of her Olympic ambitions. At the Olympic trials a month earlier, the pressure mounted to a level even higher than what Peszek said she later felt in Beijing.</p>
 
-Craddock’s performance followed Peszek’s flawless bars routine. Peszek has been resting an Achilles injury and thus has not been competing as often as she typically does during the season.<br><br>		“I went really big, and I didn’t catch my first release,” Craddock said. “And then, just like getting the flow back, I mean I should’ve been able to do that … since I do that all the time in workout.”<br><br>
+<p>After struggling on a few earlier routines, Peszek was told that the last one of the day would be do-or-die – hit it and she would make the team, miss it and she was done.</p>
 
-She said that she has never received a ten before and is encouraged to know that she can still perform despite the stall in her training.<br><br><div class="thumbnail with-caption-left"><img src="http://dailybruin.com/images/2014/02/web.peszek.picB_-300x190.jpg" width="300"><span class="photocred-inline">Katie Meyers / Daily Bruin</span></div>
+<p>Despite the heavy pressure, Peszek pulled off the routine to ultimately make Team USA.</p>
 
-“I have a lot of experience, so to know that I can do it even when I haven’t been training gives me a lot of confidence and hopefully gives the team a lot of confidence that I can do my job no matter what the circumstances are,” Peszek said.<br><br>
+<div class="thumbnail with-caption-left"><img src="http://dailybruin.com/images/2014/02/web.peszek.picC_-200x300.jpg" width="200"><span class="photocred-inline">Katie Meyers / Daily Bruin</span></div>
 
-Kondos Field praised Peszek’s work ethic, and mentioned her willingness to ruthlessly attack her weaknesses. Kondos Field added that Peszek has earned the right to be as confident as she is.<br><br>
+<p>That Peszek was able to first block heavy pressure to reach the Olympics  and then shut out the disappointment of being sidelined in Beijing speaks to her tremendous mental strength, said Tara Scanlan, a UCLA sports psychology professor.</p>
 
-Peszek’s perfect ten was followed by another nearly flawless beam routine, where she scored a 9.95.<br><br>
+<p>Scanlan pointed out that this mental focus is especially important – and especially difficult – in a sport like gymnastics. While a shooting guard in basketball or pitcher in baseball can take a moment to recuperate after a missed shot or an off-target pitch, a gymnast gets no break during a routine – once it has started, then it must finish.</p>
 
-Though Peszek delivered solid performances to help secure a win, the team’s errors during the meet have revealed mental weaknesses. The team still has to readjust perspective if it wants to finish the season with a title.<br><br>
+<p>“When you’re on the beam … if you splatter, there’s no regroup time – and you have to do it with a smile. You have to bounce right back up, let that mistake go, smile, focus and keep going," Scanlan said.</p>
 
-“They talk about being champions, and they need to believe that they are champions in order to compete like it because you can see the people that believe it – Olivia (Courtney), Syd(ney Sawa), Sam Peszek – they believe they’re champions. Danusia (Francis) believes she’s a champion,” Kondos Field said. “The other ones talk it, but they’re not walking it.”<br><br> 
-Craddock redeemed herself with a solid starting performance on beam, adding more than a point to her bars routine score with a 9.825.<br><br>   
-Craddock’s performance followed Peszek’s flawless bars routine.<br><br>
-
-Peszek has been resting an Achilles injury and thus has not been competing as often as she typically does during the season.<br><br>
-
-She said that she has never received a ten before and is encouraged to know that she can still perform despite the stall in her training.<br><br><div class="thumbnail with-caption-right"><img src="http://dailybruin.com/images/2014/02/web.peszek.picC_-200x300.jpg" width="200"><span class="photocred-inline">Katie Meyers / Daily Bruin</span></div>
-
-“I have a lot of experience, so to know that I can do it even when I haven’t been training gives me a lot of confidence and hopefully gives the team a lot of confidence that I can do my job no matter what the circumstances are,” Peszek said.<br><br>
-
-Kondos Field praised Peszek’s work ethic, and mentioned her willingness to ruthlessly attack her weaknesses. Kondos Field added that Peszek has earned the right to be as confident as she is.<br><br>
-
-The team still has to readjust perspective if it wants to finish the season with a title.Peszek’s perfect ten was followed by another nearly flawless beam routine, where she scored a 9.95.<br><br>
-
-Though Peszek delivered solid performances to help secure a win, the team’s errors during the meet have revealed mental weaknesses. The team still has to readjust perspective if it wants to finish the season with a title.<br><br>
-
-“They talk about being champions, and they need to believe that they are champions in order to compete like it because you can see the people that believe it – Olivia (Courtney), Syd(ney Sawa), Sam Peszek – they believe they’re champions. Danusia (Francis) believes she’s a champion,” Kondos Field said. “The other ones talk it, but they’re not walking it.”<br><br> 
-
-Redshirt junior Samantha Peszek swung around the bar once, twice, three times and landed firmly on the mat, solidifying a perfect routine. The judges rewarded her perfection with a perfect score of 10.<br><br>
-
-One flawless performance on uneven bars may have been enough to help the UCLA women’s gymnastics team overtake the Arizona State Sun Devils 196.925-194.950 in Pauley Pavilion on Saturday, but the meet was riddled with errors and shaky performances.<br><br><div class="thumbnail with-caption-left"><img src="http://dailybruin.com/images/2014/02/a2f93911-c786-4561-8188-74b8f5f4691e-200x300.jpg" width="300"><span class="photocred-inline">Samantha and Bela Karolyi.
+<div class="thumbnail with-caption-right"><img src="http://dailybruin.com/images/2014/02/a2f93911-c786-4561-8188-74b8f5f4691e-200x300.jpg" width="200"><span class="photocred-inline">Samantha and Bela Karolyi.
 <br>
 (Courtesy of Samantha Peszek)</span></div>
 
-“We had ups and downs … which is okay for the first or second meet, but not for the … fifth meet of the year,” said coach Valorie Kondos Field. “The mistakes that we made were not okay, simply not okay. They need to start living what they talk.”<br><br>
+<p>And mental recovery becomes that much more important in a perfection-oriented sport like gymnastics, where wobblylandings or spread toes often translate into large point deductions.</p>
 
-While the vault performances in the first rotation were fairly routine, the No. 9 Bruins exhibited errors in every event afterward.<br><br>
+<p>Years of performing under immense pressure mean that most Olympic athletes tend to have more confidence than their less-tested equivalents, said UCLA coach Valorie Kondos Field.</p>
 
-Redshirt sophomore Ellette Craddock led the second rotation with two falls on bars. She said she felt calm before the event, but ended up losing her grip on a turn, resulting in a score of 8.575.<br><br>
+<p>Peszek's ability to remain unfazed despite less-than-ideal conditions was on display in UCLA gymnastics’ most recent home meet on Feb. 8. She scored a career-first perfect 10 on uneven bars – coincidentally her least favorite event and the only one in which she was able to compete in Beijing – in only her fifth routine this year. 
+</p>
 
-Craddock added that her miscues on bars allowed her to gain clarity as to how she should approach the event in the future.<br><br><div class="pq-wrap pq-style-2"><div class="pq-thumb"></div>
+<p>And although she missed two of the season's first four meets and sat out the entire 2013 season with an Achilles tear, Peszek ranks first nationally on beam and third on uneven bars.</p>
+
+<p>But just as she has shown before, things that might rattle any athlete – like a serious injury or competing sparingly – generally do not affect Peszek's routines.</p>
+
+<p>After all, from a short-lived Olympic appearance to her UCLA gymnastics career, she's come through time and time again.</p>
+
+
+<div class="pq-wrap pq-style-2"><div class="pq-thumb"></div>
     <blockquote style-"pq-style-2">
         <p>I have a lot of experience, so to know that I can do it even when I haven’t been training gives me a lot of confidence</p>
     </blockquote>
-</div>
-“I have a lot of experience, so to know that I can do it even when I haven’t been training gives me a lot of confidence and hopefully gives the team a lot of confidence that I can do my job no matter what the circumstances are,” Peszek said.<br><br>
-
-
-Kondos Field praised Peszek’s work ethic, and mentioned her willingness to ruthlessly attack her weaknesses. Kondos Field added that Peszek has earned the right to be as confident as she is.<br><br>
-
-Peszek’s perfect ten was followed by another nearly flawless beam routine, where she scored a 9.95.<br><br>
-
-Though Peszek delivered solid performances to help secure a win, the team’s errors during the meet have revealed mental weaknesses. The team still has to readjust perspective if it wants to finish the season with a title.<br><br>
-
-“They talk about being champions, and they need to believe that they are champions in order to compete like it because you can see the people that believe it – Olivia (Courtney), Syd(ney Sawa), Sam Peszek – they believe they’re champions. Danusia (Francis) believes she’s a champion,” Kondos Field said. “The other ones talk it, but they’re not walking it.”<br><br> 
+</div> 
 </p>
 <ul class="pager">
   <li class="previous disabled">
