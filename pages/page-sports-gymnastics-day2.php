41a256c13c56e2abd98d4722efe7c91f14b71373
Aein <aein.hope@gmail.com>
2014-02-18 18:37:35 -0800

    day 2 jennifer

--- pages/page-sports-gymnastics-day2.php
+++ pages/page-sports-gymnastics-day2.php
@@ -3,13 +3,126 @@ Template Name: Sports Gymnastics day2
 */ ?>
 <?php get_header(); ?>
 <link rel="stylesheet" type="text/css" href="/css/sports-gymnastics.css">
+<link href="//fonts.googleapis.com/css?family=Raleway:100" rel="stylesheet" type="text/css">
+<link href="//fonts.googleapis.com/css?family=Droid+Serif" rel="stylesheet" type="text/css">
+<link href="//fonts.googleapis.com/css?family=Droid+Sans" rel="stylesheet" type="text/css">
+
 <!--css -->
 <article id="sp-gym">
+  <h1>Gymnastics:<br> The Competitive Demands</h1>
+  <div class="pagination pagination-centered">
+  <ul>
+    <li><a href="http://dailybruin.com/features/gymnastics-the-competitive-demands-samantha-peszek/">Samantha Peszek</a></li>
+    <li class="active"><a>Jennifer Pinches</a></li>
+    <li class="disabled"><a>Jordyn Wieber</a></li>
+
+    <li class="disabled"><a>Danusia Francis</a></li>
+  </ul>
+</div>
+
+	<img src="http://dailybruin.com/images/2014/02/web.pinches.picA_.jpg" class="aligncenter">
+	<span class="photocred">Katie Meyers / Daily Bruin senior staff</span><br><p style="text-align:center;">After competing in the London 2012 Summer Olympics, now-UCLA freshman Jennifer Pinches decided to move on from gymnastics.<p>
+
+<h2>Jennifer Pinches</h2>
+	<p><strong>BY ZACHARY LEMOS</strong></p>
+
+	<p>The day after representing Great Britain in the Olympics, Jennifer Pinches was done with gymnastics.</p>
+   <div class="pq-wrap-2 pq-style-2"><div class="pq-thumb"></div>
+       <blockquote style-"pq-style-2">
+           <p>"I was just happy with what I'd achieved."</p>
+       </blockquote><div class="pq-attribution">
+            <p class="pq-author"> – Jennifer Pinches,</p>
+            <cite>on her initial retirement</cite>
+          </div></div>
+
+<p>Despite enjoying a storybook career, complete with a documentary centered on her life as an 11-year-old gymnast, multiple national championships and a trip to the London 2012 Summer Olympics, the British native decided to move on.</p>
+
+<p>In fact, immediately following the Olympic parade, Pinches used her newfound freedom to experience things she had sacrificed for gymnastics, starting with a two-month volunteering trip in Ecuador.</p>
+
+<div class="thumbnail with-caption-left"><img src="http://dailybruin.com/images/2014/02/web.pinches.picB_-738x1024.jpg" class="img-vert">Pinches was convinced to return to gymnastics<br> after receiving an email from UCLA coach Valorie<br> Kondos Field asking Pinches to join the team.<br><br><span class="photocred-inline">Katie Meyers / Daily Bruin</span></div>
+
+<p>"I thought, ‘No way, I'm never competing again,’” she said. “I was done with the pressure, and after all that – leading up to the Olympics for like a year before, that massive buildup – I was ready to relax.”</p>
+
+<p>But two years after that Olympic buildup, Pinches is competing again. After a sudden and  unexpected recruiting process that dates back to last February, the now-UCLA freshman is becoming an increasingly important member of the Bruins' gymnastics team.  </p>
+
+<p>The British native’s retirement got its first real test on a family ski trip in Austria about a year ago.</p>
+
+<p>Around a half a year from the last day Pinches had competed – or even trained – for gymnastics, she received an email from UCLA coach Valorie Kondos Field. </p>
+
+<p>The email was simple enough: It basically said, "Come compete for me."</p>
+
+<p>“We needed a vaulter, so I sent out my feelers,” Kondos Field said.</p>
+
+<p>After the Olympics, Pinches decided to leaveher sport on a high note – a high that was elevatedafter scoring personal bests on vault and floor in the Olympic teamfinals,what she thought would be her last competition.</p>
+
+<p>“I thought, ‘It couldn't really get much better than that,’” she said. "I didn’t think any competition could possibly match the Olympic games in your home country. ... I’d achieved my goal, and I was ready for new challenges.”</p>
+
+<p>But then, out of nowhere, Pinches was offered an athletic scholarship, a chance to attend school in Los Angeles and the opportunity to return to her sport.</p>
+
+<p>Ultimately, it was because she couldn’t find a reason not to come to UCLA that Pinches ended up taking Kondos Field up on her offer.</p>
+
+<div class="pq-wrap-1 pq-style-1"><div class="pq-thumb"></div>
+       <blockquote style-"pq-style-1">
+           <p>"It was a really hard decision because I hadn't even thought about it. ... I had to consider what it was like, was it going to work, would I like it, do I really think I could get fit and do gymnastics again?"</p>
+       </blockquote><div class="pq-attribution">
+            <p class="pq-author"> – Jennifer Pinches,</p>
+            <cite>on whether to return to gymnastics</cite>
+          </div></div>
+
+<p>“The last thing that I was expecting was to get an email about a genuine possibility of doing gymnastics again after I’d been off for so long,” Pinches said. “I was just like, ‘Why not? I've had a break, L.A. sounds fun, I probably could still do it, so let’s give it a go.'”</p>
+
+<p>But coming to UCLA meant adding another sacrifice – being separated from her family and longtime boyfriend by more than just a train ride– to an already-long list.</p>
+
+<p>However, being away from her loved ones wasn’t new to Pinches; in the years leading up to the Olympics, she was frequently away from home for international competitions. Even when she wasn’t abroad, what she was able to fit into her schedule was heavily restricted because of training.</p>
+
+<p>In fact, the ski trip where Pinches received her first contact from Kondos Field was the first such trip Pinches had been able to attend. Previously, she had been forced to skip her family's annual visit to Austria because she couldn't afford to miss gymnastics training and competitions.</p>
+
+<div class="thumbnail with-caption-left"><img src="http://dailybruin.com/images/2014/02/web.pinches.picC_-690x1024.jpg" class="img-vert">While Pinches has returned to gymnastics<br> as a UCLA student, she may return home<br> to England next year.<br><br><span class="photocred-inline">Katie Meyers / Daily Bruin</span></div>
+
+<p>Though she eventually did choose to once again devote herself to her sport, she’s uncertain how long that newfound devotion will last – Pinches may choose to finish her studies in England starting next year.</p>
+
+<p>Keeping in mind that Pinches originally planned to retire after the 2012 Olympics, UCLA sports psychology professor Tara Scanlan said the fact that the freshman is considering another firm end point to her gymnastics career isn't atypical.</p>
+
+<p>Many athletes who make major sacrifices for their sport rationalize their missed opportunities because they are only able to compete for a finite period of time, Scanlan said.</p>
+
+<p>This end point is particularly apparent in a sport like gymnastics, where athletes generally peak before they legally become adults and even the most devoted athletes often have nowhere left to compete after the NCAA. </p>
+
+<p>Pinches originally set the 2012 Olympics – or at the very least making the 2012 Olympic trials – as the finale of her life as a gymnast. And even when the duration of that retirement got thrown into question by Kondos Field’s email, Pinches still kept an end point in mind.  </p>
+
+<p>At most, she’s back in the sport for four seasons, retiring again after the 2017 season, provided she doesn't redshirt because of injury. But Pinches said it's just as likely that after this season, she will retire for the second – and likely final – time.</p>
+
+<p>“The reason I came here was to test myself a little bit, to see if I could come back after all that time,” she said. “I'd be happy coming here for a year and then completing my degree in England because I feel like I would get the best of both worlds – still get my degree at home, but I would still be able to come to this amazing place, this amazing university and meet all these amazing people and find my love for the sport again.”</p>
+
+<div class="pq-wrap-1 pq-style-1"><div class="pq-thumb"></div>
+       <blockquote style-"pq-style-1">
+           <p>“I just wanted to experience what it was like here and what it's like being in another culture and country and just challenging myself to come back and compete in a different kind of environment.”</p>
+       </blockquote><div class="pq-attribution">
+            <p class="pq-author"> – Jennifer Pinches,</p>
+            <cite>on her decision to come to UCLA for gymnastics</cite>
+          </div></div>
+
+<p>While Pinches may be comfortable leaving after a year, Kondos Field said she is doing all that she can to keep the British gymnast at UCLA for all four years. </p>
+
+<p>Though Pinches developed some rust after almost two years off, Kondos Field said her learning curve is staggeringly steep.</p>
+
+<p>“The first time that she attempts a skill she’s been able to do before, she’s a little apprehensive,” she said. “By the second, third, fourth time she does it, that muscle memory kicks in, and she’s like, ‘Got that.’”</p>
+
+<p>Over the course of the 2014 season, Pinches has been steadily integrated into more and more events for the Bruins as she gets back her gymnastics fitness, competing on floor for the first time last Sunday. </p>
+
+<p>And though the once-Olympian has returned to making sacrifices for her sport, she has also rediscovered her skill and love for gymnastics, something she never thought would happen after her Olympic finale.  </p>
 
 
+<ul class="pager">
+  <li class="previous">
+    <a href="http://dailybruin.com/features/gymnastics-the-competitive-demands-samantha-peszek/">&larr; Previous</a>
+  </li>
+  <li class="next disabled">
+    <a>Next &rarr;</a>
+  </li>
+</ul>
+</p>
 
-
-
+<div id="page-credit">Page created by <a href="http://aeiny.com">Aein Hope</a> and <a href="mailto:cyang@media.ucla.edu">Christina Yang</a></div>
 
 </article> 
 
