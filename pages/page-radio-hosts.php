e541c50d495b4cdb9f891c0133a200068d0d6334
Alisha Rosenwein-Noss <arosenweinnoss@media.ucla.edu>
2014-04-07 17:34:43 -0700

    added a paragraph and heading

--- pages/page-radio-hosts.php
+++ pages/page-radio-hosts.php
@@ -8,5 +8,7 @@ Template Name: Radio: About the hosts
 
 <h1>Radio: About the Hosts</h1>
 
+<h2>Kevin Bowman</h2>
+<p>Galão redeye, so, rich, white so organic plunger pot steamed robust. Saucer grounds sit caramelization aged plunger pot bar flavour filter arabica.</p>
 
 <?php get_footer(); ?>
