2399ff9adee6d1fd48fbcbfd642934fd446b9a08
Simon Zou <simon.zou@gmail.com>
2014-04-12 20:54:36 -0700

    made mens volleyball into template

--- pages/page-sports-mens-volleyball.php
+++ pages/page-sports-mens-volleyball.php
@@ -309,125 +309,14 @@ Template Name: Sports Mens Volleyball
 <!--css -->
 
 <article>
-  <h1>Men's Volleyball:<br> The Competitive Demands</h1>
-    <p style="text-align:center;">With the Winter Olympics in full swing, the Daily Bruin takes a look at the experiences of four former Summer Olympians and current members of the UCLA gymnastics team.</p>
-  <div class="pagination pagination-centered">
-  <ul>
-    <li class="active"><a>Samantha Peszek</a></li>
-    <li><a href="http://dailybruin.com/features/gymnastics-the-competitive-demands-jennifer-pinches/">Jennifer Pinches</a></li>
-    <li><a href="http://dailybruin.com/features/jordyn-wieber-brings-strength-to-ucla-gymnastics-team/">Jordyn Wieber</a></li>
-    <li><a href="http://dailybruin.com/features/gymnastics-the-competitive-demands-danusia-francis/">Danusia Francis</a></li>
-  </ul>
-</div>
-
-	<img src="http://dailybruin.com/images/2014/02/web.peszek.picA_.jpg" class="aligncenter">
-	<span class="photocred">Katie Meyers / Daily Bruin senior staff</span><br><p style="text-align:center;">Samantha Peszek has competed at the highest levels of her sport and is currently a top ranked collegiate gymnast. The redshirt junior has battled numerous injuries throughout her career, from suffering a season-ending Achilles tear a year ago to tearing every ligament in her foot in warm-ups at the 2008 Beijing Olympics. Through it all, Peszek has relied on her mental focus to overcome the pressures of her sport.<p>
-
-<h2>Samantha Peszek</h2>
-	<p><strong>BY ZACHARY LEMOS</strong></p><div class="pq-wrap-2 pq-style-2"><div class="pq-thumb"></div>
-    <blockquote style-"pq-style-2">
-        <p>“The cortisone shot kicked in by this time and you’re just thinking about landing on your feet.”</p>
-    </blockquote><div class="pq-attribution">
-            <p class="pq-author"> – Samantha Peszek</p>
-            <cite></cite>
-          </div>
-
-</div><p>As her feet pressed into the mat below her, Samantha Peszek took just a small stutter step backward. </p>
-
-<p>For a short moment, she raised her arms, signaling the end of her routine on uneven bars and somberly forced her eyes toward the Olympic judges. </p>
-
-<div class="thumbnail with-caption-left"><img src="http://dailybruin.com/images/2014/02/web.peszek.picB_-1024x649.jpg" class="img-hori">
-Peszek delivered a perfect ten on the uneven bars in<br> UCLA's February 8th meet in only her fifth routine of the year.<br>
-<span class="photocred-inline">Katie Meyers / Daily Bruin</span></div>
-
-<p>Then her eyes flicked down, her shoulders slumped and she quickly stepped away from the pad under her feet.</p>
-
-<p>It was only after one or two limp-free steps away from the apparatus that Peszek remembered what her coaches had told her after she injured her foot in warm-ups minutes ago: You're not allowed to cry. You're not allowed to talk about it. You’re not allowed to be upset.</p>
-
-
-<p>So the then-16-year-old hid her look of pain, put a smile on her face and walked away from her first – and what turned out to be only – event in the 2008 Beijing Olympics.</p>
-
-<p>Even though the now-redshirt-junior was originally slated to represent the United States in all four gymnastics events, she showed off her ability to compete through less-than-ideal circumstances and hit the one routine Team USA needed her to hit – a skill she frequently displays as a gymnast at UCLA.</p>
-
-<p>“I asked if I could do the other events and the (coach) was like ‘No, we really just need you to hit (an uneven) bar routine,’” she said. "‘That’s your one job.’”</p>
-
-<p>While the women’s gymnastics team, headlined by household names Nastia Liukin and Shawn Johnson, went on to win a silver medal, Peszek later discovered she tore every ligament in her foot and was unable to compete after the preliminary round.</p>
-
-<div class="thumbnail with-caption-left"><img src="http://dailybruin.com/images/2014/02/web.peszek.picC_-682x1024.jpg" class="img-vert">
-Peszek represented the US in the <br>2008 Beijing Olympics.<br>
-<span class="photocred-inline">Katie Meyers / Daily Bruin</span></div>
-
-<p>And if the mental turmoil of being held from competition in Beijing wasn’t enough, Peszek was told to hide her despair for the sake of the other girls on the team.</p>
-
-<div class="pq-wrap-2 pq-style-2"><div class="pq-thumb"></div>
-    <blockquote style-"pq-style-2">
-        <p>“It was the most nerve-wracking thing ever, way more nerve-wracking than the actual Olympics.”</p>
-    </blockquote><div class="pq-attribution">
-            <p class="pq-author"> – Samantha Peszek</p>
-            <cite></cite>
-          </div>
-</div>
-
-<p>“I just had to switch gears really quick and do everything in my power to help (the team) compete well,” she said. “If I would've been distraught and a mess about it, that wouldn’t have really been helping.”</p>
-
-<p>This wasn’t the first time Peszek masked her emotions for the sake of her Olympic ambitions. At the Olympic trials a month earlier, the pressure mounted to a level even higher than what Peszek said she later felt in Beijing.</p>
-
-<p>After struggling on a few earlier routines, Peszek was told that the last one of the day would be do-or-die – hit it and she would make the team, miss it and she was done.</p>
-
-<p>Despite the heavy pressure, Peszek pulled off the routine to ultimately make Team USA.</p>
-
-
-<p>That Peszek was able to first block heavy pressure to reach the Olympics  and then shut out the disappointment of being sidelined in Beijing speaks to her tremendous mental strength, said Tara Scanlan, a UCLA sports psychology professor.</p>
-
-<p>Scanlan pointed out that this mental focus is especially important – and especially difficult – in a sport like gymnastics. While a shooting guard in basketball or pitcher in baseball can take a moment to recuperate after a missed shot or an off-target pitch, a gymnast gets no break during a routine – once it has started, then it must finish.</p>
-
-<p>“When you’re on the beam … if you splatter, there’s no regroup time – and you have to do it with a smile. You have to bounce right back up, let that mistake go, smile, focus and keep going," Scanlan said.</p>
-
-<p>And mental recovery becomes that much more important in a perfection-oriented sport like gymnastics, where wobblylandings or spread toes often translate into large point deductions.</p>
-
-<div class="thumbnail with-caption-left"><img src="http://dailybruin.com/images/2014/02/a2f93911-c786-4561-8188-74b8f5f4691e-682x1024.jpg" class="img-vert">
-World famous gymnastics coach Bela Karolyi <br>holds a young Samantha Peszek.<br><span class="photocred-inline">
-(Courtesy of Samantha Peszek)</span></div>
-
-<p>Years of performing under immense pressure mean that most Olympic athletes tend to have more confidence than their less-tested equivalents, said UCLA coach Valorie Kondos Field.</p><div class="pq-wrap-2 pq-style-2">
-
-
-<div class="pq-thumb"></div>
-    <blockquote style-"pq-style-2">
-        <p>“I think it’s 99 percent (mental). We’ve all been doing these skills for so long that when it’s time to compete, it’s purely just mental.”</p>
-    </blockquote><div class="pq-attribution">
-            <p class="pq-author"> – Sydney Sawa</p>
-            <cite>UCLA Senior Gymnast</cite>
-          </div>
-</div>
-
-<p>Peszek's ability to remain unfazed despite less-than-ideal conditions was on display in UCLA gymnastics’ most recent home meet on Feb. 8. She scored a career-first perfect 10 on uneven bars – coincidentally her least favorite event and the only one in which she was able to compete in Beijing – in only her fifth routine this year. 
-</p>
-
-<p>And although she missed two of the season's first four meets and sat out the entire 2013 season with an Achilles tear, Peszek ranks first nationally on beam and third on uneven bars.</p>
-
-<p>But just as she has shown before, things that might rattle any athlete – like a serious injury or competing sparingly – generally do not affect Peszek's routines.</p>
-
-<p>After all, from a short-lived Olympic appearance to her UCLA gymnastics career, she's come through time and time again.</p>
-
-
-<div class="sm gym-sm">
-    <a href="https://twitter.com/share" class="twitter-share-button" data-via="dailybruin" data-related="dailybruin">Tweet</a>
-    <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="//platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
-    <div class="fb-like" data-send="true" data-width="325" data-show-faces="true" data-action="recommend" data-font="lucida grande"></div>
-</div>
-
-<ul class="pager">
-  <li class="previous disabled">
-    <a href="">&larr; Previous</a>
-  </li>
-  <li class="next">
-    <a href="http://dailybruin.com/features/gymnastics-the-competitive-demands-jennifer-pinches/">Next &rarr;</a>
-  </li>
-</ul>
-</p>
-<div id="page-credit">Page created by <a href="http://aeiny.com">Aein Hope</a> and <a href="mailto:cyang@media.ucla.edu">Christina Yang</a></div>
-
+	<?php 
+		if ( have_posts() ) {
+			while ( have_posts() ) {
+				the_post();
+				the_content();
+			} // end while
+		} // end if
+	?>
 </article> 
 
 <?php get_footer(); ?>
