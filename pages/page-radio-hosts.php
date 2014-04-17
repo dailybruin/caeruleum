14fccf6e4f553cbd3ec99c255a79a4f9ae7aeecb
Daniel Yao <daniel988@ucla.edu>
2014-04-16 21:19:01 -0700

    swp backup

--- pages/page-radio-hosts.php
+++ pages/page-radio-hosts.php
@@ -4,34 +4,33 @@ Template Name: Radio: About the hosts
 <?php get_header(); ?>
 
 <style type="text/css">
-.email img {
-    width: 19px;
-    }
 </style>
 
 <h1>Radio: About the Hosts</h1>
 <h1>Long Story Short</h1>
 <div class="row-fluid">
     <div class="span6">
+<hr>
 <img src="http://i.imgur.com/SHzqj4h.jpg" alt="Two puppies in a basket." />
 <h2>Meral Arik</h2>
-<div class="email"> <img src="/img/radio_email_icon.svg"/> <a href=" mailto:marik@media.ucla.edu">marik@media.ucla.edu</a> </div>
+<div class="email"> <a href="mailto:marik@media.ucla.edu">marik@media.ucla.edu</a> </div>
 <div class="twitter">
-<i class="ticon-twitter" < a></i>
 <a href="https://twitter.com/share" rel = "external" target="_blank" data-via="dailybruin">
 Tweet 
+<i class="ticon-twitter" < a></i>
 </a>
 </div>
 <p>Iced, organic, kopi-luwak, rich ristretto cultivar dark cortado. Carajillo caffeine brewed pumpkin spice grounds seasonal mazagran that variety saucer wings. Espresso medium, robust seasonal french press cup fair trade mug french press barista percolator.</p>
     </div>
     <div class="span6">
+<hr>
 <img src="http://imgur.com/RJjNn9R.jpg" alt="A happy puppy sitting in the grass." />
 <h2>Kathleen McGovern</h2>
-<div class="email"> <img src="/img/radio_email_icon.svg"/> <a href=" mailto:marik@media.ucla.edu">marik@media.ucla.edu</a> </div>
+<div class="email"> <a href="mailto:kmcgovern@media.ucla.edu">marik@media.ucla.edu</a> </div>
 <div class="twitter">
-<i class="ticon-twitter" < a></i>
 <a href="https://twitter.com/share" rel = "external" target="_blank" data-via="dailybruin">
 Tweet 
+<i class="ticon-twitter" < a></i>
 </a>
 </div>
 <p>Barista, steamed at, whipped fair trade café au lait, coffee strong est et aroma. Froth organic ristretto, at in acerbic barista, plunger pot ristretto breve milk froth.</p>
@@ -39,54 +38,31 @@ Tweet
 </div>
 <div class="row-fluid">
     <div class="span6">
+<hr>
 <img src="http://imgur.com/CC4YycX.jpg" alt="A puppy with daisies." />
 <h2>Sidhaant Shah</h2>
-<div class="email"><img src="/img/radio_email_icon.svg"/><a href=" mailto:sshah@media.ucla.edu">sshah@media.ucla.edu</a> </div>
-<div class="twitter">
-<i class="ticon-twitter" < a></i>
-<a href="https://twitter.com/share" rel = "external" target="_blank" data-via="dailybruin">
-Tweet 
-</a>
-</div>
+a.ucla.edu">sshah@media.ucla.edu</a>
 <p>Robusta acerbic french press, carajillo shop viennese espresso blue mountain grounds. Blue mountain flavour cinnamon caramelization lungo et id white single origin white body. French press and flavour, seasonal redeye and breve java. Roast americano, crema cup, galão, chicory aroma rich cup roast.</p>
     </div>
     <div class="span6">
+<hr>
 <img src="http://imgur.com/jMxUraH.jpg" alt="Two sleeping puppies." />
 <h2>Jonaki Mehta</h2>
-<div class="email"> <img src="/img/radio_email_icon.svg"/> <a href=" mailto:jmehta@media.ucla.edu">jmehta@media.ucla.edu</a> </div>
-<div class="twitter">
-<i class="ticon-twitter" < a></i>
-<a href="https://twitter.com/share" rel = "external" target="_blank" data-via="dailybruin">
-Tweet 
-</a>
-</div>
 <p>Espresso frappuccino body, americano cappuccino brewed latte robusta. Arabica affogato cup americano trifecta cinnamon, doppio filter turkish half and half espresso. Sweet, trifecta, galão flavour, espresso variety café au lait siphon redeye medium. Dark caffeine organic, brewed, espresso aromatic strong caramelization decaffeinated.</p>
     </div>
 </div>
 <h1>Out of Bounds</h1>
 <div class="row-fluid">
     <div class="span6">
+<hr>
 <img src="http://imgur.com/zNgSw8Y.jpg" alt="A sleeping puppy with his toy." />
 <h2>Andrew Erickson</h2>
-<div class="email"> <img src="/img/radio_email_icon.svg"/> <a href=" mailto:aerickson@media.ucla.edu">aerickson@media.ucla.edu</a> </div>
-<div class="twitter">
-<i class="ticon-twitter" < a></i>
-<a href="https://twitter.com/share" rel = "external" target="_blank" data-via="dailybruin">
-Tweet 
-</a>
-</div>
 <p>Kopi-luwak, turkish, dripper kopi-luwak et irish plunger pot cup cappuccino. Cream, coffee, est mug trifecta instant roast as aromatic. Rich con panna ristretto percolator french press iced acerbic froth.</p>
     </div>
     <div class="span6">
+<hr>
 <img src="http://imgur.com/Pj621JV.jpg" alt="A puppy wearing glasses." />
 <h2>Kevin Bowman</h2>
-<div class="email"> <img src="/img/radio_email_icon.svg"/> <a href=" mailto:kbowman@media.ucla.edu">kbowman@media.ucla.edu</a> </div>
-<div class="twitter">
-<i class="ticon-twitter" < a></i>
-<a href="https://twitter.com/share" rel = "external" target="_blank" data-via="dailybruin">
-Tweet 
-</a>
-</div>
 <p>Cup, coffee id strong aromatic body latte. Pumpkin spice ristretto single origin arabica, java cultivar fair trade wings lungo. Ut pumpkin spice steamed con panna coffee turkish irish. Cappuccino, redeye, wings foam cup, percolator irish ut in plunger pot.</p>
     </div>
 </div>
