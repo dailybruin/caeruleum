19c3137370262442c9f93dd8acfb02bb4a9a1fd5
Alisha Rosenwein-Noss <arosenweinnoss@media.ucla.edu>
2014-04-21 18:10:40 -0700

    edited spacing and headings

--- pages/page-radio-hosts.php
+++ pages/page-radio-hosts.php
@@ -6,7 +6,10 @@ Template Name: Radio: About the hosts
 <style type="text/css">
 .email img {
     width: 19px;
-    }
+}
+#rh-title {
+    margin-bottom: 1em;
+}
 </style>
 
 <script src ="/js/vendor/audioplayer.js"></script>
@@ -16,12 +19,13 @@ Template Name: Radio: About the hosts
     });
 </script>
 
-<h1>Radio: About the Hosts</h1>
-<h1>Long Story Short</h1>
+<h1 id="rh-title">Radio: About the Hosts</h1>
+
+<h2>Long Story Short</h2>
 <div class="row-fluid">
     <div class="span6">
 <img src="http://i.imgur.com/SHzqj4h.jpg" alt="Two puppies in a basket." />
-<h2>Meral Arik</h2>
+<h3>Meral Arik</h3>
 <div class="email"> <img src="/img/radio_email_icon.svg"/> <a href=" mailto:marik@media.ucla.edu">marik@media.ucla.edu</a> </div>
 <div class="twitter">
 <i class="ticon-twitter" < a></i>
@@ -34,7 +38,7 @@ Tweet
     </div>
     <div class="span6">
 <img src="http://imgur.com/RJjNn9R.jpg" alt="A happy puppy sitting in the grass." />
-<h2>Kathleen McGovern</h2>
+<h3>Kathleen McGovern</h3>
 <div class="email"> <img src="/img/radio_email_icon.svg"/> <a href=" mailto:marik@media.ucla.edu">marik@media.ucla.edu</a> </div>
 <div class="twitter">
 <i class="ticon-twitter" < a></i>
@@ -49,15 +53,15 @@ Tweet
 <div class="row-fluid">
     <div class="span6">
 <img src="http://imgur.com/CC4YycX.jpg" alt="A puppy with daisies." />
-<h2>Sidhaant Shah</h2>
-<div class="email"><img src="/img/radio_email_icon.svg"/><a href=" mailto:sshah@media.ucla.edu">sshah@media.ucla.edu</a> </div>
+<h3>Sidhaant Shah</h3>
+<div class="email"><img src="/img/radio_email_icon.svg"/> <a href=" mailto:sshah@media.ucla.edu">sshah@media.ucla.edu</a> </div>
 <div class="twitter">
 <i class="ticon-twitter" < a></i>
 <a href="https://twitter.com/share" rel = "external" target="_blank" data-via="dailybruin">
 Tweet 
 </a>
 </div>
-<audio src=http://dailybruin.com/images/2014/04/Mollymann.mp3" preload="auto" controls></audio>
+<audio src="http://dailybruin.com/images/2014/04/Mollymann.mp3" preload="auto" controls></audio>
 <p>Robusta acerbic french press, carajillo shop viennese espresso blue mountain grounds. Blue mountain flavour cinnamon caramelization lungo et id white single origin white body. French press and flavour, seasonal redeye and breve java. Roast americano, crema cup, galão, chicory aroma rich cup roast.</p>
     </div>
     <div class="span6">
@@ -74,11 +78,11 @@ Tweet
 <p>Espresso frappuccino body, americano cappuccino brewed latte robusta. Arabica affogato cup americano trifecta cinnamon, doppio filter turkish half and half espresso. Sweet, trifecta, galão flavour, espresso variety café au lait siphon redeye medium. Dark caffeine organic, brewed, espresso aromatic strong caramelization decaffeinated.</p>
     </div>
 </div>
-<h1>Out of Bounds</h1>
+<h2>Out of Bounds</h2>
 <div class="row-fluid">
     <div class="span6">
 <img src="http://imgur.com/zNgSw8Y.jpg" alt="A sleeping puppy with his toy." />
-<h2>Andrew Erickson</h2>
+<h3>Andrew Erickson</h3>
 <div class="email"> <img src="/img/radio_email_icon.svg"/> <a href=" mailto:aerickson@media.ucla.edu">aerickson@media.ucla.edu</a> </div>
 <div class="twitter">
 <i class="ticon-twitter" < a></i>
@@ -91,7 +95,7 @@ Tweet
     </div>
     <div class="span6">
 <img src="http://imgur.com/Pj621JV.jpg" alt="A puppy wearing glasses." />
-<h2>Kevin Bowman</h2>
+<h3>Kevin Bowman</h3>
 <div class="email"> <img src="/img/radio_email_icon.svg"/> <a href=" mailto:kbowman@media.ucla.edu">kbowman@media.ucla.edu</a> </div>
 <div class="twitter">
 <i class="ticon-twitter" < a></i>
