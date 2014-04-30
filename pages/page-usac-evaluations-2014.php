c5fdf2ea26f8819706c8bab293d0ad731fb421a2
omniaura <omniaura5@gmail.com>
2014-04-29 22:55:51 -0700

    added 2014 USAC evaluations template with people/polls

---
+++ pages/page-usac-evaluations-2014.php
@@ -0,0 +1,261 @@
+<?php/*
+Template Name: USAC Evaluations 2014
+*/ ?>
+
+<?php get_header(); ?>
+
+<style type="text/css">
+
+    #evals h1 {
+        margin-bottom: 1em;
+    }
+
+    #evals h2, #evals h3 {
+        margin-top: 0;
+    }
+
+    #evals h3 {
+        margin-bottom: 1.2em;
+    }
+
+    #evals .person {
+        margin-bottom: 4em;
+    }
+
+    #evals .rating {
+        font-size: 1.2em;
+        margin-bottom: 0.6em;
+    }
+    
+    #evals .rating .number {
+        display: inline-block;
+        font-weight: bold;
+        color: #00d;
+        margin-right: 0.4em;
+        font-size: 1.2em;
+    }
+
+    #evals .feedback {
+        margin-bottom: 1em;
+        text-align: center;
+    }
+
+</style>
+
+<div id="evals">
+    <h1>2014 USAC Evaluations</h1>
+
+    <div class="row">
+        <span class="span6">
+        </span>
+
+        <span class="span6">
+            <div class="person">
+                <div class="row">
+                    <span class="span2">
+                        <img class="mug" src="http://dailybruin.com/images/2014/04/web.John_.Joanino.jpg"/>
+                    </span>
+                    <span class="span4">
+                        <h2>John Joanino</h2>
+                        <h3>President</h3>
+                        <div class="rating"><span class="number">4</span>Platform quality</div>
+                        <div class="rating"><span class="number">4</span>Platform completion</div>
+                        <div class="rating"><span class="number">2</span>Fulfillment of responsibilities</div>
+                        <div class="rating"><span class="number">1</span>Transparency</div>
+                    </span>
+                </div>
+                <h4 class="feedback">Students&rsquo; feedback</h4>
+                <img src="http://dailybruin.com/images/2014/04/poll_joanino.png"/>
+            </div>
+            <div class="person">
+                <div class="row">
+                    <span class="span2">
+                        <img class="mug" src="http://dailybruin.com/images/2014/04/web.Avi.Oved_.jpg"/>
+                    </span>
+                    <span class="span4">
+                        <h2>Avi Oved</h2>
+                        <h3>Internal Vice President</h3>
+                        <div class="rating"><span class="number">5</span>Platform quality</div>
+                        <div class="rating"><span class="number">4</span>Platform completion</div>
+                        <div class="rating"><span class="number">4</span>Fulfillment of responsibilities</div>
+                        <div class="rating"><span class="number">4</span>Transparency</div>
+                    </span>
+                </div>
+                <h4 class="feedback">Students&rsquo; feedback</h4>
+                <img src="http://dailybruin.com/images/2014/04/poll_oved.png"/>
+            </div>
+            <div class="person">
+                <div class="row">
+                    <span class="span2">
+                        <img class="mug" src="http://dailybruin.com/images/2014/04/web.Maryssa.Hall_.jpg"/>
+                    </span>
+                    <span class="span4">
+                        <h2>Maryssa Hall</h2>
+                        <h3>External Vice President</h3>
+                        <div class="rating"><span class="number">5</span>Platform quality</div>
+                        <div class="rating"><span class="number">3</span>Platform completion</div>
+                        <div class="rating"><span class="number">5</span>Fulfillment of responsibilities</div>
+                        <div class="rating"><span class="number">1</span>Transparency</div>
+                    </span>
+                </div>
+                <h4 class="feedback">Students&rsquo; feedback</h4>
+                <img src="http://dailybruin.com/images/2014/04/poll_hall.png"/>
+            </div>
+            <div class="person">
+                <div class="row">
+                    <span class="span2">
+                        <img class="mug" src="http://dailybruin.com/images/2014/04/web.Darren.Ramalho.jpg"/>
+                    </span>
+                    <span class="span4">
+                        <h2>Darren Ramalho</h2>
+                        <h3>Academic Affairs Commissioner</h3>
+                        <div class="rating"><span class="number">4</span>Platform quality</div>
+                        <div class="rating"><span class="number">5</span>Platform completion</div>
+                        <div class="rating"><span class="number">5</span>Fulfillment of responsibilities</div>
+                        <div class="rating"><span class="number">5</span>Transparency</div>
+                    </span>
+                </div>
+                <h4 class="feedback">Students&rsquo; feedback</h4>
+                <img src="http://dailybruin.com/images/2014/04/poll_ramalho.png"/>
+            </div>
+            <div class="person">
+                <div class="row">
+                    <span class="span2">
+                        <img class="mug" src="http://dailybruin.com/images/2014/04/web.Jessica.Kim_.jpg"/>
+                    </span>
+                    <span class="span4">
+                        <h2>Jessica Kim</h2>
+                        <h3>Campus Events Commissioner</h3>
+                        <div class="rating"><span class="number">3</span>Platform quality</div>
+                        <div class="rating"><span class="number">2</span>Platform completion</div>
+                        <div class="rating"><span class="number">5</span>Fulfillment of responsibilities</div>
+                        <div class="rating"><span class="number">2</span>Transparency</div>
+                    </span>
+                </div>
+                <h4 class="feedback">Students&rsquo; feedback</h4>
+                <img src="http://dailybruin.com/images/2014/04/poll_kim.png"/>
+            </div>
+            <div class="person">
+                <div class="row">
+                    <span class="span2">
+                        <img class="mug" src="http://dailybruin.com/images/2014/04/web.Jessica.Trumble.jpg"/>
+                    </span>
+                    <span class="span4">
+                        <h2>Jessica Trumble</h2>
+                        <h3>Cultural Affairs Commissioner</h3>
+                        <div class="rating"><span class="number">3</span>Platform quality</div>
+                        <div class="rating"><span class="number">3</span>Platform completion</div>
+                        <div class="rating"><span class="number">4</span>Fulfillment of responsibilities</div>
+                        <div class="rating"><span class="number">2</span>Transparency</div>
+                    </span>
+                </div>
+                <h4 class="feedback">Students&rsquo; feedback</h4>
+                <img src="http://dailybruin.com/images/2014/04/poll_trumble.png"/>
+            </div>
+            <div class="person">
+                <div class="row">
+                    <span class="span2">
+                        <img class="mug" src="http://dailybruin.com/images/2014/04/web.Sam_.Haws_.jpg"/>
+                    </span>
+                    <span class="span4">
+                        <h2>Sam Haws</h2>
+                        <h3>General Representative 1</h3>
+                        <div class="rating"><span class="number">3</span>Platform quality</div>
+                        <div class="rating"><span class="number">2</span>Platform completion</div>
+                        <div class="rating"><span class="number">3</span>Fulfillment of responsibilities</div>
+                        <div class="rating"><span class="number">2</span>Transparency</div>
+                    </span>
+                </div>
+                <h4 class="feedback">Students&rsquo; feedback</h4>
+                <img src="http://dailybruin.com/images/2014/04/poll_haws.png"/>
+            </div>
+            <div class="person">
+                <div class="row">
+                    <span class="span2">
+                        <img class="mug" src="http://dailybruin.com/images/2014/04/web.Sunny_.Singh_.jpg"/>
+                    </span>
+                    <span class="span4">
+                        <h2>Sunny Singh</h2>
+                        <h3>General Representative 2</h3>
+                        <div class="rating"><span class="number">4</span>Platform quality</div>
+                        <div class="rating"><span class="number">2</span>Platform completion</div>
+                        <div class="rating"><span class="number">4</span>Fulfillment of responsibilities</div>
+                        <div class="rating"><span class="number">4</span>Transparency</div>
+                    </span>
+                </div>
+                <h4 class="feedback">Students&rsquo; feedback</h4>
+                <img src="http://dailybruin.com/images/2014/04/poll_singh.png"/>
+            </div>
+            <div class="person">
+                <div class="row">
+                    <span class="span2">
+                        <img class="mug" src="http://dailybruin.com/images/2014/04/web.Lizzy_.Naameh.jpg"/>
+                    </span>
+                    <span class="span4">
+                        <h2>Lizzy Naameh</h2>
+                        <h3>General Representative 3</h3>
+                        <div class="rating"><span class="number">4</span>Platform quality</div>
+                        <div class="rating"><span class="number">1</span>Platform completion</div>
+                        <div class="rating"><span class="number">2</span>Fulfillment of responsibilities</div>
+                        <div class="rating"><span class="number">2</span>Transparency</div>
+                    </span>
+                </div>
+                <h4 class="feedback">Students&rsquo; feedback</h4>
+                <img src="http://dailybruin.com/images/2014/04/poll_naameh.png"/>
+            </div>
+            <div class="person">
+                <div class="row">
+                    <span class="span2">
+                        <img class="mug" src="http://dailybruin.com/images/2014/04/web.Armen_.Hadjimanoukian.jpg"/>
+                    </span>
+                    <span class="span4">
+                        <h2>Armen Hadjimanoukian</h2>
+                        <h3>Facilities Commissioner</h3>
+                        <div class="rating"><span class="number">3</span>Platform quality</div>
+                        <div class="rating"><span class="number">4</span>Platform completion</div>
+                        <div class="rating"><span class="number">5</span>Fulfillment of responsibilities</div>
+                        <div class="rating"><span class="number">2</span>Transparency</div>
+                    </span>
+                </div>
+                <h4 class="feedback">Students&rsquo; feedback</h4>
+                <img src="http://dailybruin.com/images/2014/04/poll_hadjimanoukian.png"/>
+            </div>
+            <div class="person">
+                <div class="row">
+                    <span class="span2">
+                        <img class="mug" src="http://dailybruin.com/images/2014/04/web.Lauren.Rogers.jpg"/>
+                    </span>
+                    <span class="span4">
+                        <h2>Lauren Rogers</h2>
+                        <h3>Financial Supports Commissioner</h3>
+                        <div class="rating"><span class="number">5</span>Platform quality</div>
+                        <div class="rating"><span class="number">5</span>Platform completion</div>
+                        <div class="rating"><span class="number">5</span>Fulfillment of responsibilities</div>
+                        <div class="rating"><span class="number">4</span>Transparency</div>
+                    </span>
+                </div>
+                <h4 class="feedback">Students&rsquo; feedback</h4>
+                <img src="http://dailybruin.com/images/2014/04/poll_rogers.png"/>
+            </div>
+            <div class="person">
+                <div class="row">
+                    <span class="span2">
+                        <img class="mug" src="http://dailybruin.com/images/2014/04/web.Savannah.Badalich.jpg"/>
+                    </span>
+                    <span class="span4">
+                        <h2>Savannah Badalich</h2>
+                        <h3>Student Wellness Commissioner</h3>
+                        <div class="rating"><span class="number">5</span>Platform quality</div>
+                        <div class="rating"><span class="number">4</span>Platform completion</div>
+                        <div class="rating"><span class="number">5</span>Fulfillment of responsibilities</div>
+                        <div class="rating"><span class="number">1</span>Transparency</div>
+                    </span>
+                </div>
+                <h4 class="feedback">Students&rsquo; feedback</h4>
+                <img src="http://dailybruin.com/images/2014/04/poll_badalich.png"/>
+            </div>
+        </span>
+    </div>
+
+</div>
+<?php get_footer(); ?>
