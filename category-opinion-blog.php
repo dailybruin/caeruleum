063ef20e4e5aa0e83dc3413e7eedd3b200f54b05
Daily Bruin Intern <dailybruin@dailybruin.(none)>
2014-05-08 17:42:59 -0700

    Set up responsive grid

--- category-opinion-blog.php
+++ category-opinion-blog.php
@@ -1,1 +1,18 @@
-"Hello World"
+<? php get_header(); ?>
+
+<link rel="stylesheet"type="text/css" href="css/bootstrap.css"/>
+<link rel="stylesheet"type="text/css" href="css/bootstrap-responsive.css"/>
+    
+<style>
+    .span8 {border: 1px solid black; height: 100px; background-color: #ccc;}
+    .span4 {border: 1px solid black; height: 100px; background-color: #000; color: #fff;}
+</style>
+
+<body>
+        
+    <div class="container-fluid">
+        <div class="row-fluid">
+            <div class="span8">Left Column</div>
+            <div class="span4">Right Column</div>                                       </div>
+    </div>
+                                                                                </body>
