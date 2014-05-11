6d1cb0acc2c4e12b585d7cb4c7ff790ecb973b76
Amy Tsao <amy.tsao227@gmail.com>
2014-05-11 13:14:44 -0700

    added cards

--- pages/page-spring-sing-2014.php
+++ pages/page-spring-sing-2014.php
@@ -58,9 +58,53 @@ Template Name: Spring Spring 2014
 	float: left;
 	width: 100%;
 	min-height: auto;
-	display: inline-block;
 	text-align: center;
 	border-radius: 10px;
+}
+#cards {
+	font-family: consolas;
+    	font-size: 0.7em;
+    	color: #454545;
+	box-shadow: 2px 2px 8px #888888
+
+    	overflow: hidden;
+	width: 300px;
+	height: 550px;
+	display: inline-block;
+}
+#cards i, em { 
+color: #93CDEE; 
+}
+#cards img {
+	max-width: 100%;
+    	max-height: 100%;
+}
+
+#cards h2, h3 {
+	font-weight: bold;
+	padding-left: 10px;
+}
+#cards blockquote p {
+  	font-size: 1.2em;    
+  	float: left;
+  	background: url(http://dailybruin.com/images/2014/05/quote-mark.png) 
+  	top left no-repeat;
+}
+
+#cards cite {
+  font-size: 1.2em;
+  float: right; 
+  padding-right: 20px;   
+}
+
+#cards cite a { 
+	padding-left: 40px;
+	color: #6B80A9; 
+}
+#cards cite a:hover { 
+	color: #6DB4DE;
+	text-decoration: normal; 
+	font-weight: bold;
 }
 
 #tweets {
@@ -132,12 +176,19 @@ ul {
 <div id="content">
 
 	<div id="cards">
-		<h1>Solo Artists</h1>
-		<ul class="profiles">
-			<li><a href="#">Item 1</a></li>
-			<li><a href="#">Item 2</a></li>
-			<li><a href="#">Item 3</a></li>		
+		<img src='https://24.media.tumblr.com/fa0cbe4c49c39f2eab9826dddb6a8414/tumblr_mu4cotMVol1rsozsco1_500.jpg' />
+		<h2>Band Name</h2>
+		<h3>Genre: Rock</h3>
+		<ul type = "circle">
+			<li>blah blah blah I'm a fact</li>
+			<li>I'm another fact</li>
+			<li>Man facts are so cool</li>
 		</ul>
+		<blockquote>
+			<p>Every monarch in history has succeeded someone who is dead. Or just about to be. My predecessor's not only alive, but very much so. Bloody mess. Can't even give them a Christmas speech.</p>
+			<cite>-- Not Socrates<br /> <br />
+			<a href="http://www.google.com">Read More...</a></cite>
+		</blockquote>
 	</div>
 
 	<div id="tweets">
