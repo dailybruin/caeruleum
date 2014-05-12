c3c911863ccfbbfa221777381e6f578d31f494e7
Daily Bruin Intern <dailybruin@dailybruin.(none)>
2014-05-11 17:02:34 -0700

    Updated main left column structure/style

--- category-opinion-blog.php
+++ category-opinion-blog.php
@@ -4,17 +4,108 @@ Template Name: Opinion Blog
 <?php get_header(); ?>
 
 <style>
-    .span8 {border: 1px solid black; height: 100px; background-color: #ccc;}
-    .span4 {border: 1px solid black; height: 100px; background-color: #000; color: #fff;}
+	.featured-img {margin: 10px 0 10px 0;}
+	.category {margin: 0 0 5px 0; height: 24px; display: inline-block; padding: 2px 20px 0 20px; font-family: Open Sans; font-size: 10pt; font-weight: bold;}
+	.c1 {background-color: #15134b; color: #fff;}
+	.c2 {background-color: #881e2f; color: #fff;}
+	.c3 {background-color: #19562a; color: #fff;}
+	.c4 {background-color: #c7ba26; color: #fff;}
+	.feature-date {margin-top: 5px; text-align: right; float: right; color: #545454; font-size: 8pt;}
+	.date {color: #545454; font-size: 8pt;}
+	a.heading {color: #000; font-family: Georgia; font-size: 18.2pt; font-weight: bold; line-height: 1.1em;}
+	.author {font-family: Arial; margin: 10px 0 5px 0;}
+	.sub-img {margin: 10px 0 10px 0;}
+	.recent-cols {padding: 20px 0 10px 10px;}
+	a.sec-head {font-size: 14pt; font-family: Georgia; font-weight: bold; line-height: 1.1em;}
+	a.sub-head {font-family: Georgia; font-weight: bold; line-height: 1.1em;}
+	a.title {color: #000;}
+	.illo {margin: 10px 0 10px 0;}
 </style>
 
 <body>
+
+<h1>Opinion Blog</h1>
         
     <div class="container-fluid">
         <div class="row-fluid">
-            <div class="span8">Left Column</div>
-            <div class="span4">Right Column</div>                                       </div>
-    </div>
-                                                                                </body>
 
-                                                                                <?php get_footer(); ?>
+            <div class="span5">
+		<img class="featured-img" src="http://pets4u.info/wp-content/uploads/2013/12/baby-cats-and-dogs-7.jpg"/>
+		<div class="category c1">CATS & DOGS</div>
+		<div class="feature-date">MAY 11, 2014</div>
+
+		<div class="content">
+			<a class="heading" href="#">Why can't we be friends: The never-ending struggle</a><br/>
+			<div class="author">BY JOE BRUIN</div>
+			<div class="description">
+				<p>It's a widely known that dogs and cats don't mix. But where did this notion of cat-people and dog-people come from? Why do we even care about this? Joe Bruin gives his two cents on the eternal feud of cat-loves and dog-lovers. <a href="#">More >></a>
+</p>
+			</div>
+		</div>
+
+		<hr style="height:2px;border:none;color:#333;background-color:#333;" />
+
+		<div class="category c2">BIRDS & OTHER WINGED ANIMALS</div>
+
+		<div class="row-fluid">
+			<div class="span5">
+				<img class="sub-img" src="http://www.news-world.us/wp-content/uploads/2013/07/Beautiful-Birds-1.jpg"/>
+				<a class="sec-head title" href="#">Birds of a feather flock together</a>
+				<div class="date">MAY 11, 2014</div><br/><br/>
+			</div>
+
+			<div class="span7 recent-cols">
+				<a class="sub-head title" href="#">10 Things You Need to Know About Birds</a><br/><span class="date">MAY 11, 2014</span><hr/>
+				<a class="sub-head title" href="#">Seagulls and other fearless animals</a><br/><span class="date">MAY 11, 2014</span><hr/>
+				<a class="sub-head title" href="#">10 Things You Don't Need to Know About Birds</a><br/><div class="date">MAY 11, 2014</div><br/>			</div>
+		</div>
+
+		<hr style="height:2px;border:none;color:#333;background-color:#333;" />
+
+		<div class="category c3">THE WONDERFUL LIVES OF TURTLES</div>
+
+		<div class="row-fluid">
+			<div class="span5">
+				<img class="sub-img" src="http://martywolff.cdn3.mirahost.com/public/uploads/content/10/turtle-facial.jpg"/>
+				<a class="sec-head title" href="#">The secrets of longevity</a>
+				<div class="date">MAY 11, 2014</div><br/><br/>
+			</div>
+
+			<div class="span7 recent-cols">
+				<a class="sub-head title" href="#">Why are turtles so slow?</a><br/><span class="date">MAY 11, 2014</span><hr/>
+				<a class="sub-head title" href="#">Surf's up dude!</a><br/><span class="date">MAY 11, 2014</span><hr/>
+				<a class="sub-head title" href="#">The struggle for survival</a><br/><span class="date">MAY 11, 2014</span><br/>			</div>
+		</div>
+
+		<hr style="height:2px;border:none;color:#333;background-color:#333;" />
+
+		<div class="category c4">FLOWER POWER</div>
+
+		<div class="row-fluid">
+			<div class="span5">
+				<img class="illo" src="http://bigwol.com/wp-content/uploads/2014/04/abstract-flower-illustration_1600x1200_15393.jpg"/>
+				<a class="sec-head title" href="#">A garden of hope</a>
+				<div class="date">MAY 11, 2014</div><br/><br/>
+			</div>
+
+			<div class="span7 recent-cols">
+				<a class="sub-head title" href="#">101 Flowers</a><br/><span class="date">MAY 11, 2014</span><br/><hr/>
+				<a class="sub-head title" href="#">Venus Flytrap and other Carnivorous Plants</a><br/><span class="date">MAY 11, 2014</span><br/><hr/>
+				<a class="sub-head title" href="#">Look at these beautiful flowers</a><br/><span class="date">MAY 11, 2014</span><br/>
+			</div>
+		</div>
+
+
+	     </div>
+
+            <div class="span3">
+
+	     </div>
+
+	     <?php get_sidebar(); ?>
+
+	</div>
+    </div>
+</body>
+
+<?php get_footer(); ?>
