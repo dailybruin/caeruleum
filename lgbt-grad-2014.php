394cfa0310450235c6f9a8ef7cd0546cc32593ac
Daily Bruin Intern <dwu@media.ucla.edu>
2014-06-06 17:17:37 -0700

    Built template for LGBT page

---
+++ lgbt-grad-2014.php
@@ -0,0 +1,253 @@
+<?php/*
+Template Name: LGBT Grad Issue 2014
+*/ ?>
+<?php get_header(); ?>
+
+<style>
+    .vignette{margin: 10px;}
+    .vignette-photo{float: right;}
+</style>
+
+<body>
+
+<div class"container-fluid">
+    <div class="row-fluid">
+        <div class="span7">
+            <article>
+                <section>
+
+                    <?php 
+                        $postid = url_to_postid('http://localhost:8080/?p=161184');
+                        $post_object = get_post($postid);
+
+                        echo '<h1>' .  $post_object->post_title . '</h1>';
+                        echo $post_object->post_content;
+                    ?>
+
+<!--<p>She remembers suppressing her emerging sexuality to comply with her own internalized homophobia that was influenced by the strict Christian ideals of her conservative hometown.this is hard to read. you wouldnt talk like this the way it is written right now. try to rephrase so it's more straightforward. you can make it more conversational if you want.</p>
+
+              <p>But when Natalie Kirsten left home for UCLA, she was finally able to accept her sexuality. With this acknowledgement came the realization that these two parts of her weren't as conflicting as she previously thought.</p>
+
+              <p>"These two parts of me, they work together," said Kirsten, a first-year undeclared student who identifies as lesbian and Christian. "My Christian identity just means that I love everybody... and my lesbian identity just means that I am attracted to women and I understand things a little differently."</p>
+
+              <p>Kirsten now attends the Metropolitan Community Church in Los Feliz, which caters to a predominately lesbian, gay, bisexual and transgender congregation and focuses on inclusivity.</p>
+
+              <p>Houses of worship that are geared toward the LGBT population are popping up across the nation and signify the increasing connections between the religious and LGBT communities.</p>
+
+              <p>"There has been a huge shift in culture in the last 20 to 30 years and the Church is playing catch up... as we see various denominations accepting people in the church,the quote reads kind of awkwardly, what'd he say where the ... is?" said Reverend Doctor Neil Thomas, the senior pastor at the Metropolitan Community Church.</p>
+
+              <p>Father Chris Ponnet, the spiritual director for the LA Archdiocese Office of Catholic Ministry with Lesbian and Gay Persons, said that while the Catholic Church does not bless LGBT relationships, the outlook is changing.</p>
+
+              <p>"During the last 50 years, we see some people being very harsh and bitter and the Church has said that all of that discrimination is contrary to our church teaching," Ponnet said. "Now we are in a period of time where in the last 8 or 10 years, civil society is beginning to look at what a committed relationship means in the gay and lesbian experience.</p>
+
+              <p>Thomas added that he thinks the traditional stereotype that the religious and the LGBT communities are in conflict takes roots in American culture.</p>
+
+              <p>"I think that the dominant culture, especially in this country, is one that says 'thou shalt not.' You shalt not be a homosexual and a Christian," Thomas said. "Many LGBT people grow up with that rhetoric both from the Church and from culture."</p>
+
+              <p>Reverend Michael Schuenemeyer, the United Church of Christ's executive for health and wholeness advocacy, said he thinks this stereotype is held by some members of the Christian community that have more conservative views.</p>
+
+              <p>He said that many of the LGBT people he has worked with have been wounded by some church leaders that have taken a "very rejective and abusive approach."</p>
+
+              <p>"But the Church is not God. The church is God's agent. It is a human institution, and it does not always get it right," Schuenemeyer said.</p>
+
+              <p>Some UCLA students said that they have also experienced this less accepting approach, particularly on the UCLA campus.</p>
+
+              <p>Steven Mion, a first-year political science student who identifies as gay and Methodist, said the first time he felt uncomfortable being with fellow Christians because of his sexuality was when he got to UCLA.</p>
+
+              <p>Mion signed up for a Christian fellowship in the beginning of the school year, without disclosing his sexuality and said he remembers telling some members about his desire to incorporate more LGBT people in the church.</p>
+
+              <p>The responses of the students in the fellowship left him surprised and confused, he said.</p>
+
+              <p>"That was the first time, I'll never forget it, I ever heard the language of, 'love the sinner, hate the sin,'" Mion said. "At that point, they didn't know they were addressing me as an LGBT person... and I had no idea how to respond."</p>
+
+              <p>Despite this instance, Mion continued to attend their meetings hoping to change the fellowship's perspective on being both Christian and gay.</p>
+
+              <p>But after a few more meetings, he decided he had to be honest with the fellowship and he told the lead pastor about his sexuality, he said.</p>
+
+              <p>Mion said the pastor responded by telling him that he was "broken."</p>
+
+              "It caught me off guard and it really scared me," Mion said. "When you say it's (being gay is) a sin, I can outright disagree with you and we can contest bible verses. When you say it's broken... that means you are going to try and fix me, and that's scary."
+
+              Mion said he never returned to the fellowship and instead found Christian groups on campus that were geared specifically toward the LGBT community.
+
+              Those groups include Cornerstone, a small group for LGBT Christians sponsored by the University Catholic Center, and another similar small groupwhat's the name of this group? it's weird to name cornerstone and not this It actually doesn't have a name its just called a LGBT small group sponsored by the InterVarsity Bruin Christian fellowship.
+
+              Cornerstone has reached out to some of the Christian groups on campus to connect with them, but none have accepted the offer, said Leiza Castillo, one of the co-leaders of Cornerstone and a third-year English student who identifies as lesbian and Catholic.not even the similar lgbt-geared groups have agreed to connect? Cornerstone is one of the two LGBT groups the people that go to cornerstone and the people that go to the intervarsity small group are the same people so i don't think they didn't reach out to intervarsity.
+
+              The Daily Bruin contacted many of the Christian groups on campus to ask their stance on LGBT inclusion in the church. All but the Christian Social Work Caucus either declined to comment or could not be reached for this article.
+
+              Jenny Rivas, Co-chair of the Christian Social Work Caucus and a graduate student in social work, said that the club has no official stance on LGBT inclusion in the church and that it is welcoming of everyone, including members of the LGBT community.
+
+              While many national and campus religious groups and organizations consider LGBT issues taboo, some institutions are changing existing policies to promote inclusion.
+              Kerry Chaplin, a rabbinic intern for Hillel at UCLA who identifies as lesbian and Jewish, is studying to be a rabbi at the Ziegler School of Rabbinic Studies. She began her studies there after a policy change in 2005 allowed applicants from the LGBT community to apply to be a rabbi.
+
+              Chaplin applied to Rabbinical schools when she graduated college before the policy changed, but said she decided to withdraw her application after realizing she would have to hide a part of herself.
+
+              "I needed to be grounded in myself and have the freedom to move. I felt like I was grounded, but I didn't have the freedom to move," Chaplin said.
+
+              When she made the decision to withdraw her application, she said she found herself straying away from her Jewish faith.
+
+              But work with a multi-faith organization to help lift people out of difficult life circumstances brought her back to her religion.
+
+              Chaplin said she realized she had to stop neglecting the religious aspect of her identity that was a large component of who she is.
+              "I decided to stop running from what was clearly the way I intended to bring Torah into this world and bring generative energy into this world," Chaplin said.
+
+              Chaplin reapplied in 2008 and has one more year left before completing her studies.
+
+              But some members of the LGBT community, like Sam Haws, do not always return to their faith.
+
+              Haws, a third-year economics student who identifies as gay and an agnostic-theist, grew up in a strict Mormon household where he woke up every day at 6 a.m. to attend Seminary, or Mormon church school.
+
+              Haws said he acknowledged that the high standards of the Mormon church undoubtedly made him who he is today.
+
+              Yet, he still found some of the values of the Mormon Church limiting.
+
+              "I got really incredible values from it but I think it is very damaging to grow up, especially when you are an LGBT person... in that environment and feel as though you can't explore who you are," Haws said. "And you feel like there is a part of you that you need to hide in order to adhere to other standards that you are told are important."
+              He added that he thinks the Mormon faith is becoming more accepting.
+
+              "The Church still has its open stances against same sex marriage and homosexual activity... but the culture in general is changing," Haws said. "And that's what gives me so much hope, because I notice that it's about the people that make up any institution for me."
+
+              For Mion, instead of trying to convince the various student groups on campus that it is possible to be LGBT and religious, he said he would rather show his dedication to both communities by following the instructions of the Bible while also embracing his sexuality.
+
+              "To me, the genuine form of outreach in ministry is to model it. It's to live as an example of how God can make a difference in your life," Mion said. "If you are living as a model to that, as a testament to God's love... that's a more compelling conversation of what it means to have faith and a relationship with God."-->
+
+              </section>
+            </article>
+        </div>
+        <div class="span5">
+            <center>
+
+                <h2>Vignettes</h2>
+
+                <a data-toggle="modal" href="#myModal"><img class="vignette" src="http://placehold.it/200x200"></a>
+
+                <a data-toggle="modal" href="#myModal2"><img class="vignette" src="http://placehold.it/200x200"></a>
+
+                <a data-toggle="modal" href="#myModal3"><img class="vignette" src="http://placehold.it/200x200"></a>
+
+                <a data-toggle="modal" href="#myModal4"><img class="vignette" src="http://placehold.it/200x200"></a>
+
+                <a data-toggle="modal" href="#myModal5"><img class="vignette" src="http://placehold.it/200x200"></a>
+
+                <a data-toggle="modal" href="#myModal6"><img class="vignette" src="http://placehold.it/200x200"></a>
+                <a data-toggle="modal" href="#myModal6"><img class="vignette" src="http://placehold.it/200x200"></a>
+
+            </center>
+
+	
+		<!--Modal content starts here-->
+	    <div id="myModal" class="modal hide fade" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+            <div class="modal-header">
+              <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
+              <h3 id="myModalLabel">Shen Heckel</h3>
+            </div>
+            <div class="modal-body">
+                  For Shen Heckel, abandoning his faith was never an option. It was his emerging sexuality that he wanted to run away from.
+
+                  "I've always been very Catholic and I was struggling (because) if this does make me gay, do I belong in the church?" said Heckel, a UCLA alumnus who identifies as Catholic and a transgender man, about first realizing his sexuality.
+
+                  Despite Heckel's perceived polarity of these two communities, he said he eventually found peace by connecting with someone toward the end of his college career who was also going through a similar experience.
+
+                  "I kept praying and hoping to find someone that I could relate to... and I ended up finding someone that was transgender and Catholic," Heckel said. "I think he really helped me through (coming out)... we were able to talk about both worlds and have emotional connections to both worlds. It made me feel like I am not alone."
+
+                  Heckel said this connection and finally feeling understood by someone brought him closer to his faith.
+
+                  "The stronger connection I have with myself being transgender, the stronger connection I have with actually being Catholic," Heckel said.
+
+                  Now, Heckel said he is committed to sharing his story to help others who may be struggling through the same situation that he did. Heckel also now works as a catechist, someone who teaches Catholic scripture, at the University Catholic Center.
+            </div>
+            <div class="modal-footer">
+              <button class="btn" data-dismiss="modal" aria-hidden="true">Close</button>
+              <button class="btn btn-primary">Save changes</button>
+            </div>
+          </div>
+
+          <div id="myModal2" class="modal hide fade" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+            <div class="modal-header">
+              <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
+              <h3 id="myModalLabel"></h3>
+            </div>
+            <div class="modal-body">
+              <p>One fine body…</p>
+            </div>
+            <div class="modal-footer">
+              <button class="btn" data-dismiss="modal" aria-hidden="true">Close</button>
+              <button class="btn btn-primary">Save changes</button>
+            </div>
+          </div>
+
+          <div id="myModal3" class="modal hide fade" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+            <div class="modal-header">
+              <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
+              <h3 id="myModalLabel"></h3>
+            </div>
+            <div class="modal-body">
+              <p>One fine body…</p>
+            </div>
+            <div class="modal-footer">
+              <button class="btn" data-dismiss="modal" aria-hidden="true">Close</button>
+              <button class="btn btn-primary">Save changes</button>
+            </div>
+          </div>
+
+          <div id="myModal4" class="modal hide fade" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+            <div class="modal-header">
+              <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
+              <h3 id="myModalLabel"></h3>
+            </div>
+            <div class="modal-body">
+              <p>One fine body…</p>
+            </div>
+            <div class="modal-footer">
+              <button class="btn" data-dismiss="modal" aria-hidden="true">Close</button>
+              <button class="btn btn-primary">Save changes</button>
+            </div>
+          </div>
+
+          <div id="myModal5" class="modal hide fade" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+            <div class="modal-header">
+              <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
+              <h3 id="myModalLabel"></h3>
+            </div>
+            <div class="modal-body">
+              <p>One fine body…</p>
+            </div>
+            <div class="modal-footer">
+              <button class="btn" data-dismiss="modal" aria-hidden="true">Close</button>
+              <button class="btn btn-primary">Save changes</button>
+            </div>
+          </div>
+
+          <div id="myModal6" class="modal hide fade" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+            <div class="modal-header">
+              <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
+              <h3 id="myModalLabel"></h3>
+            </div>
+            <div class="modal-body">
+              <p>One fine body…</p>
+            </div>
+            <div class="modal-footer">
+              <button class="btn" data-dismiss="modal" aria-hidden="true">Close</button>
+              <button class="btn btn-primary">Save changes</button>
+            </div>
+          </div>
+
+          <div id="myModal7" class="modal hide fade" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+            <div class="modal-header">
+              <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
+              <h3 id="myModalLabel"></h3>
+            </div>
+            <div class="modal-body">
+              <p>One fine body…</p>
+            </div>
+            <div class="modal-footer">
+              <button class="btn" data-dismiss="modal" aria-hidden="true">Close</button>
+              <button class="btn btn-primary">Save changes</button>
+            </div>
+          </div> 
+	<!-- end modal content-->
+    </div>
+</div>
+</body>
+
+<?php get_footer(); ?>
