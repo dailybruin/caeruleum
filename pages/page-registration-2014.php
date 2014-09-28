2598fedfe4ae5e91d61513b3c5493c492ece6803
Simon Zou <simon.zou@gmail.com>
2014-09-27 19:53:51 -0700

    added registration 2014 page

---
+++ pages/page-registration-2014.php
@@ -0,0 +1,197 @@
+<?php/*
+Template Name: Registration Issue 2014
+*/ ?>
+<?php get_header(); ?>
+<link href='http://fonts.googleapis.com/css?family=Open+Sans|Open+Sans+Condensed:700' rel='stylesheet' type='text/css'>
+<style>
+    #regissue-cover {
+        background-image: url('http://dailybruin.com/images/2014/09/Background-Landing-RGB-Crop1.jpg');
+        background-size: contain;
+        background-repeat: no-repeat;
+        height: 2200px;
+    }
+
+    #cover-title {
+        text-align:center;
+        padding-top: 30px;
+        background-color: rgba(255,255,255,0.6);
+
+    }
+    #cover-title h2 {
+        font-family: 'Open Sans';
+        font-size: 3em;
+    }
+
+    #editors-note {
+        padding: 20px;
+        background-color: rgba(255,255,255,0.6);
+
+    }
+
+    .reg-section {
+        background-color: rgba(255,255,255,0.6);
+    }
+
+    .reg-section-title {
+        text-align: center;
+        font-family: 'Open Sans Condensed';
+    }
+
+    #sections {
+        padding-top: 60%;
+    }
+
+    .feature-img-1 {
+        position: relative;
+    }
+
+    .feature-img-1 h2 {
+        position: absolute;
+        top: 20px;
+        padding: 10px 20px;
+        background-color: rgba(255,255,255,0.6);
+    }
+
+    .feature-img-2 {
+        padding-top:4px;
+        position: relative;
+    }
+    .feature-img-2 h4 {
+        position: absolute;
+        top: 10px;
+        padding: 10px 20px;
+        background-color: rgba(255,255,255,0.6);
+    }
+
+    #feature-stories {
+        position: relative;
+        padding-top: 20px;
+    }
+
+    #read-all {
+        height: 50px;
+        text-align: center;
+        margin-top: 20px;
+        background-color: rgba(255,255,255,0.6);
+        font-size: 2em;
+    }
+
+    #read-all span {
+        padding-top: 15px;
+        display:inline-block; 
+        vertical-align:middle;
+    }
+
+</style>
+
+<div id="main-page-wrapper">
+
+    <div id="regissue-cover">
+        <div class="row-fluid">
+            <div class="span6">
+                <div id="cover-title">
+                    <h1>Registration Issue 2014</h1>
+                    <h2>your turn.</h2>
+                </div>
+            </div>
+            <div class="span6">
+                <div id="editors-note">
+                <p>The start of a year at UCLA brings with it many opportunities
+                    – more than 1,000 clubs, 120 majors and hundreds of on- and 
+                    off- campus activities to choose from. All of them lead to a 
+                    different result – your own college experience.</p>
+                <p>Tweet with the hashtag 
+                <a target="_blank" href="https://twitter.com/hashtag/myturn">#MyTurn</a> 
+                to share what hopes and plans 
+                you have for the year. It’s your turn to find that path, and 
+                it’s our turn to tell your stories along the way.</p>
+                </div>
+            </div>
+        </div>
+        <div class="row-fluid" id="sections">
+            <div class="span3 reg-section">
+            <h2 class="reg-section-title">News</h2>
+            <ul>
+                <li>Napolitano's Year in Office</li>
+                <li>Mental Health Campaign</li>
+                <li>The Artist Inside</li>
+                <li>Teaching From Experience</li>
+            </ul>
+            </div>
+            <div class="span3 reg-section">
+            <h2 class="reg-section-title">Sports</h2>
+            <ul>
+                <li>Chasing His Dreams</li>
+                <li>Desert Stormed</li>
+                <li>Strong Side Story</li>
+                <li>From Adapting to Impacting</li>
+            </ul>
+            </div>
+            <div class="span3 reg-section">
+            <h2 class="reg-section-title">A&E</h2>
+            <ul>
+                <li>Singer Sandra Starks</li>
+                <li>Westwood Village Memorial Park</li>
+                <li>Bruin Bash</li>
+                <li>Flavors of Westwood</li>
+            </ul>
+            </div>
+            <div class="span3 reg-section">
+            <h2 class="reg-section-title">Opinion</h2>
+            <ul>
+                <li>On Higher Education</li>
+                <li>On the Common Book</li>
+                <li>On USAC Elections</li>
+                <li>On Sexual Assault</li>
+            </ul>
+            </div>
+        </div>
+
+        <div class="row-fluid" id="read-all">
+            <span><a target="_blank" href="link">Read all registration issue stories &rarr;</a></span>
+        </div>
+
+        <div class="row-fluid" id="feature-stories" >
+            <div class="span4 feature-img-1">
+                <img src="http://dailybruin.com/images/2014/09/News-CoverPhoto.jpg">
+                <h2>Playing His Forte</h2>
+            </div>      
+            <div class="span4 feature-img-1">
+                <img src="http://dailybruin.com/images/2014/09/Sports-CoverPhoto.jpg">
+                <h2>Nothing But Net</h2>
+            </div>
+            <div class="span4">
+                <a class="twitter-timeline" href="https://twitter.com/hashtag/myturn" data-widget-id="515986078524649473">#myturn Tweets</a>
+                <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
+            </div>
+        </div>
+        <div class="row-fluid">
+            <div class="span4 feature-img-1">
+                <img src="http://dailybruin.com/images/2014/09/Opinion-CoverPhoto.jpg">
+                <h2>On Mental Health</h2>
+            </div>
+            <div class="span4 feature-img-1">
+                <img src="http://dailybruin.com/images/2014/09/AE-CoverPhoto.jpg">
+                <h2>Lighting Up LA</h2>
+            </div>
+            <div class="span4">
+                <div class="feature-img-2">
+                    <img src="http://dailybruin.com/images/2014/02/5891eb04-18c1-4e3d-9efa-242fb4fd8fb8-640x426.jpg">
+                    <h4>Timeline: Diversity Requirement Proposal</h4>
+                </div>
+                <div class="feature-img-2">
+                    <img src="http://dailybruin.com/images/2014/09/web.ae_.reg_.meatballs.picA_.AYU_.jpg">
+                    <h4>Map: Flavors of Westwood</h4>
+                </div>
+                <div class="feature-img-2">
+                    <img src="http://dailybruin.com/images/2014/09/web.ns_.reg_.skelly.onlineonly.jpg">
+                    <h4>Prison Arts Program</h4>
+                </div>
+            </div>
+        </div> <!-- end of row -->
+
+    </div><!-- end of regissue-cover -->
+
+</div> <!-- end of main-page-wrapper -->
+
+<?php get_footer(); ?>
