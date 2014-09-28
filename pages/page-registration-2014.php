d73e3a14ad15a168b0acc75efcf446ba3da1c41e
Simon Zou <simon.zou@gmail.com>
2014-09-27 21:52:56 -0700

    added links, videos to registration issue 2014

--- pages/page-registration-2014.php
+++ pages/page-registration-2014.php
@@ -8,7 +8,6 @@ Template Name: Registration Issue 2014
         background-image: url('http://dailybruin.com/images/2014/09/Background-Landing-RGB-Crop1.jpg');
         background-size: contain;
         background-repeat: no-repeat;
-        height: 2200px;
     }
 
     #cover-title {
@@ -82,6 +81,14 @@ Template Name: Registration Issue 2014
         vertical-align:middle;
     }
 
+    #main-page-wrapper a {
+        color: black;
+        text-decoration: underline;
+    }
+
+    #videos-section {
+        margin-top: 20px;
+    }
 </style>
 
 <div id="main-page-wrapper">
@@ -101,7 +108,7 @@ Template Name: Registration Issue 2014
                     off- campus activities to choose from. All of them lead to a 
                     different result – your own college experience.</p>
                 <p>Tweet with the hashtag 
-                <a target="_blank" href="https://twitter.com/hashtag/myturn">#MyTurn</a> 
+                <a href="https://twitter.com/hashtag/myturn">#MyTurn</a> 
                 to share what hopes and plans 
                 you have for the year. It’s your turn to find that path, and 
                 it’s our turn to tell your stories along the way.</p>
@@ -112,84 +119,171 @@ Template Name: Registration Issue 2014
             <div class="span3 reg-section">
             <h2 class="reg-section-title">News</h2>
             <ul>
-                <li>Napolitano's Year in Office</li>
-                <li>Mental Health Campaign</li>
-                <li>The Artist Inside</li>
-                <li>Teaching From Experience</li>
+                <li>
+                <a href="http://dailybruin.com/2014/09/28/napolitanos-first-year-as-uc-president-draws-mixed-criticism-praise/">
+                Napolitano's Year in Office
+                </a>
+                </li>
+                <li>
+                <a href="http://dailybruin.com/2014/09/28/usac-launches-all-of-us-mental-health-awareness-campaign/">
+                Mental Health Campaign
+                </a>
+                </li>
+                <li>
+                <a href="http://dailybruin.com/2014/09/28/ucla-lecturers-work-on-prison-program-allows-inmates-exposure-to-arts/">
+                The Artist Inside
+                </a>
+                </li>
+                <li>
+                <a href="http://dailybruin.com/2014/09/28/mentor-seeks-education-reform-to-benefit-underserved-youth/">
+                Teaching From Experience
+                </a>
+                </li>
             </ul>
             </div>
             <div class="span3 reg-section">
             <h2 class="reg-section-title">Sports</h2>
             <ul>
-                <li>Chasing His Dreams</li>
-                <li>Desert Stormed</li>
-                <li>Strong Side Story</li>
-                <li>From Adapting to Impacting</li>
+                <li>
+                    <a href="http://dailybruin.com/2014/09/28/freshman-defender-chase-gasper-emerges-as-one-of-nations-top-prospects/">
+                    Chasing His Dreams
+                    </a>
+                </li>
+                <li>
+                    <a href="http://dailybruin.com/2014/09/26/silencing-doubts-brett-hundley-returns-to-field-to-knock-out-asu/">
+                    Desert Stormed
+                    </a>
+                </li>
+                <li>
+                    <a href="http://dailybruin.com/2014/09/28/ucla-footballs-nate-iese-shuffles-through-many-positions-finds-niche/">
+                    Strong Side Story
+                    </a>
+                </li>
+                <li>
+                    <a href="http://dailybruin.com/2014/09/28/womens-soccers-megan-oyster-finds-her-center-through-adaptability/">
+                    From Adapting to Impacting
+                    </a>
+                </li>
             </ul>
             </div>
             <div class="span3 reg-section">
             <h2 class="reg-section-title">A&E</h2>
             <ul>
-                <li>Singer Sandra Starks</li>
-                <li>Westwood Village Memorial Park</li>
-                <li>Bruin Bash</li>
-                <li>Flavors of Westwood</li>
+                <li>
+                    <a href="http://dailybruin.com/2014/09/28/singer-continues-ucla-education-after-decades-of-performing/">
+                    Singer Sandra Starks
+                    </a>
+                </li>
+                <li>
+                <a href="http://dailybruin.com/2014/09/28/fans-pay-homage-to-marilyn-monroe-at-westwood-village-memorial-park/">
+                Westwood Village Memorial Park
+                </a>
+                </li>
+                <li>
+                <a href="http://dailybruin.com/2014/09/23/additions-to-bruin-bash-carnival-compensate-for-concert-venue-change/">
+                Bruin Bash
+                </a>
+                </li>
+                <li>
+                <a href="http://dailybruin.com/2014/09/28/flavors-of-westwood-house-of-meatballs-to-offer-affordable-authentic-italian-food/">
+                Flavors of Westwood
+                </a>
+                </li>
             </ul>
             </div>
             <div class="span3 reg-section">
             <h2 class="reg-section-title">Opinion</h2>
             <ul>
-                <li>On Higher Education</li>
-                <li>On the Common Book</li>
-                <li>On USAC Elections</li>
-                <li>On Sexual Assault</li>
+                <li>
+                <a href="http://dailybruin.com/2014/09/28/travis-fife-students-should-be-able-to-define-own-college-experience/">
+                On Higher Education
+                </a>
+                </li>
+                <li>
+                <a href="http://dailybruin.com/2014/09/28/zoey-freedman-common-books-with-contemporary-themes-could-help-students-connect/">
+                On the Common Book
+                </a>
+                </li>
+                <li>
+                <a href="http://dailybruin.com/2014/09/28/aram-ghoogasian-hostility-in-usac-fall-election-may-hinder-future-progress/">
+                On USAC Elections
+                </a>
+                </li>
+                <li>
+                <a href="http://dailybruin.com/2014/09/28/chloe-lew-more-discussion-on-sexual-assault-needed-to-foster-conscious-community/">
+                On Sexual Assault
+                </a>
+                </li>
             </ul>
             </div>
         </div>
 
         <div class="row-fluid" id="read-all">
-            <span><a target="_blank" href="link">Read all registration issue stories &rarr;</a></span>
+            <span><a href="http://dailybruin.com/tag/reg2014/">Read all registration issue stories &rarr;</a></span>
         </div>
 
         <div class="row-fluid" id="feature-stories" >
+            <a href="http://dailybruin.com/2014/09/28/persistence-proves-instrumental-to-sixth-year-bruins-success/">
             <div class="span4 feature-img-1">
                 <img src="http://dailybruin.com/images/2014/09/News-CoverPhoto.jpg">
                 <h2>Playing His Forte</h2>
-            </div>      
+            </div>   
+            </a>   
+            <a href="http://dailybruin.com/2014/09/28/reily-buechlers-father-grows-from-volleyball-coach-to-her-biggest-fan/">
             <div class="span4 feature-img-1">
                 <img src="http://dailybruin.com/images/2014/09/Sports-CoverPhoto.jpg">
                 <h2>Nothing But Net</h2>
             </div>
+            </a>
             <div class="span4">
                 <a class="twitter-timeline" href="https://twitter.com/hashtag/myturn" data-widget-id="515986078524649473">#myturn Tweets</a>
                 <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
             </div>
         </div>
         <div class="row-fluid">
+            <a href="http://dailybruin.com/2014/09/28/samah-pirzada-positive-dialogue-about-mental-health-necessary-to-erase-stigma/">
             <div class="span4 feature-img-1">
                 <img src="http://dailybruin.com/images/2014/09/Opinion-CoverPhoto.jpg">
                 <h2>On Mental Health</h2>
             </div>
+            </a>
+            <a href="http://dailybruin.com/2014/09/28/ucla-alum-creates-music-light-show-at-walt-disney-concert-hall/">
             <div class="span4 feature-img-1">
                 <img src="http://dailybruin.com/images/2014/09/AE-CoverPhoto.jpg">
                 <h2>Lighting Up LA</h2>
             </div>
+            </a>
             <div class="span4">
+                <a href="http://dailybruin.com/2014/09/28/students-faculty-continue-to-push-for-diversity-requirement/">
                 <div class="feature-img-2">
                     <img src="http://dailybruin.com/images/2014/02/5891eb04-18c1-4e3d-9efa-242fb4fd8fb8-640x426.jpg">
                     <h4>Timeline: Diversity Requirement Proposal</h4>
                 </div>
+                </a>
+                <a href="http://dailybruin.com/features/flavors-of-westwood/">
                 <div class="feature-img-2">
                     <img src="http://dailybruin.com/images/2014/09/web.ae_.reg_.meatballs.picA_.AYU_.jpg">
                     <h4>Map: Flavors of Westwood</h4>
                 </div>
+                </a>
+                <a href="http://dailybruin.com/2014/09/28/fans-pay-homage-to-marilyn-monroe-at-westwood-village-memorial-park/">
                 <div class="feature-img-2">
-                    <img src="http://dailybruin.com/images/2014/09/web.ns_.reg_.skelly.onlineonly.jpg">
-                    <h4>Prison Arts Program</h4>
+                    <img src="http://dailybruin.com/images/2014/09/web.ae_.reg_.westwoodmemorial.picB_-640x4801.jpg">
+                    <h4>Radio: Marilyn Monroe's Grave</h4>
                 </div>
+                </a>
             </div>
         </div> <!-- end of row -->
-
+        <div class="row-fluid" id="videos-section">
+            <div class="span6">
+                <h3>UCLA engineering implements 3-D printers</h3>
+                <iframe width="100%" height="315" src="//www.youtube.com/embed/dPAYMjRFwmA" frameborder="0" allowfullscreen></iframe>
+            </div>
+            <div class="span6">
+                <h3>UCLA Pronunciation Guide</h3>
+                <iframe width="100%" height="315" src="//www.youtube.com/embed/MCM3xz4QDQc" frameborder="0" allowfullscreen></iframe>
+            </div>
+        </div>
     </div><!-- end of regissue-cover -->
 
 </div> <!-- end of main-page-wrapper -->
