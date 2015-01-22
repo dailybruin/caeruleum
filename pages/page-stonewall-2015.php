045dfeae02ecf05c46f669ad945c033c212f20cb
Jimmy Yang <jimmyyang1@gmail.com>
2015-01-21 22:35:47 -0800

    changed alignment

--- pages/page-stonewall-2015.php
+++ pages/page-stonewall-2015.php
@@ -15,58 +15,48 @@ Template Name: Stonewall
 <link rel="stylesheet" href="/stylesheets/tipsy.css" type="text/css" />
 
 <style>
-h1{
-	text-align: center;
-	font-size: 40px;
-}
-h2 {
+
+#blurb {
 	text-indent: 70px;
     border: 2px solid #a1a1a1;
-    padding: 10px 40px; 
     background: #dddddd;
-    width: 1200px;
+    width: 100%;
     border-radius: 25px;
     font-size: 20px;
+}
+#blurb p {
+	padding: 10px;
+	margin-top: 5px;
+
 }
 
 .stone
 {    
     float:left;
-    width:300px;
-    height:120px;
-    margin:10px;
-    height:120px;
-    bottom: 0;
-}
-.texts
-{
-	margin: 10 auto;
-	position: relative;
-	color: white;
-	text-align: center;
-	bottom: -80px;
+    position:relative;
+    width:100/3%;
+    height:100%;
+    text-align: center;
 }
 
-.popbox {
-    display: none;
-    position: absolute;
-    z-index: 99999;
-    width: 400px;
-    padding: 10px;
-    background: #EEEFEB;
-    color: #000000;
-    border: 1px solid #4D4F53;
-    margin: 0px;
-    -webkit-box-shadow: 0px 0px 5px 0px rgba(164, 164, 164, 1);
-    box-shadow: 0px 0px 5px 0px rgba(164, 164, 164, 1);
+.texts
+{
+	position: absolute;
+	color: white;
+	display: inline-block;
+}
+
+#stonegraphic {
+	width:100%; 
+	height:100%;
 }
 
 </style>
 </head> 
 
 <body>
-	<h1>Stonewall </h1> 
-	<h2>For 96 years, the Daily Bruin has strived to hold UCLA accountable to the community it serves. We take that responsibility seriously. And when the Bruin is unjustly thwarted in its efforts to inform students, we believe you have a right to know. Each time our reporters are stonewalled in their attempts to inform readers, we will record that here, stone by stone. No stonewalling that week, no new stone. Below, you can click on each stone in the wall to read about why it's there. </h2>
+	<div id = "banner"> Stonewall </div> 
+	<div id = "blurb"><p>For 96 years, the Daily Bruin has strived to hold UCLA accountable to the community it serves. We take that responsibility seriously. And when the Bruin is unjustly thwarted in its efforts to inform students, we believe you have a right to know. Each time our reporters are stonewalled in their attempts to inform readers, we will record that here, stone by stone. No stonewalling that week, no new stone. Below, you can click on each stone in the wall to read about why it's there. </p></div>
 	<div id="stonewall">
 	</div> 
 </body>
@@ -91,12 +81,12 @@ $.getJSON( "https://spreadsheets.google.com/feeds/list/13bucW1zrZjEfI2GibmJk6pNO
 			var dis = entry.gsx$description.$t
 			$('div#stonewall').append('<div id = "s'+i+'" class="stone" title = "'+dis+'">  <div id = "t'+i+'" class = "texts">  </div> </div>');
 			//var pic = '<span style="display:none">' + entry.id.$t + '</span>';	
-			var pic = '<img src="http://dailybruin.com/images/2015/01/stone'+(i%4+1)+'.png" style="width:300px; height:120px; "/>';
+			var pic = '<img src="http://dailybruin.com/images/2015/01/stone'+(i%4+1)+'.png" id = "stonegraphic"/>';
 
 			if (entry.gsx$date.$t)
 			{
-				var item = '<br/>' + entry.gsx$date.$t;	
-				item += '<br/>' + entry.gsx$reason.$t;
+				var item =entry.gsx$date.$t + '<br/>';	
+				item += entry.gsx$reason.$t;
 			}
 
 			$('#s'+i+'').append(pic);
