5c87d6695d9f8706ac0cfa2d74b46b365057e4fe
Neil Bedi <neilbedi@gmail.com>
2015-01-29 20:54:06 -0800

    fixed 404 page

--- 404.php
+++ 404.php
@@ -1,16 +1,16 @@
 <?php get_header(); ?>
 
 <div class="row">
-	<div class="span8">
-		<div class="fourohfour">
-			<span class="f-title">Oops...</span>
-			<span class="f-desc">Looks like we couldn't find the page you were looking for.</span>
-			<span class="f-verbose">404 Error</span>
-			<p>An administrator has been notified. If you keep getting this error email us at <a href="mailto:online@media.ucla.edu">online@media.ucla.edu</a> and we'll do our best to get it fixed.</p>
-			<div class="f-search">
-				<?php get_search_form(); ?>
-			</div><!-- end div.f-search -->
-		</div><!-- end div.fourohfour -->
+	<div class="large-8 columns">
+		<div class="row">
+			<div class="fourohfour">
+					<div class="text-center">
+						<div class="f-title">Oops...</div>
+						<div class="f-desc">Looks like we couldn't find the page you were looking for. [404 Error]</div>
+						<p>An administrator has been notified. If you keep getting this error email us at <a href="mailto:online@media.ucla.edu">online@media.ucla.edu</a> and we'll do our best to get it fixed.</p>
+					</div>
+			</div><!-- end div.fourohfour -->
+		</div>
 	</div><!-- end div.span8 -->
 
 
