20308b77f62df999cc0e0e945fd1de168407f913
Daily Bruin Intern <dailybruin@dailybruin.(none)>
2015-01-31 15:17:43 -0800

    more for 2013 elections page

--- pages/page-usac-elections-2013.php
+++ pages/page-usac-elections-2013.php
@@ -611,7 +611,7 @@ Template Name: USAC Elections 2013
   <br />
   <a class="twitter-timeline" data-dnt="true" href="https://twitter.com/search?q=%23USACelections" data-widget-id="328749991814369281">Tweets about "#USACelections"</a>
   <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
-</div><!--/span3-->
+</div><!--/span 3-->
 
 </div><!--/row-->
 
