f0dd0237b5ed71358d976aad0396533c6c98418b
Daily Bruin Intern <dailybruin@dailybruin.(none)>
2015-01-31 16:36:23 -0800

    Bootstrap to Foundation

--- pages/page-radio-hosts.php
+++ pages/page-radio-hosts.php
@@ -9,6 +9,7 @@ Template Name: Radio: About the hosts
 }
 #rh-title {
     margin-bottom: 1em;
+    font-size: 35px;
 }
 .rh-infobox {
     float: right;
@@ -16,6 +17,14 @@ Template Name: Radio: About the hosts
     padding: 0.5em 1em;
     border: 1px solid #DDD;
     box-shadow: 0 4px 20px rgba(0,0,0,0.1);  
+}
+
+#subtitle {
+    font-size:25px;
+}
+
+#out-of-bounds {
+    padding-top:2cm;
 }
 
 </style>
@@ -32,14 +41,16 @@ Template Name: Radio: About the hosts
     <li><img src="/img/radio_microphone.svg" width=19 px/> <a href="http://UCLAradio.com" target="_blank">Listen live</a></li>  
  <li><i class="ticon-twitter"></i> <a href="https://twitter.com/dailybruinradio" "external" target="_blank">Follow us on Twitter</a></li>
 </ul> 
+
 <h1 id="rh-title">Radio: About the Hosts</h1>
-    <h2>Long Story Short</h2>
-        <h3>Mondays, 6:00-6:30 p.m.</h3>
+    <h2 id="subtitle">Long Story Short</h2>
+        <h1>Mondays, 6:00-6:30 p.m.</h1>
 <p>Live from the hills of Westwood, the Daily Bruin presents its award-winning weekly news and culture talk show, Long Story Short. The show began airing in the winter of 2011 as a pre-recorded podcast with one broadcast host and single segment stories, eventually evolving into a live show broadcast from the UCLA Radio studio. The show covers all walks of life at UCLA, from science and health to arts and politics to local news and campus buzz. Long Story Short aims to focus on members of the UCLA community and their projects, personalities and perspectives.</p>
+
 <div class="row-fluid">
-    <div class="span6">
+    <div class="large-6 columns">
         <img src="http://dailybruin.com/images/2015/01/Alex-Hyun.jpg" alt="Alex Hyun" />
-        <h3>Alex Hyun</h3>
+        <h1>Alex Hyun</h1>
             <div class="email"> <img src="/img/radio_email_icon.svg"/> <a href=" mailto:ahyun@media.ucla.edu">ahyun@media.ucla.edu</a> </div>
                 <div class="twitter">
                     <i class="ticon-twitter"></i>
@@ -49,10 +60,10 @@ Template Name: Radio: About the hosts
                 </div>
                 <audio src="http://dailybruin.com/images/2015/01/alexs-bio.mp3" preload="auto" controls></audio>
             <p>In addition to being a Long Story Short host, Alex is a Daily Bruin sports writer. He covered the UCLA women's and men's track teams last year, and this year he is covering the women's and men's golf teams. He is an avid fan of sports radio and was inspired by various talk shows to join Long Story Short. He hopes to pursue a career in sports journalism or a career in the film industry as a writer or critic.</p>
-            </div>
-    <div class="span6">
+    </div>
+    <div class="large-6 columns">
         <img src="http://dailybruin.com/images/2015/01/Chirag-Govardhan.jpg" alt="Chirag Govardhan" />
-        <h3>Chirag Govardhan</h3>
+        <h1>Chirag Govardhan</h1>
             <div class="email"> <img src="/img/radio_email_icon.svg"/> <a href=" mailto:cgovardhan@media.ucla.edu">cgovardhan@media.ucla.edu</a> </div>
                 <div class="twitter">
                     <i class="ticon-twitter"></i>
@@ -62,19 +73,22 @@ Template Name: Radio: About the hosts
                 </div>
                 <audio src="http://dailybruin.com/images/2015/01/chirags-bio.mp3" preload="auto" controls></audio>
                 <p>Chirag joined Daily Bruin Radio as a reporter in winter 2014, inspired after having grown up listening to legends like Garrison Keillor and the Magliozzi brothers. When he's not studying to survive his pre-med classes, you can find him scouring the campus looking for new and exciting stories for the show.</p>
-                </div>
+                <br>
     </div>
+</div>
+
 <div class="row-fluid">
-    <div class="span6">
+    <div class="large-6 columns">
         <img src="http://dailybruin.com/images/2015/01/Sharon-Lin.jpg" alt="Sharon Lin" />
-    <h3>Sharon Lin</h3>
+    <h1>Sharon Lin</h1>
         <div class="email"><img src="/img/radio_email_icon.svg"/> <a href=" mailto:slin@media.ucla.edu">slin@media.ucla.edu</a> </div>
         <audio src="http://dailybruin.com/images/2015/01/Sharons-bio.mp3" preload="auto" controls></audio>
         <p>Sharon started out as a Mojo blogger for the Daily Bruin but started working with Long Story Short over the summer. Before UCLA, she worked as an associate editor for the online component of a national fashion magazine, where she was given the opportunity to interview various fashion bloggers and designers. Her most memorable interview was with Althea Harper, the runner-up designer of "Project Runway" Season 6. Sharon is interested in pursuing a career in public health and using her background in journalism to effectively communicate health campaign messages.</p>
+        <br> <br> 
     </div>
-    <div class="span6">
+    <div class="large-6 columns">
         <img src="http://dailybruin.com/images/2015/01/Jennifer-Crane.jpg" alt="Jonaki Mehta." />
-        <h3>Jennifer Crane</h3>
+        <h1>Jennifer Crane</h1>
         <div class="email"> <img src="/img/radio_email_icon.svg"/> <a href=" mailto:jcrane@media.ucla.edu">jcrane@media.ucla.edu</a> </div>
             <div class="twitter">
             <i class="ticon-twitter" ></i>
@@ -86,13 +100,15 @@ Template Name: Radio: About the hosts
     <p>Jennifer launched her career at the Daily Bruin in 2012 as a campus news reporter. She fell in love with obituary and feature writing, having covered multiple events and written several pieces on students, professors and artists. After trying her hand as the Community Engagement Director, she branched out into Radio and has been a part of the Radio team ever since. In 2013, Crane co-hosted "Long Story Short" for a quarter before becoming a Radio reporter. This year, she hopes to bring a little more spunk to the show as well as report on interesting Bruins across campus. In her free time, she enjoys writing, traveling and eating olives.</p>
     </div>
 </div>
-<h2>Out of Bounds</h2>
-<h3>Mondays, 6:30-7:00 p.m.</h3>
+
+
+<h2 id="subtitle">Out of Bounds</h2>
+<h1>Mondays, 6:30-7:00 p.m.</h1>
 <p> For an in-depth look at the UCLA sports scene, tune into Out of Bounds, the Daily Bruin’s weekly sports talk show. The latest and greatest of UCLA Athletics are presented with the show’s famous “Westwood Week in Review,” clips from UCLA coaches and athletes, and a high-stakes prediction segment called “You Better Be Right,” where failure to pick the winning team of the week may result in Taylor Swift showdowns and changing twitter pictures to the hosts’ worst nightmares. </p>
 <div class="row-fluid">
-    <div class="span6">
+    <div class="large-6 columns">
     <img src="http://dailybruin.com/images/2014/05/andrew.jpg" alt="Andrew Erickson." />
-    <h3>Andrew Erickson</h3>
+    <h1>Andrew Erickson</h1>
         <div class="email"> <img src="/img/radio_email_icon.svg"/> <a href=" mailto:aerickson@media.ucla.edu">aerickson@media.ucla.edu</a> </div>
             <div class="twitter">
             <i class="ticon-twitter" ></i>
@@ -103,9 +119,10 @@ Template Name: Radio: About the hosts
         <audio src="http://dailybruin.com/images/2014/05/Andrew.mp3" preload="auto" controls></audio> 
         <p>Andrew is a Sports senior staff writer for the Daily Bruin, covering football and men's basketball in the 2013-2014 school year. He was an assistant Sports editor during the 2012-2013 school year and writes sports columns and features for Los Angeles Magazine's CityThink Blog. Fun fact: He and two other Daily Bruin Sports writers once drove to Eugene, Ore. for a football game (it's not as fun as it sounds). </p>
     </div>
-    <div class="span6">
+</div>
+<div class="large-6 columns">
     <img src="http://dailybruin.com/images/2014/05/kevin.jpg" alt="Kevin Bowman." />
-    <h3>Kevin Bowman</h3>
+    <h1>Kevin Bowman</h1>
         <div class="email"> <img src="/img/radio_email_icon.svg"/> <a href=" mailto:kbowman@media.ucla.edu">kbowman@media.ucla.edu</a> </div>
             <div class="twitter">
             <i class="ticon-twitter" ></i>
@@ -118,6 +135,9 @@ Template Name: Radio: About the hosts
         <p>Kevin is the Daily Bruin’s Sports editor. He has covered six different UCLA teams in his three years with the Bruin, including men's basketball and, most recently, football. He considers himself culturally illiterate in all subjects except movies, in which he pretends to know more than he does.
     </div>
 </div>
+
+
+
 <?php get_footer(); ?>
 <!--Designed and made by: Daniel Yao and Alisha Rosenweinnoss-->
 <!--Updated by Daniel Yao 1/25/2015-->
