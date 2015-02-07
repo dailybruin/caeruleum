682005e58fed023cf911c468e1eba5c0fc2e3366
Neil Bedi <neilbedi@gmail.com>
2015-02-06 17:27:59 -0800

    temp header fix complete

--- templates/header-default.php
+++ templates/header-default.php
@@ -73,63 +73,23 @@
 	</div>
     <div id="stickyalias">
 	      <nav class="tab-bar">
-	        <section class="left-small">
-	          <a class="left-off-canvas-toggle menu-icon" href="#"><span></span>
-	          	<h1 class="hide-for-small">&nbsp;MENU</h1>
-	          </a>
-	        </section>
-	        <section class="middle tab-bar-section">
-	        	<a href="<?php echo home_url('/'); ?>">
-		          	 <div class="db-logo">
-			          	<img src="<?php echo get_stylesheet_directory_uri() ; ?>/img/db_logo.svg">
-			         </div>
-			         <div class="db-logo-spectrum">
-			          	<img src="<?php echo get_stylesheet_directory_uri() ; ?>/img/db_logo_white.svg">
-			         </div>
-			     </a>
-	        </section>
-	       	<section class="text-right">
-	          <a class="search-icon" href="#" data-reveal-id="search">
-	          	<span class="hide-for-medium hide-for-small">SEARCH&nbsp;</span> <i class="fa fa-search"></i>
-	          </a>
-	        </section>
+	      <div class="alias-bar">
+	       	<div class="small-10 columns">
+	       	  <div class="db-nav-links">
+	  				<?php wp_nav_menu(array('theme_location' => 'primary_navigation', 
+								'menu_class' => 'alias-list',
+								'container' => '',
+								'menu_id' => 'topMenu',
+								)); ?>
+	          </div>
+	        </div>
+	        <div class="search-alias small-2 columns text-right">
+		      	<a class="search-icon" href="#" data-reveal-id="search">
+		          	<span class="hide-for-medium hide-for-small"></span> <i class="fa fa-search"></i>
+		        </a>
+	        </div>
+	       </div>
 	      </nav>
-
-        <aside class="left-off-canvas-menu">
-          <ul class="off-canvas-list">
-            <li class="text-center">
-            	<a href="<?php echo home_url('/'); ?>"><img class="db-mobile-nav-logo" src="<?php echo get_stylesheet_directory_uri() ; ?>/img/db_icon.svg" /></a>
-            	</li>
-            <li><div class="row collapse postfix-round ">
-              <div class="small-10 columns">
-                <input type="text" placeholder="Search">
-              </div>
-              <div class="small-2 columns">
-                <span class="button postfix"><i class="fa fa-search"></i></span>
-              </div>
-            </div></li>
-          </ul>
-          <div class="db-nav-links">
-          	<ul class="off-canvas-list"><li><label>Sections</label></li></ul>
-  				<?php wp_nav_menu(array('theme_location' => 'primary_navigation', 
-							'menu_class' => 'off-canvas-list',
-							'container' => '',
-							'menu_id' => 'topMenu',
-							)); ?>
-			<ul class="off-canvas-list"><li><label>Pages</label></li></ul>
-  				<?php wp_nav_menu(array('theme_location' => 'top_navigation', 
-							'menu_class' => 'off-canvas-list',
-							'container' => '',
-							'menu_id' => 'topMenu',
-							)); ?>
-			<ul class="off-canvas-list"><li><label>Follow Us</label></li></ul>
-  				<?php wp_nav_menu(array('theme_location' => 'share_footer', 
-							'menu_class' => 'off-canvas-list',
-							'container' => '',
-							'menu_id' => 'topMenu',
-							)); ?>
-          </div>
-        </aside>
 
     </div>
         
