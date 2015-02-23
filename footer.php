8b1b3b0dabadc743d6f3cfe07757e94792ed6b86
Neil Bedi <neilbedi@gmail.com>
2015-02-23 12:28:04 -0800

    removed jquery from footer since its added by a plugin

--- footer.php
+++ footer.php
@@ -16,15 +16,13 @@
 
   <!-- closing off-canvas-wrap db-off-canvas-wrap db-wrapper db-content inner wrap-->
 		</div>
-	</div>
-</div>
+<!-- 	</div>
+</div> -->
   <?php roots_footer_after(); ?>
 
   <?php wp_footer(); ?>
   <?php roots_footer(); ?>
 	<span style="clear:both;display:block;"></span>
-
-  <script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
   <script src="//cdnjs.cloudflare.com/ajax/libs/foundation/5.5.0/js/foundation.min.js"></script>
   <script src="<?php echo get_template_directory_uri(); ?>/js/app.js"></script> 
   <script src="<?php echo get_template_directory_uri(); ?>/js/vendor/jquery.lightSlider.js"></script> 
