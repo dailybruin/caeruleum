86c50d626cf089151fef4279259655efed35dad9
Tim Guo <timguoqk@users.noreply.github.com>
2015-04-18 15:31:49 -0700

    hide in the news

--- js/usac-2015/main.js
+++ js/usac-2015/main.js
@@ -1,5 +1,6 @@
 var candidates, keys;
 document.addEventListener("DOMContentLoaded", function(event) {
+	$(".in-the-news").hide();  // TODO: REMOVE THIS BEFORE PRODUCTION!
 	$.getJSON("../js/usac-2015/candidates.json", function(data) {
 		candidates = data;
 		keys = _.keys(candidates[0]);
