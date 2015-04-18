2ff33037fb4aca663a28a12e0b71f65f4ac1a159
Neil Bedi <neilbedi@gmail.com>
2015-04-18 15:36:47 -0700

    initial scrolling

--- pages/page-usac-elections-2015.php
+++ pages/page-usac-elections-2015.php
@@ -4,6 +4,135 @@ Template Name: USAC Elections 2015
 
 <?php get_header(); ?>
 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8" />
+<title>USAC Elections 2015</title>
+<style>
+.db-next {
+    display: none !important;
+    }
+</style>
+</head>
+<body>
 
+    <div class="row">
+        <div class="small-3 columns" style="position: fixed">
+            <dl class="side-nav">
+                <dd><a href="#president">President</a></dd>
+                <dd><a href="#ivp">Internal Vice President</a></dd>
+                <dd><a href="#evp">External Vice President</a></dd>
+                <dd><a href="#genrep">General Representative</a></dd>
+                <dd><a href="#aac">Academic Affairs Commissioner</a></dd>
+                <dd><a href="#cec">Campus Event Commissioner</a></dd>
+                <dd><a href="#csc">Community Service Commissioner</a></dd>
+                <dd><a href="#cac">Cultural Affairs Commissioner</a></dd>
+                <dd><a href="#fac">Facilities Commissioner</a></dd>
+                <dd><a href="#fsc">Financial Support Commissioner</a></dd>
+                <dd><a href="#swc">Student Wellness Commissioner</a></dd>
+                <dd><a href="#tsr">Transfer Student Representative</a></dd>
+            </dl>
+        </div>
+        <div class="small-9 columns">
+        <a name="president"></a>
+        <h3>President</h3>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
 
+        <a name="ivp"></a>
+        <h3>Internal Vice President</h3>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+
+        <a name="evp"></a>
+        <h3>External Vice President</h3>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+
+        <a name="genrep"></a>
+        <h3>General Representative</h3>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+
+        <a name="aac"></a>
+        <h3>Academic Affairs Commissioner</h3>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+
+        <a name="cec"></a>
+        <h3>Campus Event Commissioner</h3>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+
+        <a name="csc"></a>
+        <h3>Community Service Commissioner</h3>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+
+        <a name="cac"></a>
+        <h3>Cultural Affairs Commissioner</h3>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+
+        <a name="fac"></a>
+        <h3>Facilities Commissioner</h3>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+
+        <a name="fsc"></a>
+        <h3>Financial Support Commissioner</h3>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+
+        <a name="swc"></a>
+        <h3>Student Wellness Commissioner</h3>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+
+        <a name="tsr"></a>
+        <h3>Transfer Student Representative</h3>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        </div>
+</div>
+</body>
+</html>
 <?php get_footer(); ?>
