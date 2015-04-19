4b11cdafc295c504ecd64c3c68a7ad3fd5c2d972
Julien Brundrett <julien.brundret@gmail.com>
2015-04-18 23:41:43 -0700

    added slatelogo to underscore template, still need to edit json file to get mug image links

--- pages/page-usac-elections-2015.php
+++ pages/page-usac-elections-2015.php
@@ -2,10 +2,12 @@
 Template Name: USAC Elections 2015
 */ ?>
 
+<?php get_header(); ?>
+
 <link rel="stylesheet" href="../css/usac-elections-2015.css" type="text/css">
 <script src="https://cdnjs.cloudflare.com/ajax/libs/underscore.js/1.8.3/underscore-min.js"></script>
 <script src="../js/usac-2015/main.js"></script>
-<link href='http://fonts.googleapis.com/css?family=Raleway:800,700,900,400,200' rel='stylesheet' type='text/css'>
+<link href='http://fonts.googleapis.com/css?family=Raleway:800' rel='stylesheet' type='text/css'>
 
 <script type="text/template" class="template">
 	<% _.each(input, function(candidate){ %>
@@ -14,20 +16,31 @@ Template Name: USAC Elections 2015
         <img src="<%- candidate.Mug %>"/>
       </div>
       <div class="small-8 columns">
-			  <h2><%- candidate.Name %></h2>
+        <h2><%- candidate.Name %></h2>
         <hr>
-			<ul>
-				<li><%- candidate.YearMajor %></li>
-				<li><%- candidate.Slate %></li>
-				<li><%- candidate.Platforms %></li>
-			</ul>
+        <p><%- candidate.YearMajor %></p>
+         <div class="row">
+          
+          <div class="small-8 columns platforms"
+            <h2>Platform Goals</h2>
+            <ul>
+            <% _.each(candidate.Platforms, function(p) { %>
+              <li><%- p %></li>  
+            <% }); %>			
+            </ul>
+          </div>
+          
+          <div class="small-4 columns">
+            <img src="https://dl-web.dropbox.com/get/_Daily%20Bruin/USAC%2015/cmyk.letsact.logo.jpg?_subject_uid=1391304&w=AABaaE5bGVdITeKBgx0suWoVGnLrnKkbk4-VQRu94ZPajw"/>
+          </div>
+          
+        </div>
       </div>
 		</div>
 	<% }); %>
 </script>
 
 <!-- THE PAGE STARTS HERE -->
-<?php get_header(); ?>
 
 <div class="container"><div class="row">
 	<nav class="top-bar" data-topbar role="navigation">
@@ -71,32 +84,36 @@ Template Name: USAC Elections 2015
             <dd><a href="#TSR">Transfer Student Representative</a></dd>
         </dl>
     </div>
+    
+    <!-- Main content column, resizes to full on mobile -->
     <div class="small-12 medium-9 columns candidates">
-    <h3>President</h3>
-        <div class="candidates-content" id="President"></div>
-
-		<h3>Internal Vice President</h3>
-        <div class="candidates-content" id="IVP"></div>
-		<h3>External Vice President</h3>
-        <div class="candidates-content" id="EVP"></div>
-		<h3>General Representative</h3>
-        <div class="candidates-content" id="Gen-Rep"></div>
-		<h3>Academic Affairs Commissioner</h3>
-        <div class="candidates-content" id="AAC"></div>
-		<h3>Campus Event Commissioner</h3>
-        <div class="candidates-content" id="CEC"></div>
-		<h3>Community Service Commissioner</h3>
+      <h3>President</h3>
+          <div class="candidates-content" id="President"></div>
+  		<h3>Internal Vice President</h3>
+          <div class="candidates-content" id="IVP"></div>
+  		<h3>External Vice President</h3>
+          <div class="candidates-content" id="EVP"></div>
+  		<h3>General Representative</h3>
+          <div class="candidates-content" id="Gen-Rep"></div>
+  		<h3>Academic Affairs Commissioner</h3>
+          <div class="candidates-content" id="AAC"></div>
+  		<h3>Campus Event Commissioner</h3>
+          <div class="candidates-content" id="CEC"></div>
+  		<h3>Community Service Commissioner</h3>
         <div class="candidates-content" id="CSC"></div>
-		<h3>Cultural Affairs Commissioner</h3>  
-        <div class="candidates-content" id="CAC"></div>
-		<h3>Facilities Commissioner</h3>
+  		<h3>Cultural Affairs Commissioner</h3>  
+          <div class="candidates-content" id="CAC"></div>
+  		<h3>Facilities Commissioner</h3>
         <div class="candidates-content" id="FAC"></div>
-		<h3>Financial Support Commissioner</h3>
-        <div class="candidates-content" id="FSC"></div>
-		<h3>Student Wellness Commissioner</h3>
-        <div class="candidates-content" id="SWC"></div>
-		<h3>Transfer Student Representative</h3>
-        <div class="candidates-content" id="TSR"></div>    
-    </div>  <!-- column-9 ends -->
-</div></div>
+	  	<h3>Financial Support Commissioner</h3>
+          <div class="candidates-content" id="FSC"></div>
+		  <h3>Student Wellness Commissioner</h3>
+          <div class="candidates-content" id="SWC"></div>
+		  <h3>Transfer Student Representative</h3>
+          <div class="candidates-content" id="TSR"></div>    
+    </div>  
+    <!-- Main content column ends -->
+    
+</div>
+</div>
 <?php get_footer(); ?>
