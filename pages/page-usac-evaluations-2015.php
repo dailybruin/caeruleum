ec7379d0d686061cfc59338c431bdb4e418ae466
Neil Bedi <neilbedi@gmail.com>
2015-04-21 20:58:27 -0700

    added link to council

--- pages/page-usac-evaluations-2015.php
+++ pages/page-usac-evaluations-2015.php
@@ -11,10 +11,12 @@ Template Name: USAC Evaluations 2015
 	.councilmember {
 		padding: 0.5rem;
 	}
-	.councilmember:hover img {
-		opacity: 0.3;
+	.councilmember:hover img,
+	.council:hover img {
+		opacity: 0.25;
 	}
-	.councilmember:hover span.hover-details {
+	.councilmember:hover span.hover-details,
+	.council:hover h2.hover-council {
 		display:block;
 	}
 	.councilmember img:hover + span.hover-details {
@@ -92,7 +94,19 @@ Template Name: USAC Evaluations 2015
 		margin-top: 0.5rem;
 	}
 	.council {
-		background-color: lightgrey;
+		overflow: auto;
+	}
+	h2.hover-council {
+		display: none;
+		position: absolute;
+		top: 35%;
+		margin-left: auto;
+		margin-right: auto;
+		left: 0;
+		right: 0;
+		text-align: center;
+		font-size: 2.8rem;
+		margin-right: 0.5rem;
 	}
 </style>
 
@@ -102,8 +116,11 @@ Template Name: USAC Evaluations 2015
 			<h2 class="title">2014-2015 USAC Officer Evaluations</h2>
 			<p><?php the_content(); ?></p>
 			<h3 class="click">Click on the photos below to see each officer's evaluation.</h3>
-			<div class="council">
-				<h2 class="title">Council wide evaluation</h2>
+			<div class="small-12 columns council">
+				<a href="/">
+					<img src="http://dailybruin.com/images/2015/04/council2015.jpg"/>
+					<h2 class="hover-council">Council wide evaluation</h2>
+				</a>
 			</div>
 			<div class="councilmembers">
 			</div>
@@ -114,9 +131,6 @@ Template Name: USAC Evaluations 2015
 
 <script type="text/javascript"> 
 $(document).ready(function() {	
-
-var position = ["top","center","bottom"];
-//source file is https://docs.google.com/spreadsheet/ccc?key=0Ak0qDiMLT3XddHlNempadUs1djdkQ0tFLWF6ci1rUUE	
 
 $(function showstones() {
 $.getJSON( "https://spreadsheets.google.com/feeds/list/11bRISASLiQkKMJjbInzx7f3iDYJEoPBgAQzGjJKdXDA/od6/public/values?alt=json",
