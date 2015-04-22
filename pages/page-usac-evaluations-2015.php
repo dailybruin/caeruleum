8dfe43707bd0f88c51780b81187f1a809285afaa
Neil Bedi <neilbedi@gmail.com>
2015-04-21 21:02:35 -0700

    commenting out council wide for now

--- pages/page-usac-evaluations-2015.php
+++ pages/page-usac-evaluations-2015.php
@@ -116,12 +116,12 @@ Template Name: USAC Evaluations 2015
 			<h2 class="title">2014-2015 USAC Officer Evaluations</h2>
 			<p><?php the_content(); ?></p>
 			<h3 class="click">Click on the photos below to see each officer's evaluation.</h3>
-			<div class="small-12 columns council">
+			<!-- <div class="small-12 columns council">
 				<a href="/">
 					<img src="http://dailybruin.com/images/2015/04/council2015.jpg"/>
 					<h2 class="hover-council">Council wide evaluation</h2>
 				</a>
-			</div>
+			</div> -->
 			<div class="councilmembers">
 			</div>
 		</div>
