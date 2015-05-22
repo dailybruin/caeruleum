c7e3b0fd13b0dd29e336562affa441747e72c2b1
Simon Zou <simon.zou@gmail.com>
2015-05-21 19:24:05 -0700

    Added world-insight page

---
+++ pages/page-world-insight-2015.php
@@ -0,0 +1,260 @@
+<?php/*
+Template Name: World Insight 2015
+*/ ?>
+<?php get_header(); ?>
+
+<link href='http://fonts.googleapis.com/css?family=Fjalla+One' rel='stylesheet' type='text/css'>
+
+<div class="db-off-canvas-wrap db-content">
+		<div class="row">
+			<div class="main-video">
+				<img src="http://dailybruin.com/images/2015/05/10959491_414051805427468_6791572108556502351_n1.png"/>
+				<p id="wis-logo-credit">Logo courtesy of UCLA World In-Sight</p>
+				<br><br>
+				<div class="header-text">
+					On May 27th, Wednesday, UCLA’s International Students Association (ISA) will host the 2nd annual World-In-Sight exposition. The participating student cultural groups will showcase aspects of their cultures such as local cuisines, traditional clothing and music etc. This year there will be more than thirty student cultural groups participating, including a handful of student organizations that will mark their debut.  Among the debuting cultural groups, five student groups describe their journey to understand, learn and explore culture.
+					<br><br>
+					Reported by Connie Zhou
+				</div>
+			</div>
+			<div class="body">
+				<div class="where">
+					<h3 class="title">World in Sight</h3>
+					<div class="row">
+						<div class="large-4 columns">
+							<section class="audio-section">
+								<img src="http://dailybruin.com/images/2015/05/1374989_546406965443495_867170722_n.jpg" />
+								<p id="credits">Logo courtesy of UCLA International Student Association</p>
+								<h3>ISA</h3>
+								<p id="name"><strong>Min Woo Sun</strong></p>
+								<p id="major"><strong>Second Year Economics Student</strong></p>
+								<p id="role"><strong>Director of World-In-Sight</strong></p>
+								<audio src="http://dailybruin.com/images/2015/05/Min-Woo-Sun__Director-of-WIS-track.mp3" controls></audio>
+							</section>
+						</div>
+						<div class="large-4 columns">
+							<section class="audio-section">
+								<img src="http://upload.wikimedia.org/wikipedia/commons/thumb/e/e8/Flag_of_Ecuador.svg/1280px-Flag_of_Ecuador.svg.png"/>
+								<h3>Ecuador</h3>
+								<p id="name">Andrea Valencia</p>
+								<p id="major">Fourth Year Civil Engineering Student</p>
+								<p id="role">President of Bruin Ecuadorians</p> <br>
+								<audio src="http://dailybruin.com/images/2015/05/Bruin-Ecuadorian_Revised-WIS.mp3" controls></audio>
+								<p id="credits">A Mi Lindo Ecuador by Pueblo Nuevo</p>
+							</section>
+						</div>
+						<div class="large-4 columns">
+							<section class="audio-section">
+								<img src="http://upload.wikimedia.org/wikipedia/en/thumb/f/f3/Flag_of_Russia.svg/900px-Flag_of_Russia.svg.png"/>
+								<h3>Russia</h3>
+								<p id="name">Clarissa Rodriguez</p>
+								<p id="major">Fourth Year Russian Language<br>and Literature Student</p>
+								<p id="role">President of Russian Club</p>
+								<audio src="http://dailybruin.com/images/2015/05/Russian-Club-Soundtrack_Revised-WIS.mp3" controls></audio>
+								<p id="credits">Dance of the Sugar Plum Fairy by Petyr Tchaikovsky</p>
+							</section>
+						</div>
+					</div>
+					<div class="row">
+						<div class="large-4 columns">
+							<section class="audio-section">
+								<img src="http://upload.wikimedia.org/wikipedia/commons/thumb/3/34/Flag_of_El_Salvador.svg/1064px-Flag_of_El_Salvador.svg.png"/>
+								<h3>El Salvador</h3>
+								<p id="name">Rosemary Torres</p>
+								<p id="major">Second Year Human Biology Student</p>
+								<p id="role">Outreach Coordinator of Salvadoran Union Club</p> <br>
+								<audio src="http://dailybruin.com/images/2015/05/Salvadoran-Union-Club_Revised-WIS.mp3" controls></audio>
+								<p id="credits">Los Hermanos Flores by Enfermera</p>
+							</section>
+						</div>
+						<div class="large-4 columns">
+							<section class="audio-section">
+								<img src="http://upload.wikimedia.org/wikipedia/commons/thumb/e/ef/Flag_of_Hawaii.svg/1200px-Flag_of_Hawaii.svg.png"/>
+								<h3>Hawaii</h3>
+								<div class="row" id="hawaiidiv">
+									<div id="hawaii-president" class="small-6 columns">
+										<p id="name">Monica Ashley Reyes</p>
+										<p id="major">Fourth Year<br>Economics Student</p>
+									</div>
+									<div id="hawaii-president" class="small-6 columns">
+										<p id="name">Holly Mieko Kamo</p>
+										<p id="major">Fourth Year<br>Communications Student</p>
+									</div>
+								</div>
+								<p id="role">Co-Presidents of Hui O ‘Imiloa –Hawaii Club</p> 
+								<audio src="http://dailybruin.com/images/2015/05/Hawaiian-Club-Audio-track_Revised-WIS.mp3" controls></audio>
+								<p id="credits">Island Lullabye by Fuzzy Logic</p>
+							</section>
+						</div>
+						<div class="large-4 columns">
+							<section class="audio-section">
+								<img src="http://upload.wikimedia.org/wikipedia/en/thumb/9/9e/Flag_of_Japan.svg/900px-Flag_of_Japan.svg.png"/>
+								<h3>Japan</h3>
+								<p id="name">Harsh Karthik</p>
+								<p id="major">Third Year Applied Mathematics Student</p>
+								<p id="role">President of Bruin Origami Society</p> <br>
+								<audio src="http://dailybruin.com/images/2015/05/Bruin-Origami-Society_Revised-WIS.mp3" controls></audio>
+								<p id="credits">Origami by Imade Saputra</p>
+							</section>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<p style="text-align: center;">Page designed by Shannon Phu</p>
+</div>
+
+
+<style>
+a {
+  color: #0080c6;
+}
+.main-video {
+  max-width: 50rem;
+  margin-left: auto;
+  margin-right: auto;
+  margin-bottom: 2rem;
+  margin-top: 2rem;
+}
+
+.main-video img {
+	display: block;
+	margin-left: auto;
+	margin-right: auto;
+}
+
+.main-video h2{
+  margin-bottom: 1rem;
+}
+.header-text {
+  font-size: 1rem;
+  font-family: arial;
+  margin-left: 2rem;
+  margin-right: 2rem;
+}
+
+.body {
+  margin-top: 2rem;
+  margin-bottom: 2rem;
+  max-width: 65rem;
+  margin-left: auto;
+  margin-right: auto;
+  padding-left: 2rem;
+  padding-right: 2rem;
+}
+
+.db-off-canvas-wrap {
+	background-color: #EBEBEB;
+}
+
+.body .where {
+  border-top: 4px lightgrey dotted;
+  margin-top: 2rem;
+  padding-top: 1rem;
+  max-width: 60rem;
+  margin-left: auto;
+  margin-right: auto;
+}
+.body h2 {
+  font-size: 2rem;
+  margin-bottom: 1.5rem;
+}
+.body h3 {
+  font-size: 1.4rem;
+  line-height: 1.5;
+  font-family: 'Fjalla One', sans-serif;
+  text-align: center;
+}
+.body .title {
+  font-size: 2rem;
+  text-align: center;
+  margin-bottom:2rem;
+  font-family: 'Fjalla One', sans-serif;
+}
+.body p {
+  margin-bottom: 0.2rem;
+  line-height: 1.7;
+  font-size: 0.7rem;
+  font-family: sans-serif;
+  text-align: center;
+}
+
+.body section #major {
+	font-size: 0.7rem;
+}
+
+.body section #role {
+	font-style: italic; 
+}
+
+.body section #credits {
+	margin-top: 0.5rem;
+	font-size: 0.6rem;
+	text-align: center;
+	font-family: arial;
+}
+
+.body img {
+  max-height: 150px;
+  width: auto;
+  display: block;
+  margin-left: auto;
+  margin-right: auto;
+  margin-top: 2rem;
+}
+.db-ad-leaderboard {
+  display: none;
+}
+.below-header {
+  display: none !important;
+}
+.where .row {
+  margin-top: 3rem;
+}
+
+#hawaiidiv {
+	margin-top: 0;
+}
+
+section audio {
+	margin-top: 1rem;
+	width: 100%;
+}
+
+.audio-section {
+	background-color: #C2C2C2;
+	padding: 0.9rem;
+	word-wrap: break-word;
+	border-radius: 1rem;
+	margin-bottom: 2rem;
+	height: 26rem;
+  	-webkit-transition: all ease-in 0.3s;
+    -moz-transition: all ease-in 0.3s;
+    -o-transition: all ease-in 0.3s;
+    transition: all ease-in 0.3s;
+}
+
+section:hover {
+	background-color: #C2D6D6;
+	-webkit-transition: all ease-in 0.3s;
+    -moz-transition: all ease-in 0.3s;
+    -o-transition: all ease-in 0.3s;
+    transition: all ease-in 0.3s;
+}
+/*
+section #hawaii-president {
+	display: inline-block;
+	width: 7.9rem;
+	margin-left: 0.1rem;
+}*/
+
+#wis-logo-credit {
+	font-size: 0.7rem;
+	text-align: right;
+	font-family: arial;
+	padding-right: 1rem;
+	color: gray;
+}
+</style>
+
+<?php get_footer(); ?>
