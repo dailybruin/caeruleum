670a7f6103732d24ec497e0abfa6baa5a780d683
Neil Bedi <neilbedi@gmail.com>
2015-09-19 07:20:38 -0700

    administration profiles

---
+++ js/admin-profiles.js
@@ -0,0 +1,43 @@
+$(document).foundation();
+var doc = document.documentElement;
+var contentopen = false;
+var currentmug = null;
+doc.setAttribute('data-useragent', navigator.userAgent);
+
+$(document).ready(function() {
+	$(".content .person").hide();
+
+	//$(".mugs li").hover(function() { $(this).fadeTo("fast", 0.7); }, function() { $(this).fadeTo("fast", 1.0); });
+
+	$(".mugs li").click(function() {
+		var name = $(this).attr("id");
+		
+		if (contentopen) {
+			$(currentmug).fadeTo("slow", 1.0);
+			var curname = $(currentmug).attr("id");
+			$(".content #" + curname).slideUp();
+			
+			if (currentmug == this) {
+				contentopen = false;
+				currentmug = null;
+				return;
+			}
+		}
+
+		contentopen = true;
+		$(this).fadeTo("slow", 0.4);
+		currentmug = this;
+		$(".content #" + name).slideDown();
+	});
+
+
+	var div = $('.mug-container');
+ console.log(div);
+    var width = 1.17*div.width();
+    
+    div.css('height', width);  
+});
+
+function mugClick(obj) {
+
+};
---
+++ pages/page-administration-mugs.php
@@ -0,0 +1,182 @@
+<?php/*
+Template Name: UCLA Administration Profiles
+*/ ?>
+
+<?php get_header(); ?>
+
+    <title>UCLA Administration Profiles</title>
+  <meta name="description" content="Learn more about what each UCLA chancellor does in this Daily Bruin introduction of UCLA&#39;s administration."/>
+  <meta name="author" content="Daily Bruin"/>
+  <meta name="copyright" content="UCLA Student Media. Copyright 2015."/>
+
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/foundation/5.5.2/css/foundation.min.css"/>
+  <link rel="stylesheet" href="../css/admin-profiles.css"/>
+  <!-- <script src="../assets/js/modernizr.js"></script> -->
+</head>
+
+<body>
+
+  <div class="row intro">
+    <h1 id="main-title">Profiles of UCLA&#39;s Administration</h1>
+    <p>Vice chancellors and the chancellor are the highest levels of UCLA&#39;s administrative structure. Chancellor Gene Block is at the top of the hierarchy and is in charge of fulfilling the university&#39;s mission. Below Block is Executive Vice Chancellor and Provost Scott Waugh who directly oversees the nine vice chancellors. Each of the nine vice chancellor positions supervise a specific aspect of campus, with some doing behind-the-scenes work, such as legal affairs, and others doing work that engages students directly, such as in student affairs.</p>
+  </div>
+
+
+
+  <div class="row mugs">
+    <ul class="small-block-grid-2 medium-block-grid-4 large-block-grid-6">
+
+      <li id="carol-goldberg"><div class="mug-container" id="cg"></div></li>
+      <li id="janina-montero"><div class="mug-container" id="jm"></div></li>
+      <li id="jerry-kang"><div class="mug-container" id="jk"></li>
+      <li id="john-mazziotta"><div class="mug-container" id="jm2"></li>
+      <li id="james-economou"><div class="mug-container" id="je"></li>
+      <li id="gene-block"><div class="mug-container" id="gb"></li>
+      <li id="steven-olsen"><div class="mug-container" id="so"></li>
+      <li id="kevin-reed"><div class="mug-container" id="kr"></li>
+      <li id="rhea-turteltaub"><div class="mug-container" id="rt"></li>
+      <li id="scott-waugh"><div class="mug-container" id="sw"></li>
+    </ul>
+  </div>
+
+
+  <div class="row content">
+
+    <div class="person" id="carol-goldberg">
+      <div class="small-3 columns">
+        <img src="http://dailybruin.com/images/2015/09/1.jpg"/>
+      </div>
+      <div class="small-9 columns">
+        <h2>Carol Goldberg</h2>
+        <p>Carol Goldberg is the vice chancellor of Academic Personnel. She said Academic Personnel&#39;s priority is to support UCLA recruitment, retention and recognition of world-class teachers and scholars.</p>
+        <p>This year, Goldberg intends to implement a new electronic faculty information system, conduct a faculty salary equity report and support faculty who want to remain engaged with UCLA after retirement.</p>
+        <p>Outside of UCLA, Goldberg has served as Justice of the Hualapai Court of Appeals in Arizona. Between 2011 and 2014, Goldberg served on the Indian Law and Order Commission as appointed by President Barack Obama. </p>
+      </div>
+    </div>
+
+    <div class="person" id="janina-montero">
+      <div class="small-3 columns">
+        <img src="http://dailybruin.com/images/2015/09/2.jpg"/>
+      </div>
+      <div class="small-9 columns">
+        <h2>Janina Montero</h2>
+        <p>Janina Montero is the vice chancellor of Student Affairs. Student Affairs aims to support students&#39; need by providing services that support academic development and success. </p>
+        <p>Montero said she aims to improve the campus climate by providing students opportunities to explore difficult issues related to identity and culture. Montero&#39;s office will collaborate with Jerry Kang, vice chancellor for Equity, Diversity and Inclusion, to assess the program&#39;s effectiveness. </p>
+        <p>Montero&#39;s first program this year will allow new first-years and transfer students to share their diverse identities in an event called &#34;Rehearsal for Bruin Life&#34; on Sept. 21. Montero&#39;s office also wants to find ways to provide more space for student organizations and provide programs that help students prepare for careers.</p>
+        <p>Montero was the dean of Student Life at Princeton University in 1993 and the vice president of Campus Life and Student Services in 2000. </p>
+      </div>
+    </div>
+
+    <div class="person" id="jerry-kang">
+      <div class="small-3 columns">
+        <img src="http://dailybruin.com/images/2015/09/3.jpg"/>
+      </div>
+      <div class="small-9 columns">
+        <h2>Jerry Kang</h2>
+        <p>Jerry Kang is the vice chancellor for Inclusion, Diversity and Equity, a position that was created last year, after a report conducted by a former California Supreme Court Justice found UCLA insufficiently addressed claims of racial discrimination. </p>
+        <p>Kang&#39;s office aims to create the procedures and capacity to conduct independent investigations of alleged discrimination, harassment and sexual violence. Before he was selected for the position, Kang said he plans to use empirical evidence to cite cases of discrimination by comparing salaries, employee skill level and race, for example.</p>
+        <p>Professor Kang graduated magna cum laude from Harvard College and Harvard Law School, where he was a supervising editor of the Harvard Law Review. Before coming to UCLA in 1995, Kang was a clerk for the Ninth Circuit Court of Appeals. Kang is also an Asian American studies and law professor at UCLA. </p>
+      </div>
+    </div>
+
+    <div class="person" id="john-mazziotta">
+      <div class="small-3 columns">
+        <img src="http://dailybruin.com/images/2015/09/4.jpg"/>
+      </div>
+      <div class="small-9 columns">
+        <h2>John Mazziotta</h2>
+        <p>John Mazziotta is the vice chancellor of UCLA Health Sciences and CEO of UCLA Health. This year, Mazziotta&#39;s office aims to integrate education, research and patient care within the David Geffen School of Medicine, UCLA Health and other health sciences schools. To maintain UCLA hospital&#39;s high rankings, Mazziotta wants to increase philanthropic support and move forward in scientific research.</p>
+        <p>Mazziotta was previously the chair of the Department of Neurology. He established the Brain Mapping Center at UCLA, and was the principal investigator of the International Consortium for Brain Mapping, a group that aims to create the first atlas of the human brain by collecting data from thousands of human subjects. From 2012 to 2015, he was associate vice chancellor for Health Sciences and executive vice dean of the David Geffen School of Medicine.</p>
+      </div>
+    </div>
+
+    <div class="person" id="james-economou">
+      <div class="small-3 columns">
+        <img src="http://dailybruin.com/images/2015/09/5.jpg"/>
+      </div>
+      <div class="small-9 columns">
+        <h2>James S. Economou</h2>
+        <p>James S. Economou is the vice chancellor for Research. Economou&#39;s office aims to solve UCLA Grand Challenges, a term borrowed from the White House that refers to diseases, illiteracy, inequality and inaccessibility to clean water. Economou also wants to increase the entrepreneurial culture at UCLA because he believes these breakthroughs will not influence the rest of society unless they are commercialized. </p>
+        <p>Economou is also the senior surgical oncologist at the UCLA School of Medicine, a resident of the American Surgical Association and founding director of the UCLA Human Gene Medicine Program.</p>
+      </div>
+    </div>
+
+    <div class="person" id="gene-block">
+      <div class="small-3 columns">
+        <img src="http://dailybruin.com/images/2015/09/6.jpg"/>
+      </div>
+      <div class="small-9 columns">
+        <h2>Gene Block</h2>
+        <p>Gene Block is the chancellor of UCLA. As chief executive officer, he oversees the university&#39;s three-part mission of education, research and service. Block&#39;s goal is to find innovative ways of promoting UCLA&#39;s mission. He is working to fill two vice chancellor positions, vice chancellor of Legal Affairs and administrative vice chancellor.</p>
+        <p>Chancellor Block was previously the vice president and provost of the University of Virginia. As a neuroscientist, Block is researching the effects of aging in the nervous system and how it impacts biological timing. Block holds faculty appointments in psychiatry and biobehavioral sciences in the school of medicine, and in physiological science in the College of Letters and Science.</p>
+      </div>
+    </div>
+
+    <div class="person" id="steven-olsen">
+      <div class="small-3 columns">
+        <img src="http://dailybruin.com/images/2015/09/7.jpg"/>
+      </div>
+      <div class="small-9 columns">
+        <h2>Steven A. Olsen</h2>
+        <p>Steve Olson is vice chancellor and chief financial officer for UCLA. Olson is responsible for the budget and strategic planning, as well as managing the planning and design of campus buildings and overseeing UCLA&#39;s investment policies.</p>
+        <p>Olson graduated from UCLA in 1977 and holds a master&#39;s degree from UC Berkeley. He previously worked in the Legislative Analyst&#39;s Office and in the California Department of Finance. Olsen is also a member of the Board of Directors of the Geffen Playhouse, and a classically trained cellist who performs in Southern California musical communities.</p>
+      </div>
+    </div>
+
+ <div class="person" id="kevin-reed">
+      <div class="small-3 columns">
+        <img src="http://dailybruin.com/images/2015/09/8.jpg"/>
+      </div>
+      <div class="small-9 columns">
+        <h2>Kevin Reed</h2>
+        <p>Kevin Reed is the vice chancellor of legal affairs, a position which primarily provides campus-wide legal advice to administrators. In the over seven years Reed has served at UCLA, Reed defended against several prominent charges filed against the university. The University of Oregon hired Reed to serve as the chief legal adviser under the title of vice president and general counsel. He will begin his new job at Oregon on Oct. 5.</p>
+      </div>
+    </div>
+
+     <div class="person" id="rhea-turteltaub">
+      <div class="small-3 columns">
+        <img src="http://dailybruin.com/images/2015/09/9.jpg"/>
+      </div>
+      <div class="small-9 columns">
+        <h2>Rhea Turteltaub</h2>
+        <p>Rhea Turteltaub is the vice chancellor of external affairs and leads an office that aims to build connections with external entities in order to increase support for UCLA. Turteltaub is in charge of the fundraising efforts of the school at a time when UCLA faces challenges in resource allocations. </p>
+        <p>Turteltaub has held fundraising positions with institutions such as the Otis College of Art and Design, UC Berkeley and Trinity College. When Turteltaub first joined UCLA, she was the senior associate director of major gifts. </p>
+      </div>
+    </div>
+
+     <div class="person" id="scott-waugh">
+      <div class="small-3 columns">
+        <img src="http://dailybruin.com/images/2015/09/10.jpg"/>
+      </div>
+      <div class="small-9 columns">
+        <h2>Scott Waugh</h2>
+        <p>Scott Waugh is the executive vice chancellor and provost, directly overseeing the nine vice chancellor positions. Waugh&#39;s goals for the coming years are to maintain UCLA&#39;s high ranking, ensure top-quality education for all students, work with academic and administrative staff on enrollment and budget planning, and support the Centennial Campaign.  </p>
+      </div>
+    </div>
+    <!-- 
+    <div class="person" id="">
+      <div class="small-3 columns">
+        <img src="http://i.imgur.com/D5aqu2u.jpg"/>
+      </div>
+      <div class="small-9 columns">
+        <h2></h2>
+        <p></p>
+      </div>
+    </div>
+    -->
+
+  </div>
+
+  <footer class="row">
+    
+  </footer>
+
+
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/foundation/5.5.2/js/vendor/jquery.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/foundation/5.5.2/js/foundation.min.js"></script>
+  
+  <script src="../js/admin-profiles.js"></script>
+  
+<footer> Page design by <a href="mailto:jbrundrett@media.ucla.edu" target="_blank">Julien Brundrett</a> and <a href="mailto:vzhang@media.ucla.edu" target="_blank">Vivian Zhang</a>. &#169; Daily Bruin 2015</footer>
+
+<?php get_footer(); ?>
