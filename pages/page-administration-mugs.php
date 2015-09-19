541af74e6b7dab1462fa93834beb93a8d5ca9f25
Neil Bedi <neilbedi@gmail.com>
2015-09-19 11:33:53 -0700

    copy edits and style fixing

--- pages/page-administration-mugs.php
+++ pages/page-administration-mugs.php
@@ -9,13 +9,14 @@ Template Name: UCLA Administration Profiles
   <meta name="author" content="Daily Bruin"/>
   <meta name="copyright" content="UCLA Student Media. Copyright 2015."/>
 
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/foundation/5.5.2/css/foundation.min.css"/>
+  <!-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/foundation/5.5.2/css/foundation.min.css"/> -->
   <link rel="stylesheet" href="../css/admin-profiles.css"/>
   <!-- <script src="../assets/js/modernizr.js"></script> -->
 
+
   <div class="row intro">
     <h1 id="main-title">Introduction to UCLA Leadership</h1>
-    <p>Vice chancellors and the chancellor are the highest levels of UCLA&#39;s administrative structure. Chancellor Gene Block is at the top of the hierarchy and is in charge of fulfilling the university&#39;s mission. Below Block is Executive Vice Chancellor and Provost Scott Waugh who directly oversees the nine vice chancellors. Each of the nine vice chancellor positions supervise a specific aspect of campus, with some doing behind-the-scenes work, such as legal affairs, and others doing work that engages students directly, such as in student affairs.</p>
+    <p>Vice chancellors and the chancellor are the highest levels of UCLA&#39;s administrative structure. Chancellor Gene Block is at the top of the hierarchy and is in charge of fulfilling the university&#39;s mission. Below Block is Executive Vice Chancellor and Provost Scott Waugh who directly oversees the nine vice chancellors. Each of the nine vice chancellors supervises a specific aspect of campus, with some doing behind-the-scenes work, such as legal affairs, and others doing work that engages students directly, such as in student affairs.</p>
   </div>
 
 
@@ -23,7 +24,7 @@ Template Name: UCLA Administration Profiles
   <div class="row mugs">
     <ul class="small-block-grid-2 medium-block-grid-4 large-block-grid-6">
 
-      <li id="carol-goldberg"><div class="mug-container" id="cg"></div></li>
+      <li id="carole-goldberg"><div class="mug-container" id="cg"></div></li>
       <li id="janina-montero"><div class="mug-container" id="jm"></div></li>
       <li id="jerry-kang"><div class="mug-container" id="jk"></li>
       <li id="john-mazziotta"><div class="mug-container" id="jm2"></li>
@@ -39,15 +40,15 @@ Template Name: UCLA Administration Profiles
 
   <div class="row content">
 
-    <div class="person" id="carol-goldberg">
+    <div class="person" id="carole-goldberg">
       <div class="small-3 columns">
         <img src="http://dailybruin.com/images/2015/09/1.jpg"/>
       </div>
       <div class="small-9 columns">
-        <h2>Carol Goldberg</h2>
-        <p>Carol Goldberg is the vice chancellor of Academic Personnel. She said Academic Personnel&#39;s priority is to support UCLA recruitment, retention and recognition of world-class teachers and scholars.</p>
-        <p>This year, Goldberg intends to implement a new electronic faculty information system, conduct a faculty salary equity report and support faculty who want to remain engaged with UCLA after retirement.</p>
-        <p>Outside of UCLA, Goldberg has served as Justice of the Hualapai Court of Appeals in Arizona. Between 2011 and 2014, Goldberg served on the Indian Law and Order Commission as appointed by President Barack Obama. </p>
+        <h2>Carole Goldberg</h2>
+        <p>Carole Goldberg is the vice chancellor of academic personnel. She said the UCLA Academic Personnel Office&#39;s priority is to support UCLA recruitment, retention and recognition of teachers and scholars.</p>
+<p>This year, Goldberg intends to implement a new online system for staff to contact each other, conduct a faculty salary equity report and support faculty who want to remain engaged with UCLA after retirement.</p>
+<p>Outside of UCLA, Goldberg serves as a justice of the Hualapai Tribe Court of Appeals in Arizona. Between 2011 and 2014, Goldberg served on the Indian Law and Order Commission as appointed by President Barack Obama.</p>
       </div>
     </div>
 
@@ -57,10 +58,10 @@ Template Name: UCLA Administration Profiles
       </div>
       <div class="small-9 columns">
         <h2>Janina Montero</h2>
-        <p>Janina Montero is the vice chancellor of Student Affairs. Student Affairs aims to support students&#39; need by providing services that support academic development and success. </p>
-        <p>Montero said she aims to improve the campus climate by providing students opportunities to explore difficult issues related to identity and culture. Montero&#39;s office will collaborate with Jerry Kang, vice chancellor for Equity, Diversity and Inclusion, to assess the program&#39;s effectiveness. </p>
-        <p>Montero&#39;s first program this year will allow new first-years and transfer students to share their diverse identities in an event called &#34;Rehearsal for Bruin Life&#34; on Sept. 21. Montero&#39;s office also wants to find ways to provide more space for student organizations and provide programs that help students prepare for careers.</p>
-        <p>Montero was the dean of Student Life at Princeton University in 1993 and the vice president of Campus Life and Student Services in 2000. </p>
+        <p>Janina Montero is the vice chancellor of student affairs. UCLA Student Affairs aims to support students&#39; needs by providing services that support academic development and success. </p>
+<p>Montero said she aims to improve campus climate by providing students opportunities to explore difficult issues related to identity and culture. </p>
+<p>Montero&#39;s first program this year will allow new first-year and transfer students to share their diverse identities in an event called &#34;Rehearsal for Bruin Life&#34; on Sept. 21. </p>
+<p>Montero was the dean of student life at Princeton University from 1993 to 2000 and the vice president of campus life and student services at Brown University from 2000 to 2003.</p>
       </div>
     </div>
 
@@ -70,9 +71,9 @@ Template Name: UCLA Administration Profiles
       </div>
       <div class="small-9 columns">
         <h2>Jerry Kang</h2>
-        <p>Jerry Kang is the vice chancellor for Inclusion, Diversity and Equity, a position that was created last year, after a report conducted by a former California Supreme Court Justice found UCLA insufficiently addressed claims of racial discrimination. </p>
-        <p>Kang&#39;s office aims to create the procedures and capacity to conduct independent investigations of alleged discrimination, harassment and sexual violence. Before he was selected for the position, Kang said he plans to use empirical evidence to cite cases of discrimination by comparing salaries, employee skill level and race, for example.</p>
-        <p>Professor Kang graduated magna cum laude from Harvard College and Harvard Law School, where he was a supervising editor of the Harvard Law Review. Before coming to UCLA in 1995, Kang was a clerk for the Ninth Circuit Court of Appeals. Kang is also an Asian American studies and law professor at UCLA. </p>
+        <p>Jerry Kang is the vice chancellor for equity, diversity and inclusion, a position that was created last year after a report conducted by former California Supreme Court Justice Carlos Moreno found UCLA insufficiently addressed claims of racial discrimination. </p>
+<p>Kang&#39;s office aims to create the procedures and capacity to conduct independent investigations of alleged discrimination, harassment and sexual violence. Before he was selected for the position, Kang said he planned to use empirical evidence to cite cases of discrimination by comparing salaries to employee skill level, for example.</p>
+<p>Kang graduated magna cum laude from Harvard University and Harvard Law School, where he served as supervising editor of the Harvard Law Review. Before coming to UCLA in 1995, Kang was a clerk for the 9th U.S. Circuit Court of Appeals. Kang is also an Asian American studies and law professor at UCLA.</p>
       </div>
     </div>
 
@@ -82,8 +83,8 @@ Template Name: UCLA Administration Profiles
       </div>
       <div class="small-9 columns">
         <h2>John Mazziotta</h2>
-        <p>John Mazziotta is the vice chancellor of UCLA Health Sciences and CEO of UCLA Health. This year, Mazziotta&#39;s office aims to integrate education, research and patient care within the David Geffen School of Medicine, UCLA Health and other health sciences schools. To maintain UCLA hospital&#39;s high rankings, Mazziotta wants to increase philanthropic support and move forward in scientific research.</p>
-        <p>Mazziotta was previously the chair of the Department of Neurology. He established the Brain Mapping Center at UCLA, and was the principal investigator of the International Consortium for Brain Mapping, a group that aims to create the first atlas of the human brain by collecting data from thousands of human subjects. From 2012 to 2015, he was associate vice chancellor for Health Sciences and executive vice dean of the David Geffen School of Medicine.</p>
+        <p>Dr. John Mazziotta is the vice chancellor of UCLA Health Sciences and chief executive officer of UCLA Health. This year, Mazziotta&#39;s office aims to integrate education, research and patient care within the David Geffen School of Medicine at UCLA, UCLA Health and other health sciences schools. To maintain the high rankings of UCLA&#39;s medical center, Mazziotta said he wants to increase philanthropic support and move forward in scientific research across the different health schools. </p>
+<p>Mazziotta was previously the chair of the department of neurology. He established the Brain Mapping Center at UCLA and was the principal investigator of the International Consortium for Brain Mapping, a group that aims to create the first atlas of the human brain by collecting data from thousands of human subjects. From 2012 to 2015, he was associate vice chancellor for Health Sciences and executive vice dean of the David Geffen School of Medicine.</p>
       </div>
     </div>
 
@@ -93,8 +94,8 @@ Template Name: UCLA Administration Profiles
       </div>
       <div class="small-9 columns">
         <h2>James S. Economou</h2>
-        <p>James S. Economou is the vice chancellor for Research. Economou&#39;s office aims to solve UCLA Grand Challenges, a term borrowed from the White House that refers to diseases, illiteracy, inequality and inaccessibility to clean water. Economou also wants to increase the entrepreneurial culture at UCLA because he believes these breakthroughs will not influence the rest of society unless they are commercialized. </p>
-        <p>Economou is also the senior surgical oncologist at the UCLA School of Medicine, a resident of the American Surgical Association and founding director of the UCLA Human Gene Medicine Program.</p>
+        <p>.James S. Economou is the vice chancellor for research. Economou&#39;s office aims to eliminate disease as well as reduce illiteracy rates, inequality and inaccessibility to clean water through its initiative called UCLA Grand Challenges. </p>
+<p>Economou is also the senior surgical oncologist at the David Geffen School of Medicine and founding director of the UCLA Human Gene Medicine Program.</p>
       </div>
     </div>
 
@@ -104,8 +105,8 @@ Template Name: UCLA Administration Profiles
       </div>
       <div class="small-9 columns">
         <h2>Gene Block</h2>
-        <p>Gene Block is the chancellor of UCLA. As chief executive officer, he oversees the university&#39;s three-part mission of education, research and service. Block&#39;s goal is to find innovative ways of promoting UCLA&#39;s mission. He is working to fill two vice chancellor positions, vice chancellor of Legal Affairs and administrative vice chancellor.</p>
-        <p>Chancellor Block was previously the vice president and provost of the University of Virginia. As a neuroscientist, Block is researching the effects of aging in the nervous system and how it impacts biological timing. Block holds faculty appointments in psychiatry and biobehavioral sciences in the school of medicine, and in physiological science in the College of Letters and Science.</p>
+        <p>Gene Block is the chancellor of UCLA. In his capacity as chief executive officer, he seeks to promote the university&#39;s three-part mission of education, research and service in innovative ways. He is working to fill two vice chancellor positions &#8211; vice chancellor of legal affairs and administrative vice chancellor. </p>
+<p>Block was previously the vice president and provost at the University of Virginia. As a neuroscientist, Block is researching the effects of aging on the nervous system and how it impacts biological timing. Block holds faculty appointments in psychiatry and biobehavioral sciences at the medical school, and in physiological science at the College of Letters and Science.</p>
       </div>
     </div>
 
@@ -115,8 +116,8 @@ Template Name: UCLA Administration Profiles
       </div>
       <div class="small-9 columns">
         <h2>Steven A. Olsen</h2>
-        <p>Steve Olson is vice chancellor and chief financial officer for UCLA. Olson is responsible for the budget and strategic planning, as well as managing the planning and design of campus buildings and overseeing UCLA&#39;s investment policies.</p>
-        <p>Olson graduated from UCLA in 1977 and holds a master&#39;s degree from UC Berkeley. He previously worked in the Legislative Analyst&#39;s Office and in the California Department of Finance. Olsen is also a member of the Board of Directors of the Geffen Playhouse, and a classically trained cellist who performs in Southern California musical communities.</p>
+        <p>Steven A. Olsen is the acting administrative vice chancellor, as well as vice chancellor and chief financial officer. Olsen is responsible for the budget and strategic planning, as well as managing the planning and design of campus buildings and overseeing UCLA&#39;s investment policies, including endowments.</p>
+<p>Olsen graduated from UCLA in 1977 and holds a master&#39;s degree from UC Berkeley. He previously worked in the Legislative Analyst&#39;s Office and in the California Department of Finance. Olsen is also a member on the board of directors at the Geffen Playhouse, and a classically trained cellist who performs in Southern California musical communities.</p>
       </div>
     </div>
 
@@ -126,7 +127,7 @@ Template Name: UCLA Administration Profiles
       </div>
       <div class="small-9 columns">
         <h2>Kevin Reed</h2>
-        <p>Kevin Reed is the vice chancellor of legal affairs, a position which primarily provides campus-wide legal advice to administrators. In the over seven years Reed has served at UCLA, Reed defended against several prominent charges filed against the university. The University of Oregon hired Reed to serve as the chief legal adviser under the title of vice president and general counsel. He will begin his new job at Oregon on Oct. 5.</p>
+        <p>Kevin Reed is the vice chancellor of legal affairs, a position which primarily provides campuswide legal advice to administrators. In the over seven years Reed has served at UCLA, Reed defended against several prominent charges filed against the university. The University of Oregon hired Reed to serve as the chief legal adviser under the title of vice president and general counsel. He will begin his new job at Oregon on Oct. 5. </p>
       </div>
     </div>
 
@@ -137,7 +138,7 @@ Template Name: UCLA Administration Profiles
       <div class="small-9 columns">
         <h2>Rhea Turteltaub</h2>
         <p>Rhea Turteltaub is the vice chancellor of external affairs and leads an office that aims to build connections with external entities in order to increase support for UCLA. Turteltaub is in charge of the fundraising efforts of the school at a time when UCLA faces challenges in resource allocations. </p>
-        <p>Turteltaub has held fundraising positions with institutions such as the Otis College of Art and Design, UC Berkeley and Trinity College. When Turteltaub first joined UCLA, she was the senior associate director of major gifts. </p>
+<p>Turteltaub has held fundraising positions with institutions such as the Otis College of Art and Design, UC Berkeley and Trinity College. When Turteltaub first joined UCLA, she was the senior associate director of major gifts.</p>
       </div>
     </div>
 
@@ -147,7 +148,8 @@ Template Name: UCLA Administration Profiles
       </div>
       <div class="small-9 columns">
         <h2>Scott Waugh</h2>
-        <p>Scott Waugh is the executive vice chancellor and provost, directly overseeing the nine vice chancellor positions. Waugh&#39;s goals for the coming years are to maintain UCLA&#39;s high ranking, ensure top-quality education for all students, work with academic and administrative staff on enrollment and budget planning, and support the Centennial Campaign.  </p>
+        <p>Scott Waugh is the executive vice chancellor and provost, directly overseeing the nine vice chancellor positions. Waugh&#39;s goals for the coming years are to maintain UCLA&#39;s high ranking, ensure top-quality education for all students, work with academic and administrative staff on enrollment and budget planning and support the Centennial Campaign. </p>
+<p>Prior to his current position, Waugh was the dean of the UCLA Division of Social Sciences for 14 years and taught in the history department. </p>
       </div>
     </div>
     <!-- 
