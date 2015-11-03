822014489c6d38c9082acef5effe82b4f08aa124
vivz <vivian-keke@hotmail.com>
2015-11-02 17:18:23 -0800

    ugh one more try

--- header.php
+++ header.php
@@ -62,7 +62,8 @@
   <?php else: ?>
     <meta property="og:image" content="http://dailybruin.com/images/2014/01/facebook_default.jpg" />
   <?php endif; ?>
-  	<meta property="og:description" content="<?php echo wp_trim_words(($post->post_content),30); ?>"/>
+    <?php $excerpt = wp_trim_words(($post->post_content),30)?>
+  	<meta property="og:description" content="<?php echo $excerpt; ?>"/>
 
 </head>
 
