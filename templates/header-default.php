42bc976d19714808920717e0ba8b29434da84d38
Neil Bedi <neilbedi@gmail.com>
2015-11-24 13:43:23 -0800

    twitter link seems to work now

--- templates/header-default.php
+++ templates/header-default.php
@@ -191,8 +191,8 @@
 					</div>
 
 					<div id="war-twitter-badge">
-						<a href="https://twitter.com/dailybruin" class="twitter-follow-button" data-show-count="false">Follow @dailybruin</a>
-<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script>
+						<a href="https://twitter.com/dailybruin" class="twitter-follow-button" data-show-count="true">Follow @dailybruin</a>
+						<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script>					
 					</div>
      
 					<div id="war-fb-badge" class="fb-like" data-href="https://www.facebook.com/dailybruin" data-width="400px" data-layout="button_count" data-action="like" data-show-faces="true" data-share="false"></div>
