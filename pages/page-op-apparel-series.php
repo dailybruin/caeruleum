7ddb0f0b2a24be5f9c044ab0b85d91570fc1976b
Desiree <desireelenart@gmail.com>
2016-01-15 21:10:05 -0800

    Opinion apparel series initalization.

---
+++ pages/page-op-apparel-series.php
@@ -0,0 +1,288 @@
+<?php/*
+Template Name: Opinion Apparel Series
+*/ ?>
+<?php get_header(); ?>
+
+<link href='../css/page-op-apparel-series.css' rel='stylesheet' type='text/css'>
+
+<div class="container">
+	<div class="row">
+		<div class="large-12 columns" id="content">
+			<div class="header-area">
+				<!-- <h2>By  <a class="author" title="Posts by Anastasia Lukianchikov" href="http://dailybruin.com/author/anastasia-lukianchikov/" rel="author">
+					Anastasia Lukianchikov</a></h2> -->
+				<section class="top-bar-section">
+	                <ul class="tabs" data-tab data-options="deep_linking:true; scroll_to_content: false">
+	                  <li class="tab-title active"><a href="#article-1">Part I</a></li>
+	                  <li class="tab-title"><a href="#article-2">Part II</a></li>
+	                  <li class="tab-title"><a href="#article-3">Part III</a></li>
+	                </ul>
+	             </section>
+         	</div>
+             <div class="tabs-content">
+				  <div class="content active" id="article-1">
+				  <h1 class="part1"> UC should hold licensees responsible to higher ethical standards </h1>
+				  	<figure>
+				  		<img class="feature-image .intense" src="http://dailybruin.com/images/2016/01/1-11-16_jansportedit_KB.jpg"/>
+				  		<figcaption>(Kelly Brennan/Daily Bruin senior staff)</figcaption>
+					</figure>
+				    <p class="first-paragraph"><span class="dropcap">T</span>he University of California has a listening problem.</p>
+					
+					<p>Last summer, the opportunity to commit UC apparel licensees to more stringent labor codes arose. 
+						Instead of doing so, policymakers made a commitment of passive leniency. And what’s worse, 
+						individual UCs seem barred from disagreement.</p>
+					
+					<p>The rise and fall of these ethical standards began with a historic controversy with JanSport – 
+						one of UCLA’s main licensees.</p>
+
+					<p>In April 2013 there was a disastrous building collapse in Dhaka, Bangladesh. More than 1,000 people
+					 died and more than 2,000 were injured. The Rana Plaza building housed several factories, some of which 
+					 produced apparel for VF Corporation, the parent company of JanSport.</p>
+
+					<p>The collapse spurred the creation of the Accord on Fire and Building Safety in Bangladesh. The Accord 
+					is a legally binding agreement between brands and trade unions to ensure worker safety, disclose factory 
+					locations, allow independent inspections and commit to remediation. The Accord is signed by over 200 brands,
+					retailers from over 20 countries, two global trade unions, eight Bangladesh trade unions and four nongovernmental
+					organization witnesses. It is also endorsed by the Worker Rights Consortium (WRC) and United Students Against 
+					Sweatshops (USAS).</p>
+
+					<p>VF Corporation refused to sign the Accord and responded by joining the Alliance for Bangladesh Worker Safety.</p>
+
+					<p>VF Corporation is a founding and funding member of the Alliance. Even more dubious is that the Alliance is neither
+					 legally binding nor obligates signatories to financial remediation. The Alliance has also had differing factory 
+					 inspection results compared to the Accord.</p>
+
+					 <a href="http://dailybruin.com/images/2016/01/map.png"><img clas="demo-image" src="http://dailybruin.com/images/2016/01/map.png"/></a>
+
+					<p>During all this time the UC system was monitoring the developments.</p>
+
+					<p>The UC Code of Conduct for Trademark Licensees Steering Committee listened to representatives from both the Accord
+					 and the Alliance. After a year of investigation, the committee unanimously voted for all UCs to adopt the Accord. 
+					 But in June, University of California President Janet Napolitano decided differently.</p>
+
+					<p>The Alliance is an embarrassingly poor imitation of the Accord. Licensing companies that are part of the Alliance
+					 is an affront to the honest efforts of Accord members and an affront to the moral standards of the UC system. 
+					 The UCs need to be committed to the Accord alone.</p>
+
+					<p>The decision by the UC president came as a surprise and disappointment.</p>
+
+					<p>“Just like that, the unanimous opinion of the committee was reversed,” says Richard Appelbaum, a UC Santa Barbara 
+						faculty member of the UC Code of Conduct Steering Committee.</p>
+
+					<p>The reversal was supposedly justified by stating that both the Alliance and Accord have seen progress since inception.
+					 But more obviously, the tangible evidence and data from a devoted committee’s work was seemingly overturned by 
+					 vacuous pleas.</p>
+
+					<p>“The head of the Alliance sent a letter to the [UC] president where it says not to believe the allegations made
+					 by the WRC and USAS,” said Appelbaum.</p>
+
+					<p class="quote">The UC president also received a letter from the CEO of VF Corporation stating the company has been “wrongly and 
+						unfairly targeted by USAS” and that “we consistently place worker safety as a high priority in every factory.”</p>
+
+					<p>Unanimity – difficult to achieve on any issue – reflects the seriousness of VF Corporation’s violations. But the break
+					 from this recommendation gives doubt as to how loudly these ethical concerns are heard in the first place, not to 
+					 mention a conflict of interests.</p>
+
+					<p>But the story doesn’t end there. When UCSB tried to hold its licensees to only the Accord, it was barred from doing so.</p>
+
+					<p>During the summer of 2015, the UCSB chapter of USAS urged the school to pull JanSport products from its bookstore. 
+						After the announcement from the UCOP, Appelbaum and USAS then led an initiative for UCSB to comply solely with 
+						the Accord – breaking the UC-wide policy.</p>
+
+					<p>“The students and board supported it unanimously and we thought it would be OK to hold UCSB [and licensees] to a 
+						higher standard,” said Appelbaum. “But the UC legal office ruling said that the decision 
+						[by the UC president for the UCs] was binding.”</p>
+
+					<p>The message was clear: no single UC should break from the UC-wide policy on trademark licensing.</p>
+
+					<p>Other U.S. universities responded much earlier and more definitively to infringements by VF Corporation.</p>
+
+					<p>NYU put all new orders with JanSport on hold in May 2014. Cornell cut ties with JanSport in October 2014. 
+						By February 2015, 16 U.S. universities had cut ties with JanSport, and by November 2015, 22 universities had cut 
+						ties with VF Corporation. JanSport remains licensed through UCLA.</p>
+
+					<p>JanSport’s negligence concerning worker safety in Bangladesh will most likely continue. VF Corporation 
+						continues to ignore demands and would rather face financial losses from the college market than acknowledge
+						 accountability.</p>
+
+					<p>The political, economic and ethical interests overlap greatly in these issues. I do not pretend that I am 
+						knowledgeable enough to confidently recommend whether it is more within the University’s interests to cut
+						 ties or negotiate with JanSport.</p>
+
+					<p>But an immediate action is clear: UC licensees should be required to adhere to the Accord.</p>
+
+					<p>The announcement requiring UCs to adopt either the Accord or the Alliance was a mistake, and it diminished 
+						the UC Code of Conduct Steering Committee’s entire year of work. It is hard enough to investigate and 
+						substantiate the histories of apparel companies, but those efforts become worthless and despairing 
+						if policymakers do not even listen to these findings.</p>
+
+					<p>Labor rights abuses in the garment industry are living and widespread. Change amidst large apparel companies
+					 is slow and incremental because we are fighting economic incentive. And change becomes harder to realize the
+					  more we grow complicit and impassive. The opportunity to preferentially adopt the Accord among the UCs was a rare 
+					  chance, but through rare chances come the beginning of change. Last year, we lost this chance.</p>
+
+					<p>Drop the Alliance and perhaps we can redeem that chance.</p>
+				  </div>
+				  <div class="content" id="article-2">
+				  <h1 class="part2"> UCLA Athletics must examine brand accountability in apparel deals </h1>
+				    <p class="first-paragraph"><span class="dropcap">U</span>CLA Athletics has a huge decision to make. And it has nothing to do with athletes.</p>
+
+					<p>In 2017, the licensing contract between Adidas and UCLA Athletics is set to be renewed or terminated.</p>
+
+					<p>Inevitably, speculations will begin to circulate as to whether UCLA will continue its relationship with Adidas
+					 or switch to another competing brand. Perhaps we will license Nike and follow in the footsteps of other Pac-12 members.
+					  Or maybe, if unexpectedly, Under Armour.</p>
+
+					<p>But beneath all of these brands lies the sordid not-so-secret secret of the garment industry: a torrent of labor
+					 rights abuses, worker safety violations and infringements on worker freedoms.</p>
+
+					<p>This is a widely known reality. And yet it has lost its urgency. UCLA Athletics has a duty to recall the breadth
+					 of the violations beneath Adidas – or any major brand it licenses. Athletics should consider a split from Adidas
+					  entirely and a jump to Under Armour. If this is impossible, then a renewed contract with Adidas must come with
+					   additional bargaining that assesses improved labor conditions over time.</p>
+
+					<p>There are few other legitimate alternatives for universities with competitive sports teams. Few, if any, companies
+					 are equipped to provide the need for custom uniforms, equipment, mass orders and delivery to schools as large as UCLA.</p>
+
+					<p>Which means that economics has become tangled with politics and ethics. So there is a very precarious balance between
+					 pushing apparel companies to meet better standards while still maintaining a safe business.</p>
+
+					<p>But it’s evident that the imbalance has shifted too far toward the employer. To start, let’s look
+					 at Adidas’ recent history.</p>
+
+					<p>In late 2010, labor rights violations began to worsen at the PT Kizone factory in Indonesia. In April 2011,
+					 the owner fled and the factory closed without paying an estimated $1.8 million in severance pay to 2,800 workers.
+					  Adidas reportedly produced through PT Kizone throughout this time.</p>
+
+					<p>The incident was assessed by the Worker Rights Consortium – the major independent monitoring organization that
+					 investigates and reports on labor rights at factories producing for global collegiate apparel brands. Adidas
+					  refused to financially contribute a cent of due severance. Nike paid $500,000.</p>
+
+					<p>Then, things got awkward for Adidas in 2012 when a number of new violations came to light.</p>
+
+					<p>In April, near the London Olympic Games, allegations flew that the Great Britain team uniforms manufactured by
+					 Adidas came from sweatshops.</p>
+
+					<p>In July, four more incidents erupted in the media.</p>
+
+					<p>More than a thousand workers were fired after going on strike at the PT Panarub Industry factory in Indonesia.
+					 The company was the main global manufacturer of Adidas soccer cleats.</p>
+
+					<p>More than a hundred workers contracted occupational diseases from unsafe working conditions at Dynamica Casting
+					 in Guangzhou, China. The company produced golf clubs for Adidas’ TaylorMade Golf Company.</p>
+
+					<p>Workers suffering from occupational benzene poisoning demanded medical attention and compensation at the Xingang
+					 Shoe Company in Dongguan, China. Benzene is a carcinogen found in glues used for shoe manufacturing. This company
+					  produced footwear for Adidas.</p>
+
+					<p>Simultaneous protests also took place in four Central American countries and Caribbean islands over labor rights
+					 violations toward Gildan Activewear. Gildan Activewear was Adidas and Nike’s biggest supplier in the
+					  Western Hemisphere.</p>
+
+					<p>By then some U.S. universities began to question their relationship with Adidas. Cornell University cut ties completely.</p>
+
+					<p>And during this time, United Students Against Sweatshops was conducting a “Badidas” campaign to force Adidas
+					 to compensate the workers of PT Kizone. Amid international pressure, the campaign ended in victory.</p>
+
+					<p>Adidas has a fresh history of failing to defend its workers overseas. These workers are not guaranteed a
+					 living wage, safe working conditions, the right to organize or bargain nor due severance or compensation.</p>
+
+					<p>So would Nike be the lesser of the two evils? Think again.</p>
+
+					<p>Nike has a notorious sweatshop image that stretches back to the 1990s. I will not go into detail here about
+					 its history because UCLA Athletics does not license Nike. But take it as an assertion that Nike seems no better
+					  than Adidas. Issues of low wages, forced over-time and verbal and physical abuse return throughout the years.
+					   USAS ran a successful campaign against Nike in 2010 for failing to pay severance to workers in Honduras.</p>
+
+					<p>The only claim that Nike could possibly make over Adidas is greater transparency. In the past decade Nike
+					 has disclosed its overseas factories, acknowledged issues with labor safety and worked with organizations like
+					  the WRC to improve worker conditions.</p>
+
+					<p>That is, until now.</p>
+
+					<p>In November, Nike informed the WRC that it will no longer allow them to independently monitor their factories
+					 that produce collegiate apparel. This was reaffirmed in December  when Nike communicated to universities that
+					  the WRC was not allowed to access or assess their supplier factories. Only organizations approved by Nike itself,
+					   such as the Fair Labor Association, are entitled to such privileges.</p>
+
+					<p>Nike is a giant. If it is allowed to keep this decision, it will encourage other companies to do the same.
+					 Supporting or licensing Nike at this time is to all but endorse labor rights violations.</p>
+
+					<p>Which leaves us with Under Armour.</p>
+
+					<p>I could not find a single article detailing labor rights abuses at factories for Under Armour.</p>
+
+					<p>However, that doesn’t mean Under Armour has no violations. It is a younger company and may possibly
+					 not be subject to the same scrutiny. And, more importantly, many companies produce through the same
+					  factories or regions.</p>
+
+					<p>“Because most brands rely on a globalized model of manufacturing, the odds are that one [brand’s production]
+					 isn’t better than the other,” said Morgan Currier, a national organizer for USAS.</p>
+
+					<p>Universities are fighting against the shared and far-reaching power of sports apparel companies. 
+						And while UCLA holds itself to higher standards, it is difficult to coerce companies to do the same.</p>
+
+					<p>“The problem is within the entire market for, say, Adidas, the college market is small. And within the
+					 college market, the UCLA market is even smaller,” said Patrick Healey, the ASUCLA director of apparel
+					  and accessories. “We prefer to maintain our relationships with these companies so that we can still
+					   work on these issues.”</p>
+
+					<p>Healey also said that there is movement toward better accountability though the process is moving slowly.
+					 And this process goes back to a question of balance. The ultimate goal is to hold apparel companies to better
+					  standards, but this goal comes with a trade-off. Sometimes it is better to cut ties with a company. Other
+					   times it is more worthwhile to continue a loyal relationship and build incremental change. And this
+					    trade-off is a balance of ethical, professional, economic and political interests.</p>
+
+					<p>Alone, the most UCLA can do is push for better accountability from companies at a glacial pace. 
+						It will take united momentum from student campaigns, economic alternatives and multiple universities
+						 – no small feats in themselves. But this begins with smaller efforts from UCLA Athletics. It is not
+						  enough for one party to care – we have to care together.</p>
+
+					<p>But even if they care, UCLA Athletics must be more transparent in their calculation of interests. When asked
+					 about future licenses, they released this statement:</p> <p class="quote">“UCLA Athletics is currently in a long-term, multi-sport
+					  shoe and apparel contract with Adidas, and discussing industry competitors would be inappropriate.”</p> <p> Without
+					   transparency, we cannot know the degree to which ethical interests and standards are represented.</p>
+
+					<p>As for the immediate future of the sports contract, Under Armour is our best bet for a more ethical licensee.
+					 While the company probably has labor issues, it does not have the history of abuse or negligence like Nike or
+					  Adidas. They may even be more amenable to improvement.</p>
+
+					<p>Do not dare do business with Nike at this time. If UCLA licenses the company, then we are all but endorsing
+					 labor rights violations. If we must do business, then Nike must allow the WRC access to Nike’s suppliers.</p>
+
+					<p>If there are no other options then, for now, license Adidas. But do so with the understanding that the
+					 company must work to fit UCLA’s commitment to higher standards of accountability.</p>
+
+					<p>It is never easy, but UCLA must continue to uphold its standards, values and code of conduct. And while UCLA 
+						is trying diligently, we cannot let these issues fade into a monotonous background. We have to revisit the
+						 history of these issues again year after year to assess what has changed. If we teach accountability,
+						  we must practice it too. And that responsibility only grows with this school.</p>
+				  </div>
+				  <div class="content" id="article-3">
+				  <h1 class="part3"> Grab title somehow or insert manually </h1>
+				    <p>
+				    	<?php 
+						if ( have_posts() ) {
+							while ( have_posts() ) {
+								the_post();
+								the_content();
+							} // end while
+						} // end if
+						?>
+					</p>
+				  </div>
+
+			</div> <!-- end tabs content -->
+		</div> <!-- end all content -->
+	</div> <!-- end row -->
+</div>
+
+<?php get_footer(); ?>
+
+
+
+
+
+
+
+
