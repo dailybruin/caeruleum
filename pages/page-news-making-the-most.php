bb8b5ce1256089de780f2243cb80901295f515cb
jbrundrett <julien.brundret@gmail.com>
2016-02-08 23:55:36 -0800

    making the most initial commit

---
+++ pages/page-news-making-the-most.php
@@ -0,0 +1,154 @@
+<?php/*
+Template Name: News - Making the Most
+*/ ?>
+<?php get_header(); ?>
+
+<link href="//fonts.googleapis.com/css?family=Raleway:100" rel="stylesheet" type="text/css">
+<link href="//fonts.googleapis.com/css?family=Droid+Serif" rel="stylesheet" type="text/css">
+<link href="//fonts.googleapis.com/css?family=Droid+Sans" rel="stylesheet" type="text/css">
+
+<!-- Flickity -->
+<link rel="stylesheet" media="screen" href="https://cdnjs.cloudflare.com/ajax/libs/flickity/1.1.2/flickity.css">
+<link rel="stylesheet" type="text/css" href="../css/making-the-most.css">
+
+
+<div class="container">
+
+  <div class="row title">
+    <img class="medal" id="medal1" src="http://dailybruin.com/images/2016/02/news-medal-1.jpg">
+    <h1 id="maintitle">Making the Most</h1>
+    <!-- <img class="medal" id="medal2" src="http://dailybruin.com/images/2016/02/news-medal-2.jpg"> -->
+  </div>
+
+  <div class="row content">
+
+    <p class="explainer">The Daily Bruin interviews the five True Bruin Distinguished Senior Scholarship Award winners, who were chosen based on their ability to embody integrity, excellence, accountability, respect and service.</p>
+
+    <ul class="tabs" data-tab="" data-options="deep_linking:true; scroll_to_content: false">
+      <li class="tab-title active"><a href="#Arami-Walker">Arami Walker</a></li>
+      <li class="tab-title"><a href="#Joan-Hanawi">Joan Hanawi</a></li>
+      <li class="tab-title comingsoon"><a href="">Coming Soon</a></li>
+      <li class="tab-title comingsoon"><a href="">Coming Soon</a></li>
+      <li class="tab-title comingsoon"><a href="">Coming Soon</a></li>
+    </ul>
+
+    <div class="tabs-content">
+
+      <div id="Arami-Walker" class="content">
+        <div class="Arami-Gallery">
+          <img class="gallery-img" src="http://dailybruin.com/images/2016/02/web.news_.walker.A.OE_.jpg">
+
+          <img class="gallery-img" src="http://dailybruin.com/images/2016/02/web.news_.walkerE.OE_.jpg">
+
+          <img class="gallery-img" src="http://dailybruin.com/images/2016/02/web.news_.walkerD.OE_.jpg">
+
+          <img class="gallery-img" src="http://dailybruin.com/images/2016/02/web.news_.walkerC.OE_.jpg">
+
+          <img class="gallery-img" src="http://dailybruin.com/images/2016/02/web.news_.walkerF.OE_.jpg">
+
+          <img class="gallery-img" src="http://dailybruin.com/images/2016/02/web.news_.walkerB.OE_.jpg">
+
+        </div>
+        <p class="gallerycaption">Armi Walker, a fifth-year world arts and cultures student, worked to create a bilingual poetry and song album driven by her experiences. Walker is a winner of the True Bruin Distinguished Senior Award given to students who have demonstrated excellence and service through leadership on and off campus. (Owen Emerson/Daily Bruin senior staff)</p>
+        <!-- STORY GOES HERE -->
+        <div class="article">
+          <p>In March, Arami Walker took the stage to perform with a popular Argentinian band at a crowded Buenos Aires club. She spoke to the audience of locals in Spanish, a language she only learned a couple months before.</p>
+
+          <p>Walker, a fifth-year world arts and cultures student, ran into the band in a bar earlier that night. Once band members learned Walker had singing experience, they invited her to perform their last song of the concert with them.</p>
+
+          <p>For Walker, one of the five winners of the True Bruin Distinguished Senior Award, it was just another day in her time abroad and her history of performing.</p>
+
+          <p>The True Bruin Distinguished Senior Award is given to students who have demonstrated excellence and service through leadership on and off campus, according to the scholarship's website. </p>
+
+          <p>When she was younger, Walker and her friends would write and perform skits for their families, but Walker's parents urged her to become a lawyer because they felt her love for public speaking would be better suited to a practical field, she said.</p>
+
+          <p>Walker spent part of her fourth year studying abroad in Argentina with a guitarist, who reinforced Walker's decision to devote her time at UCLA to performing arts. </p>
+
+          <p>"He told me, 'If this is what you want to do, this is what you have to do every single day,'" Walker said. "'Stop wasting your time doing what people think you should be doing, and go for it with all of your heart.'"</p>
+
+          <p>After returning to UCLA in spring, Walker began to create a bilingual poetry and song album, driven by her experiences in South America. </p>
+
+          <p>"If I dedicate as much energy to music as I am now, by the time I&#8217;m 30, I'll know that what I leave behind on earth is representative of who I am as a person," said Walker. "If I die tomorrow, I want today to be filled with everything I love."</p>
+
+          <p>Walker's close friend and UCLA alumnus Zakk Marquez said Walker's enthusiasm is clear in her powerful onstage presence.</p>
+
+          <p>"If you've ever seen her perform, she is the sun," Marquez said.  "She demands attention."</p>
+
+          <p>Walker has also performed with the UCLA Sex Squad, a group that aims to bring attention to sexual health through artistic expression at Los Angeles Unified School District high schools.</p>
+
+          <p>As a fourth-year student, Walker performed a weekly piece that focused on how hip-hop music affected her perception of self-worth. </p>
+
+          <p>"All of our lives ... we&#8217;re singing songs about our own bodies," Walker said. "What we don&#8217;t realize is that we&#8217;re chanting mantras of how our bodies aren&#8217;t worth any more (than) their physical shell." </p>
+
+          <p>She said students approached her after the shows to tell her they really connected with her message. </p>
+
+          <p>"I had a 15-year-old girl come up to me and tell me that I changed her life," Walker said. "She wanted to start writing poetry, and ... start paying attention (to the lyrics in songs)." </p>
+
+          <p>Walker's boyfriend, fifth-year anthropology student Drew Frye, said he thinks Walker's ability to connect with her audience is valuable when she is discussing health and identity with high schoolers.</p>
+
+          <p>"She&#8217;s used that presence to connect with the hearts and minds of thousands of high schoolers, to push them to see past who media tells them they should be, and be who they really are," Frye said.</p>
+
+          <p> Walker said her family was always supportive of her decision to join the group, but she truly felt the impact of her performances when she saw her conservative grandmother approach Walker's gay best friend and commend him for his performance.</p>
+
+          <p>"I actually saw in my own family a change in perception about queer people, which was really awesome for me," Walker said.</p>
+
+          <p>Walker said she aims to create dialogue through her poetry, which focuses on the common human experience. </p>
+
+          <p>Her first poem, which she wrote when she was a first-year, addressed how people use the word "n-----". Walker said  she thought her own experience of growing up in a biracial household gave her a unique perspective on the often-controversial issue she addresses.</p>
+
+          <p>"My mom has blue eyes and straight hair, and I have brown eyes and brown skin," Walker said. "Looking into my mom&#8217;s eyes and seeing how much she loved me every day, I never really  understood racism until I got to college."</p>
+
+          <p>She performed the poem at an event held by an African-American fraternity, in front of her parents and grandparents. Walker said the pride she saw in their faces encouraged her to pursue poetry. </p>
+
+          <p>Peter Sellars, one of Walker's world arts and cultures professors, said her dedication to bringing new perspectives to others has helped her in her projects both inside and outside his class.</p>
+
+          <p>During her time in Sellars' class, which focused on the Black Lives Matter movement, Walker incorporated poetry and images into a piece she performed in front of 400 students.</p>
+
+          <p>"The kind of power and commitment (she has) is really special," Sellars said. "She's going to do anything she wants to (in the future), and the world is going to watch."</p>
+
+          <p>Walker said she plans to continue exploring her passions for health education and performance art in the future, and foresees a career that allows her to have a voice in social issues. </p>
+
+          <p>"I definitely aim to be a world leader," Walker said. "I plan to have my voice heard across nations, whatever that may take."</p>
+        </div>
+      </div>
+
+      <div id="Joan-Hanawi" class="content">
+      </div>
+
+    </div>
+
+  </div>
+
+</div>
+
+
+<!-- <article>
+  <div class= "full-series">
+   <?php 
+   if ( have_posts() ) {
+   while ( have_posts() ) {
+   the_post();
+   the_content();
+ } // end while
+} // end if
+?>
+ </div>
+</article>  -->
+
+
+<!-- SCRIPTS -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/flickity/1.1.2/flickity.pkgd.js"></script>
+<script>
+  (function() {
+    $(".Arami-Gallery").flickity({
+      imagesLoaded: true,
+      percentPosition: false,
+      wrapAround: true
+    });
+
+    // Force a window resize -- fixes problems with the gallery lol
+    window.dispatchEvent(new Event('resize'));
+  })();
+</script>
+
+<?php get_footer(); ?>
