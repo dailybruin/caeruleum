57e4a24ab7c2aebccd5709f0455b303555568d17
Howard Huang <howardhuang96@gmail.com>
2016-02-09 22:37:58 -0800

    bike lane flat page

---
+++ pages/page-op-bike-lane.php
@@ -0,0 +1,58 @@
+<?php/*
+Template Name: Opinion Bike Lane
+*/ ?>
+<?php get_header(); ?>
+
+<link href='../css/page-op-bike-lane.css' rel='stylesheet' type='text/css'>
+<div class="container">
+	 <div class="row">
+		   <div class="large-12 columns" id="content">
+			      <div class="header-area">
+                <div class="title-wrap">
+  			           <h1 class="title-primary">
+  				               <span>Bike Lanes</span>
+    				      </h1>
+                </div>
+                <div class="centered-div">
+                  <iframe width="100%" height="100%" src="https://www.youtube.com/embed/UFrul7xWbws" frameborder="0" allowfullscreen></iframe>
+                </div>
+			          <section class="top-bar-section">
+	                <ul class="tabs" data-tab data-options="deep_linking:true; scroll_to_content: false">
+		                <div class="wrap">
+		                	<ul class="tabs" data-tab data-options="deep_linking:true; scroll_to_content: false">
+							           <li class="tab-title active"><a href="#article-1" class="button" >Opinion</a></li>
+							           <li class="tab-title"><a href="#article-2" class="button" >Blog</a></li>
+							        </ul>
+						        </div>
+					        </ul>
+	             </section>
+         	</div>
+          <div class="tabs-content">
+				    <div class="content active" id="article-1">
+               <h2>
+                 By
+                 <a class="author" title="Posts by Jonathan Friedland" href="http://dailybruin.com/author/jonathan-friedland/" rel="author">
+                   Jonathan Friedland
+                 </a>
+               </h2>
+
+
+			    </div>
+          <div class="content" id="article-2">
+            <h2>
+              By
+              <a class="author" title="Posts by Arthur Wang" href="http://dailybruin.com/author/arthur-wang/" rel="author">
+                 Arthur Wang
+               </a>
+            </h2>
+
+
+
+
+          </div> <!-- content for tabs -->
+		   </div> <!-- end all content -->
+	   </div> <!-- end row -->
+ </div>
+</div>
+
+<?php get_footer(); ?>
