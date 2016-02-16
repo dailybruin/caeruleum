ce569eae62f701730c90b8a85eff07ec945cc05a
Desiree <desireelenart@gmail.com>
2016-02-16 12:15:18 -0800

    Starting point for Olympic Page

---
+++ pages/page-olympic-series.php
@@ -0,0 +1,68 @@
+<?php/*
+Template Name: Olympic Series
+*/ ?>
+<?php get_header(); ?>
+
+<link href='../css/page-olympic-series.css' rel='stylesheet' type='text/css'>
+<div class="container">
+	<div class="row">
+		<div class="large-12 columns" id="content">
+			<div class="header-area">
+				  <h1 class="title-primary">
+				      <span>Title</span>
+				  </h1>
+				<h2>By  <a class="author" title="Posts by Author" href="#" rel="author">
+					Author</a></h2>
+				<section class="top-bar-section">
+	                <ul class="tabs" data-tab data-options="deep_linking:true; scroll_to_content: false">
+		                <div class="wrap">
+		                	<ul class="tabs" data-tab data-options="deep_linking:true; scroll_to_content: false">
+							  <li class="tab-title active"><a href="#article-1" class="button" >Title 1</a></li>
+							  <li class="tab-title"><a href="#article-2" class="button" >Title 2</a></li>
+							  <li class="tab-title"><a href="#article-3" class="button">Title 3</a></li>
+							</ul>
+						</div>
+					</ul>
+	             </section>
+         	</div>
+             <div class="tabs-content">
+				<div class="content active" id="article-1">
+					<h3 class="part1">  Title 1 </h3>
+					<p class="first-paragraph"><span class="dropcap">U</span>CLA Athletics has a huge decision to make. And it has nothing to do with athletes.</p>		  
+				</div>
+
+				  <div class="content" id="article-2">
+				  <h3 class="part2">Title 2</h3>
+				  	<figure>
+				  		<img class="feature-image .intense" src="http://dailybruin.com/images/2016/01/1-11-16_jansportedit_KB.jpg"/>
+				  		<figcaption>(Kelly Brennan/Daily Bruin senior staff)</figcaption>
+					</figure>
+				    <p class="first-paragraph"><span class="dropcap">T</span>he University of California has a listening problem. </p>
+				    <p>Last summer, the opportunity to commit UC apparel licensees to more stringent labor codes arose. Instead of doing so, policymakers made a commitment of passive leniency. And what's worse, individual UCs seem barred from disagreement. </p><p>The rise and fall of these ethical standards began with a historic controversy with JanSport &#8211; one of UCLA's main licensees.</p>
+					<p>In April 2013 there was a disastrous <a target="_blank" rel="nofollow" href="http://www.nytimes.com/2013/04/25/world/asia/bangladesh-building-collapse.html?_r=2">building collapse</a> in Dhaka, Bangladesh. More than 1,000 people died and more than 2,000 were injured. The Rana Plaza building housed several factories.</p>
+				  </div>
+
+				  <div class="content" id="article-3">
+				  <h3 class="part3"> Title 3 </h3>
+				  	<figure>
+				  		<img class="feature-image" src="http://dailybruin.com/images/2016/01/opclothingilloedit_VC.jpg"/>
+				  		<figcaption>(Kelly Brennan/Daily Bruin senior staff)</figcaption>
+					</figure>
+				    <p class="first-paragraph"><b></b><span class="dropcap">W</span>hen it comes to consumerism, the ethical standards of UCLA may be higher than those of its own students. <br /></p><p><b></b>In the past two weeks I have written columns exploring UCLA's licensing agreements and the labor violations behind these licensed apparel companies. The garment industry has a rampant history of worker rights infringements &#8211; incidents that still persist today. In past years, UCLA has continuously worked with these companies to hold them to higher moral standards and labor codes.</p>
+					<p>The difficulty is that the university relies on energy from student campaigns to enforce these ethical commitments.
+					 Student indifference restricts the school's negotiating power.</p>
+
+			</div> <!-- end tabs content -->
+		</div> <!-- end all content -->
+	</div> <!-- end row -->
+</div>
+
+<?php get_footer(); ?>
+
+
+
+
+
+
+
+
