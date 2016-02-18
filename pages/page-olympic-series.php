bae578a37819fb28d508fc0bcb9851cd1b4ecc17
hongee <hongee.tc@gmail.com>
2016-02-17 16:56:46 -0800

    template with animated articles

--- pages/page-olympic-series.php
+++ pages/page-olympic-series.php
@@ -2,6 +2,8 @@
 Template Name: Olympic Series
 */ ?>
 <?php get_header(); ?>
+<!-- fullPage.js CSS -->
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/fullPage.js/2.7.7/jquery.fullPage.min.css">
 
 <link href='../css/page-olympic-series.css' rel='stylesheet' type='text/css'>
 <div class="container">
@@ -14,49 +16,211 @@ Template Name: Olympic Series
 				<h2>By  <a class="author" title="Posts by Author" href="#" rel="author">
 					Author</a></h2>
 				<section class="top-bar-section">
-	                <ul class="tabs" data-tab data-options="deep_linking:true; scroll_to_content: false">
-		                <div class="wrap">
-		                	<ul class="tabs" data-tab data-options="deep_linking:true; scroll_to_content: false">
-							  <li class="tab-title active"><a href="#article-1" class="button" >Title 1</a></li>
+	        <ul class="tabs">
+		        <div class="wrap">
+		          <ul class="tabs">
+							  <li class="tab-title active"><a href="#article-1" class="button">Title 1</a></li>
 							  <li class="tab-title"><a href="#article-2" class="button" >Title 2</a></li>
 							  <li class="tab-title"><a href="#article-3" class="button">Title 3</a></li>
+								<li class="tab-title">
+									<a class="button" id="carousel-next">Next</a>
+								</li>
+								<li class="tab-title">
+									<a class="button" id="carousel-prev">Prev</a>
+								</li>
 							</ul>
 						</div>
 					</ul>
 	             </section>
-         	</div>
-             <div class="tabs-content">
-				<div class="content active" id="article-1">
-					<h3 class="part1">  Title 1 </h3>
-					<p class="first-paragraph"><span class="dropcap">U</span>CLA Athletics has a huge decision to make. And it has nothing to do with athletes.</p>		  
-				</div>
+      </div>
+			<div class="slide-wrapper">
+				<div class="content-carousel set">
+					<div class="content active" id="article-1" data-index="1">
+						<div class="medium-10 columns">
+							<h3 class="part1">  Title 1 </h3>
 
-				  <div class="content" id="article-2">
-				  <h3 class="part2">Coming Soon</h3>
-				  	<figure>
-				  		<img class="feature-image .intense" src=""/>
-				  		<figcaption></figcaption>
-					</figure>
-				    <p></p>
-				  </div>
+							<p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.
+								Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit
+								amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo. Quisque sit amet est
+								et sapien ullamcorper pharetra. Vestibulum erat wisi, condimentum sed, commodo vitae, ornare sit amet, wisi.
+								Aenean fermentum, elit eget tincidunt condimentum, eros ipsum rutrum orci, sagittis tempus lacus enim ac dui.
+								Donec non enim in turpis pulvinar facilisis. Ut felis. Praesent dapibus, neque id cursus faucibus, tortor neque
+								egestas augue, eu vulputate magna eros eu erat. Aliquam erat volutpat. Nam dui mi, tincidunt quis, accumsan porttitor,
+								 facilisis luctus, metus</p>
 
-				  <div class="content" id="article-3">
-				  <h3 class="part3"> Coming Soon </h3>
-				  	<figure>
-				  		<img class="feature-image" src=""/>
-				  		<figcaption></figcaption>
-					</figure>
-			</div> <!-- end tabs content -->
+							<p class="pullquote">
+							  Here's a list of my favorite songs from The Cure in order of coolness. The Love Cats, Friday I'm In Love, Boys Don't Cry,
+							  In Between Days, Pictures of You, Close To Me, and Just Like Heaven. How is The Cure's genre gothic rock. I dunno.
+							  What even is gothic rock? This lotion makes me smell like jasmine <br> tea.
+							</p>
+
+							<p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.
+								Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit
+								amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo. Quisque sit amet est
+								et sapien ullamcorper pharetra. Vestibulum erat wisi, condimentum sed, commodo vitae, ornare sit amet, wisi.
+								Aenean fermentum, elit eget tincidunt condimentum, eros ipsum rutrum orci, sagittis tempus lacus enim ac dui.
+								Donec non enim in turpis pulvinar facilisis. Ut felis. Praesent dapibus, neque id cursus faucibus, tortor neque
+								egestas augue, eu vulputate magna eros eu erat. Aliquam erat volutpat. Nam dui mi, tincidunt quis, accumsan porttitor,
+								 facilisis luctus, metus</p>
+
+							<p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.
+								Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit
+								amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo. Quisque sit amet est
+								et sapien ullamcorper pharetra. Vestibulum erat wisi, condimentum sed, commodo vitae, ornare sit amet, wisi.
+								Aenean fermentum, elit eget tincidunt condimentum, eros ipsum rutrum orci, sagittis tempus lacus enim ac dui.
+								Donec non enim in turpis pulvinar facilisis. Ut felis. Praesent dapibus, neque id cursus faucibus, tortor neque
+								egestas augue, eu vulputate magna eros eu erat. Aliquam erat volutpat. Nam dui mi, tincidunt quis, accumsan porttitor,
+								 facilisis luctus, metus</p>
+
+							<p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.
+								Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit
+								amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo. Quisque sit amet est
+								et sapien ullamcorper pharetra. Vestibulum erat wisi, condimentum sed, commodo vitae, ornare sit amet, wisi.
+								Aenean fermentum, elit eget tincidunt condimentum, eros ipsum rutrum orci, sagittis tempus lacus enim ac dui.
+								Donec non enim in turpis pulvinar facilisis. Ut felis. Praesent dapibus, neque id cursus faucibus, tortor neque
+								egestas augue, eu vulputate magna eros eu erat. Aliquam erat volutpat. Nam dui mi, tincidunt quis, accumsan porttitor,
+								 facilisis luctus, metus</p>
+
+							<p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.
+								Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit
+								amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo. Quisque sit amet est
+								et sapien ullamcorper pharetra. Vestibulum erat wisi, condimentum sed, commodo vitae, ornare sit amet, wisi.
+								Aenean fermentum, elit eget tincidunt condimentum, eros ipsum rutrum orci, sagittis tempus lacus enim ac dui.
+								Donec non enim in turpis pulvinar facilisis. Ut felis. Praesent dapibus, neque id cursus faucibus, tortor neque
+								egestas augue, eu vulputate magna eros eu erat. Aliquam erat volutpat. Nam dui mi, tincidunt quis, accumsan porttitor,
+								 facilisis luctus, metus</p>
+
+							<p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.
+								Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit
+								amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo. Quisque sit amet est
+								et sapien ullamcorper pharetra. Vestibulum erat wisi, condimentum sed, commodo vitae, ornare sit amet, wisi.
+								Aenean fermentum, elit eget tincidunt condimentum, eros ipsum rutrum orci, sagittis tempus lacus enim ac dui.
+								Donec non enim in turpis pulvinar facilisis. Ut felis. Praesent dapibus, neque id cursus faucibus, tortor neque
+								egestas augue, eu vulputate magna eros eu erat. Aliquam erat volutpat. Nam dui mi, tincidunt quis, accumsan porttitor,
+								 facilisis luctus, metus</p>
+
+							<p class="pullquote2">
+							  Legend of Zelda Wind Waker was amazing ok. I liked the art style. You nutsos who didn't like it... you're nuts.
+							  Other favorite Zelda games, all of them. I like A Link to the Past a lot too. Guessing Game House makes me nostalic.
+							  What's another cool game, ... probably Okami. Yeah. I got Yokai Watch for Christmas. Very fun. Very <br>rad.
+							</p>
+
+							<p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.
+								Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit
+								amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo. Quisque sit amet est
+								et sapien ullamcorper pharetra. Vestibulum erat wisi, condimentum sed, commodo vitae, ornare sit amet, wisi.
+								Aenean fermentum, elit eget tincidunt condimentum, eros ipsum rutrum orci, sagittis tempus lacus enim ac dui.
+								Donec non enim in turpis pulvinar facilisis. Ut felis. Praesent dapibus, neque id cursus faucibus, tortor neque
+								egestas augue, eu vulputate magna eros eu erat. Aliquam erat volutpat. Nam dui mi, tincidunt quis, accumsan porttitor,
+								 facilisis luctus, metus</p>
+
+							<p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.
+								Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit
+								amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo. Quisque sit amet est
+								et sapien ullamcorper pharetra. Vestibulum erat wisi, condimentum sed, commodo vitae, ornare sit amet, wisi.
+								Aenean fermentum, elit eget tincidunt condimentum, eros ipsum rutrum orci, sagittis tempus lacus enim ac dui.
+								Donec non enim in turpis pulvinar facilisis. Ut felis. Praesent dapibus, neque id cursus faucibus, tortor neque
+								egestas augue, eu vulputate magna eros eu erat. Aliquam erat volutpat. Nam dui mi, tincidunt quis, accumsan porttitor,
+								 facilisis luctus, metus</p>
+
+						 </div>
+					</div>
+
+					<div class="content" id="article-2" data-index="2">
+						  <h3 class="part2">Coming Soon</h3>
+						  	<figure>
+						  		<img class="feature-image .intense" src=""/>
+						  		<figcaption></figcaption>
+							</figure>
+						    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+					</div>
+
+					<div class="content" id="article-3" data-index="3">
+						  <h3 class="part3"> Coming Soon </h3>
+						  	<figure>
+						  		<img class="feature-image" src=""/>
+						  		<figcaption></figcaption>
+							</figure>
+							<p>
+								Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+							</p>
+					</div>
+
+				</div> <!-- end content carousel -->
+			</div> <!-- end wrapper -->
 		</div> <!-- end all content -->
 	</div> <!-- end row -->
 </div>
+<!-- JS -->
+<script>
+	$(document).ready(function(){
+		/*Thanks http://madewithenvy.com/ecosystem/articles/2015/exploring-order-flexbox-carousel/ */
 
+		var articleCount = $('.content').length;
+		var $nextBtn = $('#carousel-next');
+		var $prevBtn = $('#carousel-prev');
+		var $articleCarousel = $('.content-carousel');
+
+		var articleCarousel = {
+			animateLeft: function() {
+				$articleCarousel.removeClass('reverse');
+				$articleCarousel.removeClass('set');
+				setTimeout(function() { $articleCarousel.addClass("set") }, 50);
+			},
+			animateRight: function() {
+				$articleCarousel.addClass('reverse');
+				$articleCarousel.removeClass('set');
+				setTimeout(function() { $articleCarousel.addClass("set") }, 50);
+			},
+			setHeight: function(height) {
+				//hack to fix the height of the container
+				$articleCarousel.css('max-height',height);
+			},
+			handleNext: function() {
+				var $activeEl = $('.content.active');
+				var index =$activeEl.data("index");
+
+				$activeEl.removeClass('active');
+				if(index < articleCount) {
+					var $newEl = $('.content[data-index='+ (index+1) +']').addClass('active');
+				} else {
+					var $newEl = $('.content[data-index=1]').addClass('active');
+				}
+				articleCarousel.animateLeft();
+				articleCarousel.setHeight($newEl.outerHeight());
+			},
+			handlePrev: function() {
+				var $activeEl = $('.content.active');
+				var index =$activeEl.data("index");
+
+				$activeEl.removeClass('active');
+				if(index > 1) {
+					var $newEl = $('.content[data-index='+ (index-1) +']').addClass('active');
+				} else {
+					var $newEl = $('.content[data-index=3]').addClass('active');
+				}
+				articleCarousel.animateRight();
+				articleCarousel.setHeight($newEl.outerHeight());
+			}
+		}
+
+		//Handlers
+		$nextBtn.click(articleCarousel.handleNext);
+		$prevBtn.click(articleCarousel.handlePrev);
+		$articleCarousel.on("swipeleft", articleCarousel.handleNext);
+		$articleCarousel.on("swiperight", articleCarousel.handlePrev);
+		$("body").keydown(function(e){
+			//left arrow
+			if(e.which == 37) {
+				articleCarousel.handlePrev();
+			}
+			//right arrow
+			if(e.which == 39) {
+				articleCarousel.handleNext();
+			}
+		})
+
+	});
+
+</script>
 <?php get_footer(); ?>
-
-
-
-
-
-
-
-
