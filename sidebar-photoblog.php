e0127bb1764a8db55624fdf2e26afafc0a448c16
shannonphu <shannonphu@sbcglobal.net>
2016-02-20 16:03:39 -0800

    add nav new categories and fix styling

--- sidebar-photoblog.php
+++ sidebar-photoblog.php
@@ -2,14 +2,18 @@
 	<div id="sidebar-inside">
 <!--       	<?php get_template_part('ad','side'); ?>
  -->      	<div id="sidebar-reference"> </div>
-		<div class="sidebar-menu hidden-phone">
-			<hr />
+		<!-- <div class="sidebar-menu hidden-phone"> -->
+			<hr>
 	        <!-- <?php wp_nav_menu( array( 'theme_location' => 'photoblog_sidebar' ) );?> -->
-			<a class="sidebar-link" href="/category/blogs/spectrum">All Stories</a>
-	        <a class="sidebar-link" href="/category/spectrum/sports-spectrum">Sports</a>
+	        <div class="row">
+				<a class="sidebar-category small-3 columns" href="/category/blogs/spectrum">All Stories</a>
+				<a class="sidebar-category small-3 columns" href="/category/spectrum/ae-spectrum">A&E</a>
+		        <a class="sidebar-category small-3 columns" href="/category/spectrum/news-spectrum">News</a>
+		        <a class="sidebar-category small-3 columns" href="/category/spectrum/sports-spectrum">Sports</a>
+	        </div>
 	        <hr />
             <a class="sidebar-link" href="/category/photo-galleries">Archive</a> 
 	        <a class="sidebar-link" href="/about-spectrum"> <?php _e( 'About Spectrum', 'gryphondor' ); ?></a>
-     	 </div><!--sidebar-menu-->
+     	 <!--</div>--><!--sidebar-menu-->
   </div>
 </aside>
