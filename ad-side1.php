f4701a0ad43ab136d8b2fc778ef667a9ad3d3135
Simon Zou <simzou@users.noreply.github.com>
2016-02-23 12:34:56 -0800

    Revert "replaced ad links per vendor request"

--- ad-side1.php
+++ ad-side1.php
@@ -3,7 +3,7 @@
 
 
 <!--/* OpenX IFrame tag */-->
-<iframe id="cd2bfd42bc" name="cd2bfd42bc" src="//oncampusweb-d.openx.net/w/1.0/afr?auid=536871805&cb=<?php echo $rand_num; ?>" frameBorder="0" frameSpacing="0" scrolling="no" width="300" height="250"><a href="//oncampusweb-d.openx.net/w/1.0/rc?cs=cd2bfd42bc&cb=<?php echo $rand_num; ?>" ><img src="//oncampusweb-d.openx.net/w/1.0/ai?auid=536871805&cs=cd2bfd42bc&cb=<?php echo $rand_num; ?>" border="0" alt=""></a></iframe>
+<iframe id="cd2bfd42bc" name="cd2bfd42bc" src="http://ox-d.oncampusweb.com/w/1.0/afr?auid=536871805&cb=<?php echo $rand_num; ?>" frameBorder="0" frameSpacing="0" scrolling="no" width="300" height="250"><a href="http://ox-d.oncampusweb.com/w/1.0/rc?cs=cd2bfd42bc&cb=<?php echo $rand_num; ?>" ><img src="http://ox-d.oncampusweb.com/w/1.0/ai?auid=536871805&cs=cd2bfd42bc&cb=<?php echo $rand_num; ?>" border="0" alt=""></a></iframe>
 
 <!-- 				<img class="ad visible-desktop" src="http://placehold.it/300x250&text=ADVERTISEMENT" /> -->
 </div>
