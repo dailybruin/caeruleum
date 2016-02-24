b21db01ed52d512199995d93bbea8808b1fd33c6
Desiree <desireelenart@gmail.com>
2016-02-24 09:20:33 -0800

    Added in content

--- pages/page-olympic-series.php
+++ pages/page-olympic-series.php
@@ -54,58 +54,54 @@ Template Name: Olympic Series
 					</div>
 					<div class="row">
 						<div class="small-12 medium-9 large-9 medium-push-1 columns">
-							<h3 class="part1">  Headline Here Thanks Here </h3>
+							<h3 class="part1">  Former coach, player Adam Krikorian upholds legacy of excellence </h3>
 							<h4>By
-								<a class="author" title="Posts by Author" href="#" rel="author">
-									Firstname Lastname
+								<a class="author" title="Posts by Author" href="http://dailybruin.com/author/melissa-zhang/" rel="author">
+									Melissa Zhang
 								</a>
 							</h4>
-							<h2>Daily Bruin Senior Staff</h2>
+							<h2>Sports Writer</h2>
 							<div class="first-article">
-								<p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.
-									Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit
-									amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo. Quisque sit amet est
-									et sapien ullamcorper pharetra. Vestibulum erat wisi, condimentum sed, commodo vitae, ornare sit amet, wisi.
-									Aenean fermentum, elit eget tincidunt condimentum, eros ipsum rutrum orci, sagittis tempus lacus enim ac dui.
-									Donec non enim in turpis pulvinar facilisis. Ut felis. Praesent dapibus, neque id cursus faucibus, tortor neque
-									egestas augue, eu vulputate magna eros eu erat. Aliquam erat volutpat. Nam dui mi, tincidunt quis, accumsan porttitor,
-									 facilisis luctus, metus</p>
+								<p>Adam Krikorian's Manhattan Beach home office isn't clean, but it isn't messy either – it's cluttered.</p>
+
+								<p>Boxes line the wall of the office, filled with past photos and awards. Without the time to frame or hang them, 
+									the women's water polo national team coach simply lets them sit there.</p>
+
+								<p>Krikorian would use this space more often, he just doesn't have time with his busy travel schedule. 
+									The women's national team is flying to places like Gouda, Netherlands, to qualify for the upcoming 
+									Summer Olympics, and Shanghai, China. Needless to say, free time is rare these days.</p>
+							
+								<p>In one corner of the office is a framed John Wooden's "Pyramid of Success" with a personal note from
+								 the famous basketball coach. It's the same one that only a handful of other UCLA coaches can proudly display.</p>
+
 
 								<div class="pullquote">
 									<p class="pullquotetext">
-									  "Here's a list of my favorite songs from The Cure in order of coolness. The Love Cats, Friday I'm In Love, Boys Don't Cry,
-									  In Between Days, Pictures of You, Close To Me, and Just Like Heaven. How is The Cure's genre gothic rock. I dunno.
-									  What even is gothic rock? This lotion makes me smell like jasmine tea."
+									  "When you end up deciding to coach at a place like UCLA, there's an unspoken rule that you're signing on
+									   to be a disciple of coach Wooden whether you knew him or didn't know him."
 									</p>
-									<p class="quote-author">Author Lastname</p>
+									<p class="quote-author">Krikorian</p>
 								</div>
 
-								<p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.
-									Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit
-									amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo. Quisque sit amet est
-									et sapien ullamcorper pharetra. Vestibulum erat wisi, condimentum sed, commodo vitae, ornare sit amet, wisi.
-									Aenean fermentum, elit eget tincidunt condimentum, eros ipsum rutrum orci, sagittis tempus lacus enim ac dui.
-									Donec non enim in turpis pulvinar facilisis. Ut felis. Praesent dapibus, neque id cursus faucibus, tortor neque
-									egestas augue, eu vulputate magna eros eu erat. Aliquam erat volutpat. Nam dui mi, tincidunt quis, accumsan porttitor,
-									 facilisis luctus, metus</p>
+								 <p>“When I think back to my time, I certainly think of the great athletes and assistant coaches that I had,
+								  but I can't help but to think of coach Wooden's influence not only on me, but the entire university," Krikorian said.</p>
 
-								<p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.
-									Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit
-									amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo. Quisque sit amet est
-									et sapien ullamcorper pharetra. Vestibulum erat wisi, condimentum sed, commodo vitae, ornare sit amet, wisi.
-									Aenean fermentum, elit eget tincidunt condimentum, eros ipsum rutrum orci, sagittis tempus lacus enim ac dui.
-									Donec non enim in turpis pulvinar facilisis. Ut felis. Praesent dapibus, neque id cursus faucibus, tortor neque
-									egestas augue, eu vulputate magna eros eu erat. Aliquam erat volutpat. Nam dui mi, tincidunt quis, accumsan porttitor,
-									 facilisis luctus, metus</p>
+								  <p>Krikorian saw how Wooden’s lessons were applicable to countless pursuits, including water polo. To this day, 
+								  	he attributes much of his inspiration for his leadership style to the teachings of the revered coaching giant.</p>
 
-								<p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.
-									Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit
-									amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo. Quisque sit amet est
-									et sapien ullamcorper pharetra. Vestibulum erat wisi, condimentum sed, commodo vitae, ornare sit amet, wisi.
-									Aenean fermentum, elit eget tincidunt condimentum, eros ipsum rutrum orci, sagittis tempus lacus enim ac dui.
-									Donec non enim in turpis pulvinar facilisis. Ut felis. Praesent dapibus, neque id cursus faucibus, tortor neque
-									egestas augue, eu vulputate magna eros eu erat. Aliquam erat volutpat. Nam dui mi, tincidunt quis, accumsan porttitor,
-									 facilisis luctus, metus</p>
+
+								 <p>“When you end up deciding to coach at a place like UCLA, there's an unspoken rule that you're signing on to be a disciple
+								  of coach Wooden whether you knew him or didn't know him," Krikorian said. “You better follow those principles,
+								   those values and those standards.”</p>
+
+
+								<p>One of the most successful names in the sport, Krikorian is currently in his seventh year as coach of the USA Water Polo 
+									Women's Senior National Team. Before he began his national coaching career, Krikorian made significant contributions 
+									to UCLA water polo as a player, assistant coach and head coach.</p>
+
+								<p>A four-year letterman and two-time captain for the men’s team during his collegiate playing career, Krikorian guided the
+								 Bruins to their first national championship since 1972 in 1995. His influence among his teammates earned him the title of
+								  “Most Inspirational Player” during his last three seasons.</p>
 
 								<div class="pullimage">
 									<img src="http://dailybruin.com/images/2016/02/web.sp_.olympic.feat_.KC_.PicB_.jpg"/>
@@ -118,51 +114,72 @@ Template Name: Olympic Series
 									</div>
 								</div>
 
-								<p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.
-									Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit
-									amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo. Quisque sit amet est
-									et sapien ullamcorper pharetra. Vestibulum erat wisi, condimentum sed, commodo vitae, ornare sit amet, wisi.
-									Aenean fermentum, elit eget tincidunt condimentum, eros ipsum rutrum orci, sagittis tempus lacus enim ac dui.
-									Donec non enim in turpis pulvinar facilisis. Ut felis. Praesent dapibus, neque id cursus faucibus, tortor neque
-									egestas augue, eu vulputate magna eros eu erat. Aliquam erat volutpat. Nam dui mi, tincidunt quis, accumsan porttitor,
-									 facilisis luctus, metus</p>
+								<p>After graduation, Krikorian began serving the program as a coach and his impact only grew from there. 
+									Krikorian racked up seven NCAA titles while at the helm of the women’s team. Amassing a trove of honors, 
+									he has been named National Women’s Water Polo Coach of the Year on five different occasions. </p>
 
-								<p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.
-									Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit
-									amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo. Quisque sit amet est
-									et sapien ullamcorper pharetra. Vestibulum erat wisi, condimentum sed, commodo vitae, ornare sit amet, wisi.
-									Aenean fermentum, elit eget tincidunt condimentum, eros ipsum rutrum orci, sagittis tempus lacus enim ac dui.
-									Donec non enim in turpis pulvinar facilisis. Ut felis. Praesent dapibus, neque id cursus faucibus, tortor neque
-									egestas augue, eu vulputate magna eros eu erat. Aliquam erat volutpat. Nam dui mi, tincidunt quis, accumsan porttitor,
-									 facilisis luctus, metus</p>
+								
 
-								<div class="pullquote2">
+								<p>In both his roles as a player and coach, Krikorian helped win an unprecedented 15 national
+								 championships in men’s and women’s water polo.</p>
+
+								<p>Brandon Brooks, the current coach for UCLA's women’s team, has seen numerous Bruins graduate and join the coaching ranks 
+									at other schools. As a result, the shared principles of Krikorian and Wooden have shaped teams like the University of
+									 Hawai'i, UC Santa Barbara and UC San Diego, to name a few.</p>
+
+							
+
+								<p>“You can imagine how teams across the country are taking early UCLA teams' success and putting that into their own teams,” Brooks said.
+								 “It’s great for our sport, it’s great for our national teams and it’s great for our UCLA teams. For us, we have to continue to 
+								 push and get better because I know personally that the other coaches expect excellence as well.”</p>
+
+								 <p> <b>Bringing two worlds together</b></p>
+
+								 <p>Since his national coaching debut in 2009, Krikorian has led the U.S. team to capture the gold in nine of 12 FINA Championships, including a gold medal at the 2012 Olympic Games in London.</p>
+								 <p>Hoping to represent the United States in Rio this summer, the coach is embarking on his second Olympic journey. The first stop on the road to Rio is the Olympic Qualification Tournament in Gouda, Netherlands, from March 21 to 28.</p>
+								 <p>Just a few weeks ago, Krikorian returned to Westwood with the national team to scrimmage the current Bruins. Playing at a place filled with countless memories for members of both parties, the national team came away with a 13-4 victory.</p>
+								 <p>“They're obviously missing some players, which I've stolen from them,” Krikorian said. “They have plenty of talented players still on that team though and they look good. As always, I think there's three or four teams that can compete for a national championship and they're certainly one of them.”</p>
+								 <p>One of those stolen players is junior attacker Kodi Hill, who is taking a year off from college to play for the national team.</p>
+								 <p>“I've learned so much in the short time that I've been under him as the head coach,” Hill said. “A lot of us have been playing for 10, 12 years so I think it’s really special to show up everyday and learn something new. That's pretty rare.”</p>
+								 <p>Although many newcomers to the national team have just started playing under Krikorian, veteran Courtney Mathewson has been at his side for 12 years.</p>
+								 <p>A Bruin alum from the class of 2008, Mathewson earned Player of the Year honors and was one of five graduating seniors that year. A successful group, they became the second team and only women in UCLA history to win national titles in all four of their collegiate seasons.</p>
+								 
+								 	<div class="pullquote2">
 									<p class="pullquote2text">
-									  "Legend of Zelda Wind Waker was amazing ok. I liked the art style. You nutsos who didn't like it... you're nuts.
-									  Other favorite Zelda games, all of them. I like A Link to the Past a lot too. Guessing Game House makes me nostalic.
-									  What's another cool game, ... probably Okami. Yeah. I got Yokai Watch for Christmas. Very fun. Very rad."
+									  "I'm extremely grateful for all that he's taught me, not just in the water but specifically outside the water – 
+									  how to strive to be better and give it your all, all the time, not just when people are looking."
 									</p>
-									<p class="quote-author">Author Lastname</p>
+									<p class="quote-author">Mathewson</p>
 								</div>
 
-								<p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.
-									Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit
-									amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo. Quisque sit amet est
-									et sapien ullamcorper pharetra. Vestibulum erat wisi, condimentum sed, commodo vitae, ornare sit amet, wisi.
-									Aenean fermentum, elit eget tincidunt condimentum, eros ipsum rutrum orci, sagittis tempus lacus enim ac dui.
-									Donec non enim in turpis pulvinar facilisis. Ut felis. Praesent dapibus, neque id cursus faucibus, tortor neque
-									egestas augue, eu vulputate magna eros eu erat. Aliquam erat volutpat. Nam dui mi, tincidunt quis, accumsan porttitor,
-									 facilisis luctus, metus</p>
+								 <p>Mathewson maintained her player-coach partnership with Krikorian at the national level, citing him as a major influence on her entire athletic career.</p>
+								 <p>“It's been really neat to go on this journey together,” Mathewson said. “I'm extremely grateful for all that he's taught me, not just in the water but specifically outside the water – how to strive to be better and give it your all, all the time, not just when people are looking.”</p>
+								 <p><b>The road to Rio</b></p>
+								 <p>With seven former and current Bruins on the current U.S. national team, there is a strong UCLA presence at the international level of women’s water polo. Rachel Fattal, Hill and Alys Williams have taken time off from school to represent the U.S. in the pool during the months to come.</p>
+								 <p>Compared to the 2012 Olympic team, the current group of women is much younger and features only four returning players. Mathewson views the large number of newcomers as an asset.<p>
+								 	<p>“There's a lot of youthfulness, but it’s nice because they're full of energy and enthusiasm,” Mathewson said. “You're inspired by it and motivated to take things a little bit lighter. It’d be boring if we were all exactly the same.”</p>
+								 	<p>Still, certain challenges are inevitable at the highest level of the sport. With the national team consisting of the best water polo players in the country, some issues arise from the team’s diverse and varying collegiate backgrounds.</p>
+								 	<p>“I think one of the challenges at the Olympic level for me has been trying to get the team to mesh, quite honestly,” Krikorian said. "We have players that are supposed to play as a cohesive unit, who have spent years battling each other on the collegiate scene. When you talk about players from UCLA, USC and Stanford, those are some heavy rivalries.”</p>
+								 	<p>In order to improve the overall team dynamic, Krikorian leads without any sort of bias or preference in practices and competition. Based on their unified performances so far, it seems that he has largely rendered this challenge obsolete.</p>
+								 	<p>“I don't look at them as Bruins or Cardinal or Trojans,” said Krikorian. “What we're trying to do is form the best team possible, so it doesn't matter what university they're from. They have to make this team on their own merit, drive and work ethic.”</p>
+								 	
+								 	  <div class="pullquote">
+										<p class="pullquotetext">
+										  "What we're trying to do is form the best team possible, so it doesn't matter what university they're from. 
+										  They have to make this team on their own merit, drive and work ethic."
+										</p>
+										<p class="quote-author">Krikorian</p>
+									</div>
 
-								<p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.
-									Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit
-									amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo. Quisque sit amet est
-									et sapien ullamcorper pharetra. Vestibulum erat wisi, condimentum sed, commodo vitae, ornare sit amet, wisi.
-									Aenean fermentum, elit eget tincidunt condimentum, eros ipsum rutrum orci, sagittis tempus lacus enim ac dui.
-									Donec non enim in turpis pulvinar facilisis. Ut felis. Praesent dapibus, neque id cursus faucibus, tortor neque
-									egestas augue, eu vulputate magna eros eu erat. Aliquam erat volutpat. Nam dui mi, tincidunt quis, accumsan porttitor,
-									 facilisis luctus, metus</p>
-							</div>
+
+								 	<p>This seems true for Hill, who has built strong friendships among her teammates even with the contentions they may have had at the collegiate level.</p>
+								 	<p>“Adam makes it a point to have it be about Team USA, so there really aren't that many conversations about where everyone went to school or that sort of thing,” Hill said. “It really shows you that even if you're on different teams and you don't get along for a certain part, we're all on similar paths.”</p>
+								 	<p>For Hill and her current teammates, their path is an exciting one. This past weekend, Team USA dominated the FINA Women's Intercontinental Tournament in Lewisville, Texas, beating Australia in the final round 6-5 to claim the gold.</p>
+								 	<p>Things look promising as the qualifying tournament draws nearer. With his sights set on a second trip to the Olympics, Krikorian looks forward to another opportunity to bringing the U.S. a victory in one of the most celebrated international arenas.</p>
+								 	<p>“Honestly, that’s one of the things I enjoy most: combining forces in some way to represent something much larger than ourselves,” Krikorian said.</p>
+								 	<p>Preparing for the work, travel and press demanded by the Olympic journey, Krikorian continues to expand his extraordinary success. Cleaning the office will just have to wait.</p>
+									
+								</div>
 						</div>
 					</div>
 				</div>
