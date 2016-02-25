67cb0d9e6df60f0609f8512bb0c9d47581cf125b
Simon Zou <simzou@users.noreply.github.com>
2016-02-24 17:28:00 -0800

    Revert "Revert "replaced ad links per vendor request""

--- ad-side2.php
+++ ad-side2.php
@@ -1,7 +1,7 @@
 <div class="visible-desktop side-ads" id="side-ad-low2">
 	<?php $rand_num = mt_rand(1,999999999); ?>
 
-<iframe id="03d6bad12c" name="03d6bad12c" src="http://ox-d.oncampusweb.com/w/1.0/afr?auid=536871806&cb=<?php echo $rand_num; ?>" frameborder="0" scrolling="no" width="300" height="250"><a href="http://ox-d.oncampusweb.com/w/1.0/rc?cs=03d6bad12c&cb=<?php echo $rand_num; ?>" ><img src="http://ox-d.oncampusweb.com/w/1.0/ai?auid=536871806&cs=03d6bad12c&cb=<?php echo $rand_num; ?>" border="0" alt=""></a></iframe>
+<iframe id="03d6bad12c" name="03d6bad12c" src="//oncampusweb-d.openx.net/w/1.0/afr?auid=536871806&cb=<?php echo $rand_num; ?>" frameborder="0" scrolling="no" width="300" height="250"><a href="//oncampusweb-d.openx.net/w/1.0/rc?cs=03d6bad12c&cb=<?php echo $rand_num; ?>" ><img src="//oncampusweb-d.openx.net/w/1.0/ai?auid=536871806&cs=03d6bad12c&cb=<?php echo $rand_num; ?>" border="0" alt=""></a></iframe>
 
 <!-- 				<img class="ad visible-desktop" src="http://placehold.it/300x250&text=ADVERTISEMENT" /> -->
 </div>
