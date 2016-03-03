75cc163ae1ae8fb06efa3aed9fa1c910d9d7326c
McKenna <mckennagalvin@me.com>
2016-03-03 15:38:40 -0800

    make active shooters page work on main site

--- pages/page-radio-active-shooters.php
+++ pages/page-radio-active-shooters.php
@@ -7,12 +7,12 @@ Template Name: Active Shooters
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/foundation/6.2.0/foundation.min.css" type="text/css">
 <link rel="stylesheet" href="../css/unslider.css">
 <link rel="stylesheet" href="../css/unslider-dots.css">
-<link rel="stylesheet" href="../radio-active-shooters.css">
+<link rel="stylesheet" href="../css/radio-active-shooters.css">
 <link href='https://fonts.googleapis.com/css?family=Roboto:100,300,500,700' rel='stylesheet' type='text/css'>
 <link href='https://fonts.googleapis.com/css?family=Alegreya:400italic,700italic' rel='stylesheet' type='text/css'>
 
 
-
+<div class="main">
 
 <div class="card medium-8 medium-centered columns">
 <div style="padding: 1em;">
@@ -291,10 +291,10 @@ Template Name: Active Shooters
 
 
 </div>
-<div>
 
+</div>
 
-
+</div>
 
 
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
