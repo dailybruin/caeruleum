2f70694d72f1ed51206bd3f818ae4c619d89fe8d
oozgur <oozgur217@gmail.com>
2016-03-08 17:00:48 -0800

    Tried to implement suggested galleries

--- loop-photoblog.php
+++ loop-photoblog.php
@@ -14,7 +14,7 @@
 </div>
 <div class = "row">
 	<div class="small-12 columns" id="post">
-		<?php the_post();
+		<?php /*the_post();
 		$cats = get_the_category();
 		if ($cats) {
 		foreach($cats as $cat){//DON'T HAVE MULTIPLE CATS OTHER THAN SPECTRUM AND THE SUBCAT
@@ -22,7 +22,7 @@
 			$the_cat = $cat;
 		}
 	}
-}
+}*/
 ?>
 </div>
 </div>
@@ -57,6 +57,99 @@
 			?>
 		</div>
 
+
+
+
+
+<div class="grid">
+	<?php while (have_posts() ): the_post();
+		$cats = get_the_category();
+		//$the_cat = get_the_category()[0];
+		if ($cats) {
+			foreach($cats as $cat){
+				if ($cat->slug == 'ae-spectrum') {
+					$the_cat = $cat;
+					break;
+				} elseif ($cat->slug == 'news-spectrum') {
+					$the_cat = $cat;
+					break;
+				} elseif ($cat->slug == 'sports-spectrum') {
+					$the_cat = $cat;
+					break;
+				}
+				$the_cat = $cat;
+			}
+		} ?>
+		<div class="grid-item <?php echo $the_cat->slug ?>">
+			<div id="photoblog-post-block">
+				<div class="photoblog-post-image" id ="pb-image-<?php echo $the_cat->slug; ?>">
+					<a href="<?php the_permalink(); ?>">
+						<?php $singlepicture = get_post_meta($post->ID, 'singlepic', true);
+						$image = do_shortcode('[singlepic id='.$singlepicture.']');
+		            
+
+		            	$permalink = get_permalink();
+		            	$start_of_href = strpos($image, 'href');
+		            	$start_of_url = strpos($image, '"', $start_of_href);
+		            	$end_of_url = strpos($image, '"', $start_of_url+1);
+
+		            	$final_tag = substr($image, 0, $start_of_url) . "\"" . $permalink . substr($image, $end_of_url);
+		            	echo $final_tag;
+						?>
+					</a>
+					
+
+				
+				</div>
+
+				<div class="picOverlay">
+					<!-- <a href= "/category/spectrum/<?php echo $the_cat->slug; ?>" class="photoblog-post-tag pb-tag-<?php echo $the_cat->slug; ?>">
+					<?php if ($the_cat->slug == "archives"): { ?>
+						ARCHIVES <?php } ?>
+					<?php elseif ($the_cat->slug == "campus-spectrum"): { ?>
+						CAMPUS <?php } ?>
+					<?php elseif ($the_cat->slug == "humans-of-westwood"):{ ?>
+						HOWEWO <?php } ?>
+					<?php elseif ($the_cat->slug == "sports-spectrum"):{ ?>
+						SPORTS <?php } ?>
+					<?php elseif ($the_cat->slug == "westwoodla"):{ ?>
+						WW/LA <?php } ?>
+					<?php endif ?>
+					</a> -->
+					
+					<div class="photoblog-text" id="photoblog-post-text">
+						<h3>
+							<p id="photoblog-title">
+								<a href="<?php the_permalink() ?>">
+									<?php
+									the_title();
+									?>
+								</a>
+							</p>
+						</h3> 
+						<?php $t = get_the_excerpt(); 
+							$periodPosition = strpos($t, '.');
+							if ($periodPosition > 0) {
+								echo  $t = substr($t, 0, $periodPosition); 
+							} else {
+								echo $t;
+							}
+							?>
+						
+						<p id="photoblog-post-author">Credit: <?php coauthors(); ?></p>
+						<p class="photoblog-sm">
+						    <div class="fb-like" data-href="<?php echo get_permalink(); ?>" data-layout="button" data-action="like" data-show-faces="true" data-share="true"></div>
+						</p>
+					</div>
+				</div>
+			</div> <!--photoblog-post-block-->
+		</div> <!-- isotope grid -->
+		<?php endwhile; ?>
+</div>
+
+
+
+
 		<?php if(function_exists('the_audio')) the_audio(); ?>
 		<hr>
 		<?php the_content(); ?>
@@ -68,8 +161,50 @@
 		<?php wp_link_pages(array('before' => '<nav class="pagination">', 'after' => '</nav>')); ?>
 		
 	</div> <!--photoblog-post-block-->
+</div>
 
 </div><!-- end div#post -->
+
+
+
+
+
+
+<script type="text/javascript">
+	 /* Isotope
+  * ==================== */
+var elem = document.querySelector('.grid');
+var iso = new Isotope( elem, {
+  // options
+  itemSelector: '.grid-item',
+  layoutMode: 'fitRows'
+});
+// element argument can be a selector string
+//   for an individual element
+var iso = new Isotope( '.grid', {
+  // options
+});
+$("div.grid-item").hover(
+	function () {
+		$(this).find(".picOverlay").fadeIn();
+	}, 
+	function () {
+		$(this).find(".picOverlay").fadeOut();
+	}
+);
+
+
+/*
+ * Isotope animations
+ */
+
+function filterPhotos(tag) {
+  	iso.arrange({
+  		filter: tag
+	})
+}
+
+</script>
 
 
 
