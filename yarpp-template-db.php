674eddc314cf00273548109ab9ad6776f8fb9820
Howard Huang <howardhuang96@gmail.com>
2017-03-24 15:54:30 -0700

    added text in case there are no related posts found

--- yarpp-template-db.php
+++ yarpp-template-db.php
@@ -6,7 +6,7 @@ Author: bryanvtran (Bryan Tran)
 */
 ?><?php if (have_posts()):?>
 <div id="sidebar-similar" class="side-info">
-		<h4 class="yarpp-title">Related Posts</h4>
+		<h4 class="yarpp-title">Related Content</h4>
 		<ul class="related-posts">
 			<?php while (have_posts()) : the_post(); ?>
             <li>
@@ -42,4 +42,9 @@ Author: bryanvtran (Bryan Tran)
 			<?php endwhile; ?>
 		</ul>
 </div><!-- end div#sidebar-similar -->
+<?php else: ?>
+	<div id="sidebar-similar" class="side-info">
+			<h4 class="yarpp-title">Related Content</h4>
+			<p class="redirect-text">Read more for <?php the_category(", "); ?></p>
+	</div>
 <?php endif; ?>
