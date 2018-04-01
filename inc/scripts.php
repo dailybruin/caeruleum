19df2003ac777ee6254d9bb922ae4eb74c43d47e
Nathan Smith <nathan@nathansmith.io>
2018-03-31 23:22:12 -0700

    array null

--- inc/scripts.php
+++ inc/scripts.php
@@ -21,7 +21,7 @@ function roots_scripts() {
   // If you're not using Bootstrap, include HTML5 Boilerplate's main.css:
   // wp_enqueue_style('roots_h5bp', get_template_directory_uri() . '/css/main.css', false, null);
 
-  wp_enqueue_style('roots_app', get_template_directory_uri() . '/css/app.css', false, null);
+  wp_enqueue_style('roots_app', get_template_directory_uri() . '/css/app.css', array(), null);
   
 
   // Load style.css from child theme
