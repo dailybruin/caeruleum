dea3d4704340080b7432a421fd55cf207d26e97f
Dustin Newman <dustinnewman98@gmail.com>
2019-04-25 13:26:28 -0700

    updated non-email banners

--- ad-programmatic.php
+++ ad-programmatic.php
@@ -1,3 +1,8 @@
  <script type="text/javascript">
- 	!function(){var e=document.createElement("script");e.type="text/javascript",e.async=!0,e.src="https://www.rtb123.com/tags/773F3C17-7C67-20A7-40A0-F1BBB5264072/btp.js";var t=document.getElementsByTagName("head")[0];t?t.appendChild(e,t):(t=document.getElementsByTagName("script")[0]).parentNode.insertBefore(e,t)}();
+ 	!function(){var e=document.createElement("script");e.type="text/javascript",e.async=!0,e.src="https://www.rtb123.com/tags/773F3C17-7C67-20A7-40A0-F1BBB5264072/btp.js";var t=document.getElementsByTagName("head")[0];t?t.appendChild(e,t):(t=document.getElementsByTagName("script")[0]).parentNode.insertBefore(e,t);
+	 var m = document.createElement("script");
+	 m.type = "text/javascript", m.async = !0, m.src = "https://cdn.broadstreetads.com/init-2.min.js";
+	 t ? t.appendChild(m, t) : (t=document.getElementsByTagName("script")[0]).parentNode.insertBefore(m,t);
+	 }();
  </script>
+ <script>broadstreet.watch({ networkId: 5876 })</script>
--- ad-smallbanner.php
+++ ad-smallbanner.php
@@ -1,9 +1,1 @@
-<div id="ad-banner-small">
-	<?php $rand_num = mt_rand(1,999999999); ?>
-
-
-<iframe id="b301e745ca" name="b301e745ca" src="//oncampusweb-d.openx.net/w/1.0/afr?auid=538378860&cb=<?php echo $rand_num; ?>" frameborder="0" scrolling="no" width="320" height="50"><a href="//oncampusweb-d.openx.net/w/1.0/rc?cs=b301e745ca&cb=<?php echo $rand_num; ?>" ><img src="//oncampusweb-d.openx.net/w/1.0/ai?auid=538378860&cs=b301e745ca&cb=<?php echo $rand_num; ?>" border="0" alt=""></a></iframe>
-
-
-<!-- 					<img class="ad visible-desktop" src="http://placehold.it/300x250&text=ADVERTISEMENT" />
- --></div>
+<broadstreet-zone zone-id="69406"></broadstreet-zone>
