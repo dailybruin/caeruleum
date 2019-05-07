0efcae65f0c2c1bcefe575c00b2add6ca0c24faa
Dustin Newman <dustinnewman98@gmail.com>
2019-05-07 13:34:49 -0700

    use new version of photoblog css

--- pages/page-spectrum-about.php
+++ pages/page-spectrum-about.php
@@ -10,7 +10,7 @@ Template Name: About Spectrum
 <div class="row">
 	<div class="small-12 columns" id="post-listing">
     <div class="pb-btb-text">
-      <?php echo '<link href="/css/photoblog.css?v=1365830493" rel="stylesheet" media="screen" type="text/css" />'; ?>
+      <?php echo '<link href="/css/photoblog.css?v=1365830494" rel="stylesheet" media="screen" type="text/css" />'; ?>
 			<img class="small-12 columns" id="spectrum-about-img" src="http://dailybruin.com/images/2016/04/spectrum.png" alt="spectrum-about-img">
 			<div class="small-12 columns">
 				<p class="spectrum-about-text">Spectrum is the photojournalism blog of the Daily Bruin, showcasing the finest the department has to offer in visual journalism. This blog explores the versatility of digital media presentation by using photography as its primary storytelling device. From coverage of campus culture to the Los Angeles lifestyle to the latest in Bruin sports, Spectrum captures a broad range of the UCLA experience through visual terms.</p>
